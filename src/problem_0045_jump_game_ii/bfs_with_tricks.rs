25424eebe46b852a8c8e4ea0e7e524d59586ad29
EFanZh <efanzh@gmail.com>
2019-03-21 21:15:31 +0800

    Add solutions to algorithm problem 45: Jump Game II

---
+++ src/problem_0045_jump_game_ii/bfs_with_tricks.rs
@@ -0,0 +1,32 @@
+pub struct Solution {}
+
+impl Solution {
+    fn jump(nums: Vec<i32>) -> i32 {
+        let mut step = 0;
+        let mut left = 0;
+        let mut right = 1;
+
+        while right < nums.len() {
+            let farthest = nums
+                .iter()
+                .enumerate()
+                .take(right)
+                .skip(left)
+                .map(|(i, n)| i + *n as usize)
+                .max()
+                .unwrap();
+
+            step += 1;
+            left = right;
+            right = farthest + 1;
+        }
+
+        step
+    }
+}
+
+impl super::Solution for Solution {
+    fn jump(nums: Vec<i32>) -> i32 {
+        Solution::jump(nums)
+    }
+}
