dbcd144c5790fe0f260da8d5e35c01bd26b93884
EFanZh <efanzh@gmail.com>
2019-06-05 22:29:35 +0800

    Add algorithm problem 4: Median of Two Sorted Arrays

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod problem_0001_two_sum;
+pub mod problem_0004_median_of_two_sorted_arrays;
 pub mod problem_0015_3sum;
 pub mod problem_0018_4sum;
 pub mod problem_0035_search_insert_position;
---
+++ src/problem_0004_median_of_two_sorted_arrays/binary_search.rs
@@ -0,0 +1,73 @@
+struct Solution {}
+
+use std::mem::swap;
+
+impl Solution {
+    pub fn find_median_sorted_arrays(mut nums1: Vec<i32>, mut nums2: Vec<i32>) -> f64 {
+        if nums2.len() < nums1.len() {
+            swap(&mut nums1, &mut nums2);
+        };
+
+        let total_count = nums1.len() + nums2.len();
+        let half_total_count = total_count / 2;
+
+        let mut left = 0;
+        let mut size = nums1.len();
+
+        while size > 0 {
+            let half_size = size / 2;
+            let i = left + half_size;
+            let j = half_total_count - i;
+
+            if nums2[j - 1] > nums1[i] {
+                left = i + 1;
+                size -= half_size + 1;
+            } else {
+                size = half_size;
+            }
+        }
+
+        let n = if let Some(&i_value) = nums1.get(left) {
+            if let Some(&j_value) = nums2.get(half_total_count - left) {
+                i_value.min(j_value)
+            } else {
+                i_value
+            }
+        } else {
+            nums2[half_total_count - left]
+        };
+
+        if total_count % 2 == 0 {
+            let m = if let Some(&i_left_value) = nums1.get(left.wrapping_sub(1)) {
+                if let Some(&j_left_value) = nums2.get((half_total_count - left).wrapping_sub(1)) {
+                    i_left_value.max(j_left_value)
+                } else {
+                    i_left_value
+                }
+            } else {
+                nums2[half_total_count - 1]
+            };
+
+            f64::from(m + n) / 2.0
+        } else {
+            f64::from(n)
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        Solution::find_median_sorted_arrays(nums1, nums2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
