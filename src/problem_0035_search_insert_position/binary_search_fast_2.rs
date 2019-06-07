5b7527ca6d7d851a52badbcba1b56eca2d4df946
EFanZh <efanzh@gmail.com>
2019-06-07 09:46:53 +0800

    Add another solution to algorithm problem 35

---
+++ src/problem_0035_search_insert_position/binary_search_fast_2.rs
@@ -0,0 +1,37 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut size = nums.len();
+
+        while size > 0 {
+            let half = size / 2;
+
+            if nums[left + half] < target {
+                left += size - half;
+            }
+
+            size = half;
+        }
+
+        left as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        Solution::search_insert(nums, target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
