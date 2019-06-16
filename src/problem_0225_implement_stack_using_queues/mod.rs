69241460cd3d3d01575796f6c304731a94fe645f
EFanZh <efanzh@gmail.com>
2019-06-16 21:50:30 +0800

    Add solution to algorithm problem 225: Implement Stack using Queues

---
+++ src/problem_0225_implement_stack_using_queues/mod.rs
@@ -0,0 +1,40 @@
+pub mod slow_push;
+
+pub trait MyStack {
+    fn new() -> Self;
+    fn push(&mut self, x: i32);
+    fn pop(&mut self) -> i32;
+    fn top(&self) -> i32;
+    fn empty(&self) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MyStack;
+
+    enum StackOperation {
+        Push(i32),
+        Pop(i32),
+        Top(i32),
+        Empty(bool),
+    }
+
+    pub fn run_tests<S: MyStack>() {
+        use StackOperation::{Empty, Pop, Push, Top};
+
+        let test_cases = vec![vec![Push(1), Push(2), Top(2), Pop(2), Empty(false)]];
+
+        for test_case in test_cases {
+            let mut stack = S::new();
+
+            for operation in test_case {
+                match operation {
+                    Push(value) => stack.push(value),
+                    Pop(value) => assert_eq!(stack.pop(), value),
+                    Top(value) => assert_eq!(stack.top(), value),
+                    Empty(value) => assert_eq!(stack.empty(), value),
+                }
+            }
+        }
+    }
+}
---
+++ src/problem_0225_implement_stack_using_queues/slow_push.rs
@@ -0,0 +1,66 @@
+use std::collections::VecDeque;
+
+struct MyStack {
+    q: VecDeque<i32>,
+}
+
+impl MyStack {
+    fn new() -> Self {
+        Self { q: VecDeque::new() }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.q.push_back(x);
+
+        for _ in 1..self.q.len() {
+            let value = self.q.pop_front().unwrap();
+
+            self.q.push_back(value)
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.q.pop_front().unwrap()
+    }
+
+    fn top(&self) -> i32 {
+        *self.q.front().unwrap()
+    }
+
+    fn empty(&self) -> bool {
+        self.q.is_empty()
+    }
+}
+
+impl super::MyStack for MyStack {
+    fn new() -> Self {
+        Self::new()
+    }
+
+    fn push(&mut self, x: i32) {
+        self.push(x);
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.pop()
+    }
+
+    fn top(&self) -> i32 {
+        self.top()
+    }
+
+    fn empty(&self) -> bool {
+        self.empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::MyStack;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<MyStack>();
+    }
+}
