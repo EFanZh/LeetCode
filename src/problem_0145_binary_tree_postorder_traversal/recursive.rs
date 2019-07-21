378f95f4ca76603262a1a01d3557e889d23554fb
EFanZh <efanzh@gmail.com>
2019-07-21 13:26:16 +0800

    Add algorithm problem 145: Binary Tree Postorder Traversal

---
+++ src/problem_0145_binary_tree_postorder_traversal/recursive.rs
@@ -0,0 +1,44 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        fn helper(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
+            if let Some(node) = root {
+                let node_ref = node.borrow();
+
+                helper(&node_ref.left, result);
+                helper(&node_ref.right, result);
+
+                result.push(node_ref.val);
+            }
+        }
+
+        let mut result = Vec::new();
+
+        helper(&root, &mut result);
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        Solution::postorder_traversal(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
