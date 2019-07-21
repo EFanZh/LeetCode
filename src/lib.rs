d0e455b947ba0f2cd010ee389e9072c587ac4ace
EFanZh <efanzh@gmail.com>
2019-07-21 11:58:59 +0800

    Add algorithm problem 102: Binary Tree Level Order Traversal

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod problem_0035_search_insert_position;
 pub mod problem_0045_jump_game_ii;
 pub mod problem_0089_gray_code;
 pub mod problem_0094_binary_tree_inorder_traversal;
+pub mod problem_0102_binary_tree_level_order_traversal;
 pub mod problem_0136_single_number;
 pub mod problem_0225_implement_stack_using_queues;
 pub mod problem_0232_implement_queue_using_stacks;
---
+++ src/problem_0102_binary_tree_level_order_traversal/iterative.rs
@@ -0,0 +1,64 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+
+        if let Some(node) = root {
+            let mut node_level = VecDeque::new();
+
+            result.push(vec![node.borrow().val]);
+
+            node_level.push_back(node);
+
+            loop {
+                let mut value_level = Vec::new();
+
+                for _ in 0..node_level.len() {
+                    let node = node_level.pop_front().unwrap();
+
+                    if let Some(left) = &node.borrow().left {
+                        node_level.push_back(left.clone());
+                        value_level.push(left.borrow().val);
+                    }
+
+                    if let Some(right) = &node.borrow().right {
+                        node_level.push_back(right.clone());
+                        value_level.push(right.borrow().val);
+                    };
+                }
+
+                if node_level.is_empty() {
+                    break;
+                } else {
+                    result.push(value_level);
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        Solution::level_order(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
