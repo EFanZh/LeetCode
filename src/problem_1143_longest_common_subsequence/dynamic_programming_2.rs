b311dc772aab757c0509ce6aae04b8da149c2d55
EFanZh <efanzh@gmail.com>
2019-11-23 17:18:34 +0800

    Add problem 1143: Longest Common Subsequence

---
+++ src/problem_1143_longest_common_subsequence/dynamic_programming_2.rs
@@ -0,0 +1,48 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        let (text1, text2) = if text2.len() < text1.len() {
+            (text2.as_bytes(), text1.as_bytes())
+        } else {
+            (text1.as_bytes(), text2.as_bytes())
+        };
+
+        let mut cache = vec![0; text1.len() + 1];
+
+        for y_j in text2 {
+            let mut old_value = 0;
+
+            for (i, x_i) in text1.iter().enumerate() {
+                let new_value = if x_i == y_j {
+                    old_value + 1
+                } else {
+                    cache[i].max(cache[i + 1])
+                };
+
+                old_value = mem::replace(&mut cache[i + 1], new_value);
+            }
+        }
+
+        *cache.last().unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn longest_common_subsequence(text1: String, text2: String) -> i32 {
+        Solution::longest_common_subsequence(text1, text2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
---
+++ src/problem_1143_longest_common_subsequence/mod.rs
@@ -0,0 +1,27 @@
+pub mod dynamic_programming_1;
+pub mod dynamic_programming_2;
+pub mod memoized_dynamic_programming_1;
+pub mod memoized_dynamic_programming_2;
+
+pub trait Solution {
+    fn longest_common_subsequence(text1: String, text2: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = vec![
+            (("abcde", "ace"), 3),
+            (("abc", "abc"), 3),
+            (("abc", "def"), 0),
+            (("psnw", "vozsh"), 1),
+            (("ac", "bc"), 1),
+        ];
+
+        for ((text1, text2), expected) in test_cases {
+            assert_eq!(S::longest_common_subsequence(text1.into(), text2.into()), expected);
+        }
+    }
+}
