3a506e39e69b750838556e749aaf3d0116a460ef
EFanZh <efanzh@gmail.com>
2019-12-01 19:26:46 +0800

    Add problem 516: Longest Palindromic Subsequence

---
+++ src/problem_0516_longest_palindromic_subsequence/mod.rs
@@ -0,0 +1,19 @@
+pub mod dynamic_programming;
+pub mod memoized_dynamic_programming;
+
+pub trait Solution {
+    fn longest_palindrome_subseq(s: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [("", 0), ("a", 1), ("bbbab", 4), ("cbbd", 2), ("character", 5)];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::longest_palindrome_subseq(s.to_owned()), expected);
+        }
+    }
+}
