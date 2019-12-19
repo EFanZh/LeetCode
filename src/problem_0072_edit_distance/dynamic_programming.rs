96efce1b69aec59a2c7a4bc4ccfb104a0a9145f0
EFanZh <efanzh@gmail.com>
2019-12-19 22:00:24 +0800

    Add problem 72: Edit Distance

---
+++ src/problem_0072_edit_distance/dynamic_programming.rs
@@ -0,0 +1,48 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    fn min_distance(mut word1: String, mut word2: String) -> i32 {
+        if word2.len() < word1.len() {
+            mem::swap(&mut word1, &mut word2);
+        }
+
+        let mut cache = (0..=word1.len() as _).rev().collect::<Box<_>>();
+
+        for (prev_base, c2) in word2.as_bytes().iter().rev().enumerate() {
+            let mut prev = prev_base as _;
+
+            cache[word1.len()] = prev + 1;
+
+            for (i, c1) in word1.as_bytes().iter().enumerate().rev() {
+                let distance = if c1 == c2 {
+                    prev
+                } else {
+                    cache[i].min(cache[i + 1]).min(prev) + 1
+                };
+
+                prev = mem::replace(&mut cache[i], distance);
+            }
+        }
+
+        cache[0]
+    }
+}
+
+impl super::Solution for Solution {
+    fn min_distance(word1: String, word2: String) -> i32 {
+        Self::min_distance(word1, word2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
