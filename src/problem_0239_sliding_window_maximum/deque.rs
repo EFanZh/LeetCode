c60922ea0d613487bf14f88f4437b7bdf7ca2041
EFanZh <efanzh@gmail.com>
2019-12-28 11:46:45 +0800

    Add problem 239: Sliding Window Maximum

---
+++ src/problem_0239_sliding_window_maximum/deque.rs
@@ -0,0 +1,60 @@
+pub struct Solution {}
+
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let k = k as _;
+        let mut result = Vec::with_capacity(nums.len() - k + 1);
+
+        if !nums.is_empty() {
+            let mut stack = VecDeque::with_capacity(k);
+            let (nums_1, nums_2) = nums.split_at(k);
+
+            for num in nums_1 {
+                if let Some(i) = stack.iter().position(|x| x < num) {
+                    stack.truncate(i);
+                }
+
+                stack.push_back(*num);
+            }
+
+            for (new_num, old_num) in nums_2.iter().zip(&nums) {
+                let front = stack[0];
+
+                result.push(front);
+
+                if front == *old_num {
+                    stack.pop_front();
+                }
+
+                if let Some(i) = stack.iter().position(|x| x < new_num) {
+                    stack.truncate(i);
+                }
+
+                stack.push_back(*new_num);
+            }
+
+            result.push(stack[0]);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        Self::max_sliding_window(nums, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
