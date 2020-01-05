817d4530c71480ecbf1b661e77a74ae9839b686a
EFanZh <efanzh@gmail.com>
2020-01-05 19:41:46 +0800

    Add problem 435: Non-overlapping Intervals

---
+++ src/problem_0435_non_overlapping_intervals/mod.rs
@@ -0,0 +1,25 @@
+pub mod greedy;
+
+pub trait Solution {
+    fn erase_overlap_intervals(intervals: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            (&[(1, 2), (2, 3), (3, 4), (1, 3)] as &[_], 1),
+            (&[(1, 2), (1, 2), (1, 2)], 2),
+            (&[(1, 2), (2, 3)], 0),
+        ];
+
+        for (intervals, expected) in &test_cases {
+            assert_eq!(
+                S::erase_overlap_intervals(intervals.iter().map(|&(s, f)| vec![s, f]).collect()),
+                *expected
+            );
+        }
+    }
+}
