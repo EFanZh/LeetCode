b6dc615ff70da8ff4ea81bb63bc7d36b06bb4518
EFanZh <efanzh@gmail.com>
2020-01-05 14:06:56 +0800

    Add problem 164: Maximum Gap

---
+++ src/problem_0164_maximum_gap/buckets_and_pigeonhole.rs
@@ -0,0 +1,68 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn maximum_gap(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let maybe_min_value = nums.iter().min();
+        let maybe_max_value = nums.iter().max();
+
+        if maybe_min_value != maybe_max_value {
+            let min_value = *maybe_min_value.unwrap();
+            let max_value = *maybe_max_value.unwrap();
+            let length = max_value - min_value;
+
+            // The maximum gap must be greater than or equal to length / (n - 1), so as long as the bucket size is less
+            // than length / (n - 1) + 1, the maximum gap can not be inside one bucket. So we only need to check gaps
+            // between buckets.
+
+            let bucket_size = {
+                let n = nums.len() as i32;
+
+                (length + n - 2) / (n - 1)
+            };
+
+            let mut buckets = vec![(-1, 0); (length / bucket_size + 1) as _];
+
+            for num in nums {
+                let (left, right) = &mut buckets[((num - min_value) / bucket_size) as usize];
+
+                if *left == -1 {
+                    *left = num;
+                    *right = num;
+                } else if num < *left {
+                    *left = num;
+                } else if num > *right {
+                    *right = num;
+                }
+            }
+
+            let mut bucket_iter = buckets.into_iter().filter(|(left, _)| *left != -1);
+            let mut previous = bucket_iter.next().unwrap().1;
+
+            for (left, right) in bucket_iter {
+                result = result.max(left - previous);
+
+                previous = right;
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn maximum_gap(nums: Vec<i32>) -> i32 {
+        Self::maximum_gap(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
---
+++ src/problem_0164_maximum_gap/radix_sort.rs
@@ -0,0 +1,91 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    pub fn maximum_gap(mut nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+
+        if let Some(max_value) = nums.iter().max() {
+            let n = nums.len();
+
+            if *max_value > 0 && n > 1 {
+                // From the book Introduction to Algorithms, third edition, page 199.
+
+                let b = mem::size_of_val(max_value) * 8 - (max_value.leading_zeros() as usize);
+                let log2_n = mem::size_of_val(&n) * 8 - (n.leading_zeros() as usize) - 1;
+                let r = b.min(log2_n);
+                let num_digits = (b + (r - 1)) / r;
+
+                // Radix sort n b-bit numbers with  ⌈b / r⌉ r-bit digits.
+
+                let mut temp_nums = vec![0; n];
+                let mut counts = vec![0; 1 << r];
+                let mask = (1 << r) - 1;
+
+                for key_bit in (0..r * num_digits).step_by(r) {
+                    // Sort by this digit with counting sort.
+
+                    // Count numbers.
+
+                    for value in nums.iter() {
+                        counts[((value >> key_bit) & mask) as usize] += 1;
+                    }
+
+                    // Calculate indices.
+
+                    for i in 1..counts.len() {
+                        counts[i] += counts[i - 1];
+                    }
+
+                    // Place result to temp_nums.
+
+                    for value in nums.iter().rev() {
+                        let key = ((value >> key_bit) & mask) as usize;
+
+                        temp_nums[counts[key] - 1] = *value;
+
+                        counts[key] -= 1;
+                    }
+
+                    // Maintain loop invariant.
+
+                    for c in counts.iter_mut() {
+                        *c = 0;
+                    }
+
+                    mem::swap(&mut nums, &mut temp_nums);
+                }
+
+                let mut iter = nums.into_iter();
+
+                if let Some(mut previous) = iter.next() {
+                    for current in iter {
+                        result = result.max(current - previous);
+
+                        previous = current
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn maximum_gap(nums: Vec<i32>) -> i32 {
+        Self::maximum_gap(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
