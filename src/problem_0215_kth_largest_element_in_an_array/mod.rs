d0576da4d19240e25caa1bf504a3df6c6d651fd7
EFanZh <efanzh@gmail.com>
2020-01-18 22:37:24 +0800

    Add problem 215: Kth Largest Element in an Array

---
+++ src/problem_0215_kth_largest_element_in_an_array/mod.rs
@@ -0,0 +1,23 @@
+pub mod quick_select;
+pub mod randomized_quick_select;
+
+pub trait Solution {
+    fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            ((&[3, 2, 1, 5, 6, 4] as &[_], 2), 5),
+            ((&[3, 2, 3, 1, 2, 4, 5, 5, 6], 4), 4),
+            ((&[99, 99], 1), 99),
+        ];
+
+        for ((nums, k), expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_kth_largest(nums.iter().copied().collect(), k), expected);
+        }
+    }
+}
---
+++ src/problem_0215_kth_largest_element_in_an_array/quick_select.rs
@@ -0,0 +1,73 @@
+pub struct Solution {}
+
+use std::cmp::Ordering;
+use std::mem;
+
+impl Solution {
+    fn partition_by<T, P: FnMut(&T) -> bool>(mut values: &mut [T], mut predicate: P) -> usize {
+        let mut result = 0;
+
+        'k: while let Some((first, mut rest)) = values.split_first_mut() {
+            if predicate(first) {
+                values = rest;
+                result += 1;
+            } else {
+                while let Some((last, body)) = rest.split_last_mut() {
+                    if predicate(last) {
+                        mem::swap(first, last);
+                        values = body;
+                        result += 1;
+
+                        continue 'k;
+                    } else {
+                        rest = body;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        result
+    }
+
+    fn nth_element<T: Ord + Clone>(mut values: &mut [T], mut n: usize) -> T {
+        loop {
+            let (last, rest) = values.split_last_mut().unwrap();
+            let left_length = Self::partition_by(rest, |x| x < last);
+
+            match n.cmp(&left_length) {
+                Ordering::Less => values = &mut values[..left_length],
+                Ordering::Equal => return last.clone(),
+                Ordering::Greater => {
+                    values.swap(left_length, values.len() - 1);
+                    values = &mut values[left_length + 1..];
+                    n -= left_length + 1;
+                }
+            }
+        }
+    }
+
+    pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+        let k = nums.len() - (k as usize);
+
+        Self::nth_element(&mut nums, k)
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+        Self::find_kth_largest(nums, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
