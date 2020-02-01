ef3fc0c3ce120f9113fb8305199a3f76cf1a3183
EFanZh <efanzh@gmail.com>
2020-02-01 19:57:39 +0800

    Add another solution to problem 322: Coin Change

--- src/problem_0322_coin_change/dynamic_programming_2.rs
+++ src/problem_0322_coin_change/dynamic_programming_2.rs
@@ -3,17 +3,30 @@ pub struct Solution {}
 impl Solution {
     pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
         let amount = amount as usize;
-        let mut cache = vec![Some(0); amount + 1];
+        let mut cache = vec![0; amount + 1];
 
         for i in 1..=amount {
             cache[i] = coins
                 .iter()
-                .filter_map(|c| i.checked_sub(*c as usize).and_then(|r| cache[r]))
+                .filter_map(|c| {
+                    if let Some(r) = i.checked_sub(*c as usize) {
+                        let v = cache[r];
+
+                        if v < 0 {
+                            None
+                        } else {
+                            Some(v)
+                        }
+                    } else {
+                        None
+                    }
+                })
                 .min()
-                .map(|x| x + 1);
+                .map(|x| x + 1)
+                .unwrap_or(-1);
         }
 
-        cache.last().unwrap().unwrap_or(-1)
+        *cache.last().unwrap()
     }
 }
 
