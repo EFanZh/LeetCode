245ff7a2794853806b4b26f9ebc24eec214677f7
EFanZh <efanzh@gmail.com>
2020-03-23 22:27:24 +0800

    Add problem 3: Longest Substring Without Repeating Characters

---
+++ src/problem_0003_longest_substring_without_repeating_characters/sequential_scan.rs
@@ -0,0 +1,48 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        dbg!(&s);
+
+        const INVALID_INDEX: usize = usize::max_value();
+        let s = s.as_bytes();
+        let mut result = 0;
+        let mut start = 0;
+        let mut byte_locations = [INVALID_INDEX; 256];
+
+        for (i, byte) in s.iter().copied().enumerate() {
+            let old_index = mem::replace(&mut byte_locations[byte as usize], i);
+
+            if old_index == INVALID_INDEX {
+                result = result.max(i - start + 1);
+            } else {
+                for removed_byte in s[start..old_index].iter().copied() {
+                    byte_locations[removed_byte as usize] = INVALID_INDEX;
+                }
+
+                start = old_index + 1;
+            }
+        }
+
+        result as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn length_of_longest_substring(s: String) -> i32 {
+        Self::length_of_longest_substring(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
