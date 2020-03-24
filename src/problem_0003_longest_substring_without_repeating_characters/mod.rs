3b3e5fa9daab84b4ae4a72752d8be7a503de4272
EFanZh <efanzh@gmail.com>
2020-03-24 21:16:51 +0800

    Add two more solutions

--- src/problem_0003_longest_substring_without_repeating_characters/mod.rs
+++ src/problem_0003_longest_substring_without_repeating_characters/mod.rs
@@ -1,4 +1,6 @@
 pub mod sliding_window;
+pub mod sliding_window_2;
+pub mod sliding_window_3;
 
 pub trait Solution {
     fn length_of_longest_substring(s: String) -> i32;
@@ -9,7 +11,13 @@ mod tests {
     use super::Solution;
 
     pub fn run_tests<S: Solution>() {
-        let test_cases = [("abcabcbb", 3), ("bbbbb", 1), ("pwwkew", 3), ("abba", 2)];
+        let test_cases = [
+            ("abcabcbb", 3),
+            ("bbbbb", 1),
+            ("pwwkew", 3),
+            ("abba", 2),
+            ("tmmzuxt", 5),
+        ];
 
         for (s, expected) in test_cases.iter().copied() {
             assert_eq!(S::length_of_longest_substring(s.to_owned()), expected);
