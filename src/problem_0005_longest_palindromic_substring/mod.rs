5075efcec89e191718c13a433777238bb31cb71d
EFanZh <efanzh@gmail.com>
2020-03-26 21:57:12 +0800

    Add problem 5: Longest Palindromic Substring

---
+++ src/problem_0005_longest_palindromic_substring/mod.rs
@@ -0,0 +1,18 @@
+pub mod brute_force;
+
+pub trait Solution {
+    fn longest_palindrome(s: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [("babad", &["bab", "aba"] as &[_]), ("cbbd", &["bb"])];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert!(expected.contains(&S::longest_palindrome(s.to_owned()).as_str()));
+        }
+    }
+}
