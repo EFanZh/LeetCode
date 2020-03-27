657eee742f86bf6c121bd0dc50e6cf77d22edcab
EFanZh <efanzh@gmail.com>
2020-03-27 19:50:02 +0800

    Add problem 7: Reverse Integer

---
+++ src/problem_0007_reverse_integer/mod.rs
@@ -0,0 +1,18 @@
+pub mod brute_force;
+
+pub trait Solution {
+    fn reverse(x: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [(123, 321), (-123, -321), (120, 21), (1_534_236_469, 0)];
+
+        for (x, expected) in test_cases.iter().copied() {
+            assert_eq!(S::reverse(x), expected);
+        }
+    }
+}
