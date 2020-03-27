7d4f5591c6a67804d50c964e88016e80e448d546
EFanZh <efanzh@gmail.com>
2020-03-27 19:50:02 +0800

    Add problem 7: Reverse Integer

---
+++ src/problem_0007_reverse_integer/brute_force.rs
@@ -0,0 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn reverse(mut x: i32) -> i32 {
+        let mut result = 0i32;
+
+        while x != 0 {
+            if let Some(new_result) = result.checked_mul(10).and_then(|r| r.checked_add(x % 10)) {
+                result = new_result;
+            } else {
+                return 0;
+            }
+
+            x /= 10;
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn reverse(x: i32) -> i32 {
+        Self::reverse(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
