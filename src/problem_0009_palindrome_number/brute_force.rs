c4bac0d696a9e9d2e522333f612c6fef4a0e6483
EFanZh <efanzh@gmail.com>
2020-03-29 08:17:47 +0800

    Add problem 9: Palindrome Number

---
+++ src/problem_0009_palindrome_number/brute_force.rs
@@ -0,0 +1,41 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            false
+        } else {
+            let mut x2 = x;
+            let mut reversed = 0i32;
+
+            while x2 != 0 {
+                if let Some(new_reversed) = reversed.checked_mul(10).and_then(|r| r.checked_add(x2 % 10)) {
+                    reversed = new_reversed;
+                } else {
+                    return false;
+                }
+
+                x2 /= 10;
+            }
+
+            x == reversed
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_palindrome(x: i32) -> bool {
+        Self::is_palindrome(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
---
+++ src/problem_0009_palindrome_number/brute_force_unsafe.rs
@@ -0,0 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        if x < 0 {
+            false
+        } else {
+            let mut x2 = x;
+            let mut reversed = 0i32;
+
+            while x2 != 0 {
+                reversed = reversed.wrapping_mul(10).wrapping_add(x2 % 10);
+                x2 /= 10;
+            }
+
+            x == reversed
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_palindrome(x: i32) -> bool {
+        Self::is_palindrome(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
