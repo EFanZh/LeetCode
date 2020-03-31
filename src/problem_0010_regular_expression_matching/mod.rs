a7dfcfbca070f0a11236875abe407b671b1ed8d0
EFanZh <efanzh@gmail.com>
2020-03-31 21:22:56 +0800

    Add problem 10: Regular Expression Matching

---
+++ src/problem_0010_regular_expression_matching/mod.rs
@@ -0,0 +1,24 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn is_match(s: String, p: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            (("aa", "a"), false),
+            (("aa", "a*"), true),
+            (("ab", ".*"), true),
+            (("aab", "c*a*b"), true),
+            (("mississippi", "c*a*b"), false),
+        ];
+
+        for ((s, p), expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_match(s.to_owned(), p.to_owned()), expected);
+        }
+    }
+}
