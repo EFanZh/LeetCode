ee5e15e3e4207e3553d2e741eb38e4b21994b868
EFanZh <efanzh@gmail.com>
2020-03-31 21:22:56 +0800

    Add problem 10: Regular Expression Matching

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod problem_0006_zigzag_conversion;
 pub mod problem_0007_reverse_integer;
 pub mod problem_0008_string_to_integer_atoi;
 pub mod problem_0009_palindrome_number;
+pub mod problem_0010_regular_expression_matching;
 pub mod problem_0015_3sum;
 pub mod problem_0018_4sum;
 pub mod problem_0020_valid_parentheses;
---
+++ src/problem_0010_regular_expression_matching/dynamic_programming.rs
@@ -0,0 +1,49 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        let (s, p) = (s.as_bytes(), p.as_bytes());
+        let columns = p.len() + 1;
+        let mut cache = vec![false; columns * (s.len() + 1)];
+        let cache_len = cache.len();
+        let last_row = &mut cache[cache_len - columns..];
+
+        *last_row.last_mut().unwrap() = true;
+
+        for j in (0..p.len()).rev() {
+            if p.get(j + 1) == Some(&b'*') {
+                last_row[j] = last_row[j + 2];
+            }
+        }
+
+        for (i, s_i) in s.iter().enumerate().rev() {
+            for (j, p_j) in p.iter().enumerate().rev() {
+                cache[columns * i + j] = match (*p_j == b'.' || p_j == s_i, p.get(j + 1) == Some(&b'*')) {
+                    (false, false) => continue,
+                    (false, true) => cache[columns * i + (j + 2)],
+                    (true, false) => cache[columns * (i + 1) + (j + 1)],
+                    (true, true) => cache[columns * i + (j + 2)] || cache[columns * (i + 1) + j],
+                }
+            }
+        }
+
+        cache[0]
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_match(s: String, p: String) -> bool {
+        Self::is_match(s, p)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
