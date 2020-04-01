de1b14aa671f1219b1f54e723c43520a602c6f1c
EFanZh <efanzh@gmail.com>
2020-04-01 18:26:35 +0800

    Add problem 11: Container With Most Water

---
+++ src/problem_0011_container_with_most_water/two_pointers.rs
@@ -0,0 +1,53 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let (mut head, rest) = height.split_first().unwrap();
+        let (mut tail, mut body) = rest.split_last().unwrap();
+        let mut length = rest.len() as i32;
+
+        loop {
+            if head < tail {
+                result = result.max(length * head);
+
+                if let Some((new_head, new_body)) = body.split_first() {
+                    head = new_head;
+                    body = new_body;
+                } else {
+                    break;
+                }
+            } else {
+                result = result.max(length * tail);
+
+                if let Some((new_tail, new_body)) = body.split_last() {
+                    tail = new_tail;
+                    body = new_body;
+                } else {
+                    break;
+                }
+            }
+
+            length -= 1;
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn max_area(height: Vec<i32>) -> i32 {
+        Self::max_area(height)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
