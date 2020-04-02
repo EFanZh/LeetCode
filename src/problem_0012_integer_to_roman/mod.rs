4dfe08b771ae2309baa0de487bc627343efffa5f
EFanZh <efanzh@gmail.com>
2020-04-02 21:37:46 +0800

    Add problem 12: Integer to Roman

---
+++ src/problem_0012_integer_to_roman/mod.rs
@@ -0,0 +1,19 @@
+pub mod generic;
+pub mod specialized_1;
+
+pub trait Solution {
+    fn int_to_roman(num: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [(3, "III"), (4, "IV"), (9, "IX"), (58, "LVIII"), (1994, "MCMXCIV")];
+
+        for (num, expected) in test_cases.iter().copied() {
+            assert_eq!(S::int_to_roman(num), expected);
+        }
+    }
+}
---
+++ src/problem_0012_integer_to_roman/specialized_1.rs
@@ -0,0 +1,50 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn int_to_roman(mut num: i32) -> String {
+        const DIGITS: [(i32, &str); 13] = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ];
+
+        let mut result = String::new();
+
+        for (base, symbol) in DIGITS.iter().copied() {
+            for _ in 0..num / base {
+                result.push_str(symbol);
+            }
+
+            num %= base;
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn int_to_roman(num: i32) -> String {
+        Self::int_to_roman(num)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
