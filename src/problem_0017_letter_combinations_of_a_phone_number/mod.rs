f13e28f0de44af6f9ab368639c6e597ae827f842
EFanZh <efanzh@gmail.com>
2020-04-06 20:11:24 +0800

    Add problem 17: Letter Combinations of a Phone Number

---
+++ src/problem_0017_letter_combinations_of_a_phone_number/mod.rs
@@ -0,0 +1,21 @@
+pub mod backtrack;
+
+pub trait Solution {
+    fn letter_combinations(digits: String) -> Vec<String>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            ("23", &["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"] as &[_]),
+            ("", &[]),
+        ];
+
+        for (digits, expected) in test_cases.iter().copied() {
+            assert_eq!(S::letter_combinations(digits.to_owned()), expected);
+        }
+    }
+}
