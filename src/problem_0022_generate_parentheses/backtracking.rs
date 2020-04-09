055d8228b70fa287656eac74f798ac5e0b233cdf
EFanZh <efanzh@gmail.com>
2020-04-09 23:00:49 +0800

    Add problem 22: Generate Parentheses

---
+++ src/problem_0022_generate_parentheses/backtracking.rs
@@ -0,0 +1,38 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        if n == 0 {
+            return vec!["".to_owned()];
+        } else {
+            let mut result = Vec::new();
+
+            for i in (0..n).rev() {
+                for left in Self::generate_parenthesis(i) {
+                    for right in Self::generate_parenthesis(n - i - 1) {
+                        result.push(format!("({}){}", left, right));
+                    }
+                }
+            }
+
+            result
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn generate_parenthesis(n: i32) -> Vec<String> {
+        Self::generate_parenthesis(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
