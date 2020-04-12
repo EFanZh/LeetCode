5514e59c9b6bce29fefd5095af54dd3b56b2042c
EFanZh <efanzh@gmail.com>
2020-04-12 13:54:07 +0800

    Add problem 25: Reverse Nodes in k-Group

---
+++ src/problem_0025_reverse_nodes_in_k_group/mod.rs
@@ -0,0 +1,31 @@
+use super::data_structures::ListNode;
+
+pub mod reverse_group;
+pub mod reverse_group_2;
+
+pub trait Solution {
+    fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5] as &[_], 2), &[2, 1, 4, 3, 5] as &[_]),
+            ((&[1, 2, 3, 4, 5] as &[_], 3), &[3, 2, 1, 4, 5] as &[_]),
+        ];
+
+        for ((head, k), expected) in test_cases.iter().copied() {
+            let head = test_utilities::make_list(head.iter().copied());
+
+            let result = test_utilities::iter_list(&S::reverse_k_group(head, k))
+                .copied()
+                .collect::<Box<_>>();
+
+            assert_eq!(result.as_ref(), expected);
+        }
+    }
+}
