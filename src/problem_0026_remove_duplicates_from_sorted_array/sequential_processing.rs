376ea07c430d71aaf1010931e31e4a333f0281eb
EFanZh <efanzh@gmail.com>
2020-04-13 21:51:49 +0800

    Add problem 26: Remove Duplicates from Sorted Array

---
+++ src/problem_0026_remove_duplicates_from_sorted_array/sequential_processing.rs
@@ -0,0 +1,41 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if let Some(mut first) = nums.first() {
+            let mut deduped = 1;
+            let mut i = 1;
+
+            while let Some(value) = nums.get(i) {
+                if value != first {
+                    nums.swap(deduped, i);
+                    first = &nums[deduped];
+                    deduped += 1;
+                }
+
+                i += 1;
+            }
+
+            nums.truncate(deduped);
+        }
+
+        nums.len() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        Self::remove_duplicates(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
