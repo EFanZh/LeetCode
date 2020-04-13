12c2fc91430376b4dbda0b82773bf6a06553ca3f
EFanZh <efanzh@gmail.com>
2020-04-13 21:51:49 +0800

    Add problem 26: Remove Duplicates from Sorted Array

---
+++ src/problem_0026_remove_duplicates_from_sorted_array/mod.rs
@@ -0,0 +1,28 @@
+pub mod cheating;
+pub mod sequential_processing;
+
+pub trait Solution {
+    fn remove_duplicates(nums: &mut Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            (&[1, 1, 2] as &[_], &[1, 2] as &[_]),
+            (&[0, 0, 1, 1, 1, 2, 2, 3, 3, 4], &[0, 1, 2, 3, 4]),
+            (&[1], &[1]),
+            (&[], &[]),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            let mut nums = nums.to_owned();
+            let result = S::remove_duplicates(&mut nums);
+
+            assert_eq!(nums.as_slice(), expected);
+            assert_eq!(result as usize, expected.len());
+        }
+    }
+}
