ae30f5dfe9726399abe1b277956ae4dcdce4cae2
EFanZh <efanzh@gmail.com>
2020-04-14 22:05:35 +0800

    Add problem 27: Remove Element

---
+++ src/problem_0027_remove_element/sequential_processing.rs
@@ -0,0 +1,38 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut retained = 0;
+        let mut i = 0;
+
+        while let Some(item) = nums.get(i) {
+            if *item != val {
+                nums.swap(retained, i);
+                retained += 1;
+            }
+
+            i += 1;
+        }
+
+        nums.truncate(retained);
+
+        nums.len() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        Self::remove_element(nums, val)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
