b5e45c1ae6b3a63e56a045473f239a7694a944d2
EFanZh <efanzh@gmail.com>
2020-04-15 22:42:11 +0800

    Add problem 31: Next Permutation

---
+++ src/problem_0031_next_permutation/mod.rs
@@ -0,0 +1,27 @@
+pub mod find_last_inversion;
+
+pub trait Solution {
+    fn next_permutation(nums: &mut Vec<i32>);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            (&[1, 2, 3] as &[_], &[1, 3, 2] as &[_]),
+            (&[3, 2, 1], &[1, 2, 3]),
+            (&[1, 1, 5], &[1, 5, 1]),
+            (&[1, 5, 1], &[5, 1, 1]),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            let mut nums = nums.to_owned();
+
+            S::next_permutation(&mut nums);
+
+            assert_eq!(nums.as_slice(), expected);
+        }
+    }
+}
