60756c5deb007528232cba2b1e42558ca5e4ac6c
EFanZh <efanzh@gmail.com>
2020-04-18 20:50:54 +0800

    Add problem 34: Find First and Last Position of Element in Sorted Array

---
+++ src/problem_0034_find_first_and_last_position_of_element_in_sorted_array/mod.rs
@@ -0,0 +1,23 @@
+pub mod binary_search;
+pub mod binary_search_2;
+
+pub trait Solution {
+    fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            ((&[5, 7, 7, 8, 8, 10] as &[_], 8), [3, 4]),
+            ((&[5, 7, 7, 8, 8, 10], 6), [-1, -1]),
+            ((&[1, 2, 3] as &[_], 3), [2, 2]),
+        ];
+
+        for ((nums, target), expected) in test_cases.iter().copied() {
+            assert_eq!(S::search_range(nums.to_owned(), target), expected);
+        }
+    }
+}
