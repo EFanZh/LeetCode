114bc7efacf99ea8aafdbf186296030342f5c0f0
EFanZh <efanzh@gmail.com>
2020-04-20 21:23:46 +0800

    Add problem 29: Divide Two Integers

---
+++ src/problem_0029_divide_two_integers/exponential_check.rs
@@ -0,0 +1,60 @@
+pub struct Solution {}
+
+use std::iter;
+
+impl Solution {
+    fn divide_negative(mut dividend: i32, divisor: i32) -> i32 {
+        let mut result = 0;
+
+        while dividend <= divisor {
+            dividend -= divisor;
+            result -= 1;
+
+            for (x, i) in iter::successors(divisor.checked_add(divisor).map(|x| (x, -2)), |(x, i)| {
+                x.checked_add(*x).map(|x| (x, i + i))
+            }) {
+                if dividend <= x {
+                    dividend -= x;
+                    result += i;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+
+    pub fn divide(dividend: i32, divisor: i32) -> i32 {
+        if divisor == 0 || (dividend == i32::min_value() && divisor == -1) {
+            i32::max_value()
+        } else if dividend < 0 {
+            if divisor < 0 {
+                -Self::divide_negative(dividend, divisor)
+            } else {
+                Self::divide_negative(dividend, -divisor)
+            }
+        } else if divisor < 0 {
+            Self::divide_negative(-dividend, divisor)
+        } else {
+            -Self::divide_negative(-dividend, -divisor)
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn divide(dividend: i32, divisor: i32) -> i32 {
+        Self::divide(dividend, divisor)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
