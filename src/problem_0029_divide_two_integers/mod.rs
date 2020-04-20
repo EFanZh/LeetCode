c73e07cbf9192763466efe854d434b0cc429afef
EFanZh <efanzh@gmail.com>
2020-04-20 21:23:46 +0800

    Add problem 29: Divide Two Integers

---
+++ src/problem_0029_divide_two_integers/mod.rs
@@ -0,0 +1,23 @@
+pub mod exponential_check;
+
+pub trait Solution {
+    fn divide(dividend: i32, divisor: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            ((10, 3), 3),
+            ((7, -3), -2),
+            ((1_100_540_749, -1_090_366_779), -1),
+            ((2_147_483_647, 1), 2_147_483_647),
+        ];
+
+        for ((dividend, divisor), expected) in test_cases.iter().copied() {
+            assert_eq!(S::divide(dividend, divisor), expected);
+        }
+    }
+}
