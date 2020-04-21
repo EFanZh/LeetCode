dae8317b783eee3c75177e2bf37bdb6c5591fde1
EFanZh <efanzh@gmail.com>
2020-04-21 21:56:25 +0800

    Add problem 36: Valid Sudoku

---
+++ src/problem_0036_valid_sudoku/mod.rs
@@ -0,0 +1,50 @@
+pub mod naive;
+pub mod single_pass;
+
+pub trait Solution {
+    fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [
+            (
+                [
+                    "53..7....",
+                    "6..195...",
+                    ".98....6.",
+                    "8...6...3",
+                    "4..8.3..1",
+                    "7...2...6",
+                    ".6....28.",
+                    "...419..5",
+                    "....8..79",
+                ],
+                true,
+            ),
+            (
+                [
+                    "83..7....",
+                    "6..195...",
+                    ".98....6.",
+                    "8...6...3",
+                    "4..8.3..1",
+                    "7...2...6",
+                    ".6....28.",
+                    "...419..5",
+                    "....8..79",
+                ],
+                false,
+            ),
+        ];
+
+        for (board, expected) in test_cases.iter().copied() {
+            let result = S::is_valid_sudoku(board.iter().map(|row| row.chars().collect()).collect());
+
+            assert_eq!(result, expected);
+        }
+    }
+}
---
+++ src/problem_0036_valid_sudoku/naive.rs
@@ -0,0 +1,66 @@
+pub struct Solution {}
+
+impl Solution {
+    fn has_duplication<I: IntoIterator<Item = char>>(iter: I) -> bool {
+        let mut count = [false; 9];
+
+        for slot in iter {
+            if let Some(digit) = slot.to_digit(10) {
+                let target = &mut count[digit as usize - 1];
+
+                if *target {
+                    return true;
+                } else {
+                    *target = true;
+                }
+            }
+        }
+
+        false
+    }
+
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        for row in &board {
+            if Self::has_duplication(row.iter().copied()) {
+                return false;
+            }
+        }
+
+        for column in (0..9).map(|i| board.iter().map(move |row| row[i])) {
+            if Self::has_duplication(column) {
+                return false;
+            }
+        }
+
+        for block_row in (0..9).step_by(3) {
+            for block_column in (0..9).step_by(3) {
+                if Self::has_duplication(
+                    board[block_row..block_row + 3]
+                        .iter()
+                        .flat_map(|row| row[block_column..block_column + 3].iter().copied()),
+                ) {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        Self::is_valid_sudoku(board)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
