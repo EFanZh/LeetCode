8ab38504abdcc37ac5740fd078dd1084b5c12f94
EFanZh <efanzh@gmail.com>
2020-04-22 21:23:11 +0800

    Add problem 37: Sudoku Solver

---
+++ src/problem_0037_sudoku_solver/backtracking_1.rs
@@ -0,0 +1,57 @@
+pub struct Solution {}
+
+impl Solution {
+    fn solve_sudoku_helper(board: &mut [Vec<char>], i: usize) -> Result<(), ()> {
+        if i == 81 {
+            Err(())
+        } else {
+            let row = i / 9;
+            let column = i % 9;
+
+            if board[row][column] == '.' {
+                let block_row = row / 3 * 3;
+                let block_column = column / 3 * 3;
+
+                for num in (b'1'..=b'9').map(char::from) {
+                    if !board[row].contains(&num)
+                        && (0..9).all(|i| board[i][column] != num)
+                        && board[block_row..block_row + 3]
+                            .iter()
+                            .all(|r| !r[block_column..block_column + 3].contains(&num))
+                    {
+                        board[row][column] = num;
+
+                        Self::solve_sudoku_helper(board, i + 1)?;
+                    }
+                }
+
+                board[row][column] = '.';
+
+                Ok(())
+            } else {
+                Self::solve_sudoku_helper(board, i + 1)
+            }
+        }
+    }
+
+    pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+        Self::solve_sudoku_helper(board, 0).unwrap_err();
+    }
+}
+
+impl super::Solution for Solution {
+    fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+        Self::solve_sudoku(board)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::tests::run_tests;
+    use super::Solution;
+
+    #[test]
+    fn test_solution() {
+        run_tests::<Solution>();
+    }
+}
