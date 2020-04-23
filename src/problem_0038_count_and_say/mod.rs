5d207235683cfa68f9006fcd5f882f8a9fcee238
EFanZh <efanzh@gmail.com>
2020-04-23 21:45:58 +0800

    Add problem 38: Count and Say

---
+++ src/problem_0038_count_and_say/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn count_and_say(n: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run_tests<S: Solution>() {
+        let test_cases = [(1, "1"), (2, "11"), (3, "21"), (4, "1211"), (5, "111221")];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::count_and_say(n), expected);
+        }
+    }
+}
