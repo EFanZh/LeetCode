500dca1ddd78b39011f574719285bbf5c279c935
EFanZh <efanzh@gmail.com>
2020-04-28 21:50:17 +0800

    Add two pointers solution to problem 42

---
+++ src/problem_0042_trapping_rain_water/two_pointers.rs
@@ -0,0 +1,57 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn trap(height: Vec<i32>) -> i32 {
+        let mut result = 0;
+
+        if let Some((mut left, rest)) = height.split_first() {
+            if let Some((mut right, mut rest)) = rest.split_last() {
+                'outer: loop {
+                    if left < right {
+                        while let Some((middle, new_rest)) = rest.split_first() {
+                            rest = new_rest;
+
+                            if middle < left {
+                                result += left - middle;
+                            } else {
+                                left = middle;
+
+                                continue 'outer;
+                            }
+                        }
+                    } else {
+                        while let Some((middle, new_rest)) = rest.split_last() {
+                            rest = new_rest;
+
+                            if middle < right {
+                                result += right - middle;
+                            } else {
+                                right = middle;
+
+                                continue 'outer;
+                            }
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn trap(height: Vec<i32>) -> i32 {
+        Self::trap(height)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
