a82be717d02bb7fe1edd588cc42eea80242d7475
EFanZh <efanzh@gmail.com>
2020-04-30 20:37:28 +0800

    Add problem 46: Permutations

---
+++ src/problem_0046_permutations/backtracking.rs
@@ -0,0 +1,49 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    fn permute_helper(nums: &mut [i32], base: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
+        if let Some((first, rest)) = nums.split_first_mut() {
+            base.push(*first);
+
+            Self::permute_helper(rest, base, result);
+
+            for i in 0..rest.len() {
+                mem::swap(first, &mut rest[i]);
+
+                *base.last_mut().unwrap() = *first;
+                Self::permute_helper(rest, base, result);
+
+                mem::swap(first, &mut rest[i]);
+            }
+
+            base.pop();
+        } else {
+            result.push(base.clone());
+        }
+    }
+
+    fn permute(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+        let nums_len = nums.len();
+
+        Self::permute_helper(&mut nums, &mut Vec::with_capacity(nums_len), &mut result);
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        Self::permute(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0046_permutations/heap.rs
@@ -0,0 +1,54 @@
+pub struct Solution {}
+
+impl Solution {
+    fn permute_helper_even(nums: &mut [i32], k: usize, result: &mut Vec<Vec<i32>>) {
+        if k == 0 {
+            result.push(nums.to_vec());
+        } else {
+            Self::permute_helper_odd(nums, k - 1, result);
+
+            for i in 0..k - 1 {
+                nums.swap(i, k - 1);
+
+                Self::permute_helper_odd(nums, k - 1, result);
+            }
+        }
+    }
+
+    fn permute_helper_odd(nums: &mut [i32], k: usize, result: &mut Vec<Vec<i32>>) {
+        Self::permute_helper_even(nums, k - 1, result);
+
+        for _ in 0..k - 1 {
+            nums.swap(0, k - 1);
+
+            Self::permute_helper_even(nums, k - 1, result);
+        }
+    }
+
+    fn permute(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+        let nums_len = nums.len();
+
+        if nums.len() % 2 == 0 {
+            Self::permute_helper_even(&mut nums, nums_len, &mut result);
+        } else {
+            Self::permute_helper_odd(&mut nums, nums_len, &mut result);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        Self::permute(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
