b80dc463bad5ec1b103aa7a423935fe1a85ac44b
EFanZh <efanzh@gmail.com>
2020-05-01 11:34:30 +0800

    Update solution to problem 31: Next Permutation

--- src/problem_0031_next_permutation/find_last_inversion.rs
+++ src/problem_0031_next_permutation/find_last_inversion.rs
@@ -3,24 +3,26 @@ pub struct Solution {}
 use std::cmp::Ordering;
 
 impl Solution {
-    pub fn next_permutation(nums: &mut Vec<i32>) {
-        if let Some((i, (current, _))) = nums
-            .iter()
-            .zip(&nums[1..])
-            .enumerate()
-            .rfind(|(_, (current, next))| current < next)
-        {
-            let j = i + nums[i + 1..]
-                .binary_search_by(|x| match current.cmp(x) {
-                    Ordering::Less => Ordering::Less,
-                    _ => Ordering::Greater,
-                })
-                .unwrap_err();
+    pub fn next_permutation(nums: &mut [i32]) {
+        if let Some((_, rest)) = nums.split_first() {
+            if let Some((i, (current, _))) = nums
+                .iter()
+                .zip(rest)
+                .enumerate()
+                .rfind(|(_, (current, next))| current < next)
+            {
+                let j = nums[i + 2..]
+                    .binary_search_by(|x| match current.cmp(x) {
+                        Ordering::Less => Ordering::Less,
+                        _ => Ordering::Greater,
+                    })
+                    .unwrap_err();
 
-            nums.swap(i, j);
-            nums[i + 1..].reverse();
-        } else {
-            nums.reverse();
+                nums.swap(i, i + 1 + j);
+                nums[i + 1..].reverse();
+            } else {
+                nums.reverse();
+            }
         }
     }
 }
