423a821d19d78585de94a3039f0643bfd4acaa81
EFanZh <efanzh@gmail.com>
2020-05-02 19:38:21 +0800

    Add problem 30: Substring with Concatenation of All Words

---
+++ src/problem_0030_substring_with_concatenation_of_all_words/brute_force.rs
@@ -0,0 +1,49 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn find_substring(s: String, mut words: Vec<String>) -> Vec<i32> {
+        if words.is_empty() {
+            return vec![];
+        }
+
+        let word_length = words[0].len();
+        let num_words = words.len();
+        let window_length = word_length * num_words;
+
+        words.sort_unstable_by(|lhs, rhs| lhs.as_bytes().cmp(rhs.as_bytes()));
+
+        s.as_bytes()
+            .windows(window_length)
+            .enumerate()
+            .filter_map(move |(i, window)| {
+                let mut window_words = window.chunks(word_length).collect::<Box<_>>();
+
+                window_words.sort_unstable();
+
+                if window_words
+                    .iter()
+                    .map(AsRef::as_ref)
+                    .eq(words.iter().map(String::as_bytes))
+                {
+                    Some(i as _)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        Self::find_substring(s, words)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0030_substring_with_concatenation_of_all_words/interleaving_sliding_window_sets.rs
@@ -0,0 +1,124 @@
+pub struct Solution {}
+
+use std::borrow::Borrow;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::iter::FromIterator;
+
+#[derive(PartialEq)]
+struct HashMultiSet<T: Hash + Eq> {
+    data: HashMap<T, usize>,
+}
+
+impl<T: Hash + Eq> HashMultiSet<T> {
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            data: HashMap::with_capacity(capacity),
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        match self.data.entry(value) {
+            Entry::Occupied(entry) => *entry.into_mut() += 1,
+            Entry::Vacant(entry) => {
+                entry.insert(1);
+            }
+        }
+    }
+
+    fn remove<Q: Hash + Eq + ?Sized>(&mut self, value: &Q) -> bool
+    where
+        T: Borrow<Q>,
+    {
+        if let Some(count) = self.data.get_mut(value) {
+            if *count == 1 {
+                self.data.remove(value);
+            } else {
+                *count -= 1;
+            }
+
+            true
+        } else {
+            false
+        }
+    }
+
+    fn clear(&mut self) {
+        self.data.clear();
+    }
+}
+
+impl<T: Eq + Hash> FromIterator<T> for HashMultiSet<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut result = Self { data: HashMap::new() };
+
+        result.extend(iter);
+
+        result
+    }
+}
+
+impl<T: Eq + Hash> Extend<T> for HashMultiSet<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            self.insert(item)
+        }
+    }
+}
+
+impl Solution {
+    pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        let s = s.into_bytes();
+        let word_length = words.first().map_or(0, String::len);
+        let num_words = words.len();
+        let window_length = word_length * num_words;
+        let mut result = Vec::new();
+
+        if let Some(last_split) = s.len().checked_sub(window_length) {
+            let words = words.iter().map(String::as_bytes).collect::<HashMultiSet<_>>();
+            let mut words_cache = HashMultiSet::with_capacity(num_words);
+
+            for (i, chunks) in (0..word_length.min(last_split + 1))
+                .map(|start| s[start..].chunks_exact(word_length))
+                .enumerate()
+            {
+                words_cache.extend(chunks.clone().take(num_words));
+
+                if words_cache == words {
+                    result.push(i as _);
+                }
+
+                let current_chunks = chunks.clone().skip(num_words);
+
+                for (j, (previous, current)) in (i + word_length..).step_by(word_length).zip(chunks.zip(current_chunks))
+                {
+                    words_cache.remove(previous);
+                    words_cache.insert(current);
+
+                    if words_cache == words {
+                        result.push(j as _);
+                    }
+                }
+
+                words_cache.clear();
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        Self::find_substring(s, words)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
