83025b4c0ae9e730143f12233c08021114da2bc7
EFanZh <efanzh@gmail.com>
2020-05-06 20:40:28 +0800

    Add problem 51: N-Queens

---
+++ src/problem_0051_n_queens/mod.rs
@@ -0,0 +1,24 @@
+pub mod backtracking;
+
+pub trait Solution {
+    fn solve_n_queens(n: i32) -> Vec<Vec<String>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(
+            4,
+            &[
+                &[".Q..", "...Q", "Q...", "..Q."] as &[_],
+                &["..Q.", "Q...", "...Q", ".Q.."],
+            ] as &[_],
+        )];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::solve_n_queens(n), expected);
+        }
+    }
+}
