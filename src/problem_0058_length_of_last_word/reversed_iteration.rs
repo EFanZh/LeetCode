2a7cd6a8eb4bd3c967e594c413919453c3cbb5e5
EFanZh <efanzh@gmail.com>
2020-05-13 20:45:12 +0800

    Add problem 58: Length of Last Word

---
+++ src/problem_0058_length_of_last_word/reversed_iteration.rs
@@ -0,0 +1,26 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.into_bytes()
+            .into_iter()
+            .rev()
+            .skip_while(|c| *c == b' ')
+            .take_while(|c| *c != b' ')
+            .count() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn length_of_last_word(s: String) -> i32 {
+        Self::length_of_last_word(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
