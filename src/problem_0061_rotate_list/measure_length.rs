54518d252539e250052461f37f13b3a353e56399
EFanZh <efanzh@gmail.com>
2020-05-16 07:02:03 +0800

    Add problem 61: Rotate List

---
+++ src/problem_0061_rotate_list/measure_length.rs
@@ -0,0 +1,50 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+use std::iter;
+
+impl Solution {
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        let length = iter::successors(head.as_deref(), |node| node.next.as_deref()).count();
+        let k = (k as usize).checked_rem(length).unwrap_or(0);
+
+        let mut rest = {
+            let mut maybe_node = &mut head;
+
+            for _ in 0..(length - k) {
+                maybe_node = &mut maybe_node.as_deref_mut().unwrap().next;
+            }
+
+            maybe_node.take()
+        };
+
+        let rest_tail = {
+            let mut maybe_node = &mut rest;
+
+            while let Some(node) = maybe_node {
+                maybe_node = &mut node.next;
+            }
+
+            maybe_node
+        };
+
+        *rest_tail = head;
+
+        rest
+    }
+}
+
+impl super::Solution for Solution {
+    fn rotate_right(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        Self::rotate_right(head, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
