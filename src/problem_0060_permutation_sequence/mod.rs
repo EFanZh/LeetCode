b92230b79275ba17f6800aed283efe783d66fe19
EFanZh <efanzh@gmail.com>
2020-05-15 20:47:53 +0800

    Add problem 60: Permutation Sequence

---
+++ src/problem_0060_permutation_sequence/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn get_permutation(n: i32, k: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [((3, 3), "213"), ((4, 9), "2314"), ((1, 1), "1")];
+
+        for ((n, k), expected) in test_cases.iter().copied() {
+            assert_eq!(S::get_permutation(n, k), expected);
+        }
+    }
+}
