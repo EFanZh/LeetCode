0c1dd04284f5950efa5bfc4cc89daf164356d3ab
EFanZh <efanzh@gmail.com>
2020-05-22 07:02:17 +0800

    Add problem 67: Add Binary

---
+++ src/problem_0067_add_binary/mod.rs
@@ -0,0 +1,18 @@
+pub mod naive;
+
+pub trait Solution {
+    fn add_binary(a: String, b: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(("11", "1"), "100"), (("1010", "1011"), "10101")];
+
+        for ((a, b), expected) in test_cases.iter().copied() {
+            assert_eq!(S::add_binary(a.to_string(), b.to_string()), expected);
+        }
+    }
+}
---
+++ src/problem_0067_add_binary/naive.rs
@@ -0,0 +1,56 @@
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    pub fn add_binary(mut a: String, mut b: String) -> String {
+        if b.len() < a.len() {
+            mem::swap(&mut a, &mut b);
+        }
+
+        let a_len = a.len();
+        let mut b = b.into_bytes();
+        let split = b.len() - a_len;
+        let (b_left, b_right) = b.split_at_mut(split);
+        let mut carry = false;
+
+        for (target, source) in b_right.iter_mut().zip(a.into_bytes()).rev() {
+            *target += source - b'0' + u8::from(carry);
+            carry = *target > b'1';
+
+            if carry {
+                *target -= 2;
+            }
+        }
+
+        if carry {
+            for target in b_left.iter_mut().rev() {
+                if *target == b'0' {
+                    *target = b'1';
+
+                    return String::from_utf8(b).unwrap();
+                } else {
+                    *target = b'0';
+                }
+            }
+
+            b.insert(0, b'1');
+        }
+
+        String::from_utf8(b).unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn add_binary(a: String, b: String) -> String {
+        Self::add_binary(a, b)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
