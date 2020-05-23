71df57f34ee7fd963d1fbd12c1630f8265955fd6
EFanZh <efanzh@gmail.com>
2020-05-23 20:28:11 +0800

    Add problem 68: Text Justification

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,7 @@ pub mod problem_0064_minimum_path_sum;
 pub mod problem_0065_valid_number;
 pub mod problem_0066_plus_one;
 pub mod problem_0067_add_binary;
+pub mod problem_0068_text_justification;
 pub mod problem_0072_edit_distance;
 pub mod problem_0075_sort_colors;
 pub mod problem_0089_gray_code;
---
+++ src/problem_0068_text_justification/mod.rs
@@ -0,0 +1,67 @@
+pub mod iterative;
+pub mod recursive;
+
+pub trait Solution {
+    fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &["This", "is", "an", "example", "of", "text", "justification."] as &[_],
+                    16,
+                ),
+                &["This    is    an", "example  of text", "justification.  "] as &[_],
+            ),
+            (
+                (&["What", "must", "be", "acknowledgment", "shall", "be"], 16),
+                &["What   must   be", "acknowledgment  ", "shall be        "],
+            ),
+            (
+                (
+                    &[
+                        "Science",
+                        "is",
+                        "what",
+                        "we",
+                        "understand",
+                        "well",
+                        "enough",
+                        "to",
+                        "explain",
+                        "to",
+                        "a",
+                        "computer.",
+                        "Art",
+                        "is",
+                        "everything",
+                        "else",
+                        "we",
+                        "do",
+                    ],
+                    20,
+                ),
+                &[
+                    "Science  is  what we",
+                    "understand      well",
+                    "enough to explain to",
+                    "a  computer.  Art is",
+                    "everything  else  we",
+                    "do                  ",
+                ],
+            ),
+        ];
+
+        for ((words, max_width), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::full_justify(words.iter().map(ToString::to_string).collect(), max_width),
+                expected
+            );
+        }
+    }
+}
