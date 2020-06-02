c1bef537798d26a41707a02bf1e6ce8ec2f15c92
EFanZh <efanzh@gmail.com>
2020-06-03 06:49:50 +0800

    Add problem 79: Word Search

---
+++ src/problem_0079_word_search/mod.rs
@@ -0,0 +1,48 @@
+pub mod recursive;
+
+pub trait Solution {
+    fn exist(board: Vec<Vec<char>>, word: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[
+                        &['A', 'B', 'C', 'E'] as &[_],
+                        &['S', 'F', 'C', 'S'],
+                        &['A', 'D', 'E', 'E'],
+                    ] as &[&[_]],
+                    "ABCCED",
+                ),
+                true,
+            ),
+            (
+                (
+                    &[&['A', 'B', 'C', 'E'], &['S', 'F', 'C', 'S'], &['A', 'D', 'E', 'E']],
+                    "SEE",
+                ),
+                true,
+            ),
+            (
+                (
+                    &[&['A', 'B', 'C', 'E'], &['S', 'F', 'C', 'S'], &['A', 'D', 'E', 'E']],
+                    "ABCB",
+                ),
+                false,
+            ),
+            ((&[&['a']], "a"), true),
+        ];
+
+        for ((board, word), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::exist(board.iter().map(|row| row.to_vec()).collect(), word.to_string()),
+                expected
+            );
+        }
+    }
+}
