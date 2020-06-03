4a7737d73ad6fca7ffb6747cddbba68827ef8872
EFanZh <efanzh@gmail.com>
2020-06-04 06:56:06 +0800

    Add problem 80: Remove Duplicates from Sorted Array II

---
+++ src/problem_0080_remove_duplicates_from_sorted_array_ii/recursive.rs
@@ -0,0 +1,55 @@
+pub struct Solution {}
+
+impl Solution {
+    fn remove_duplicates_single(nums: &mut [i32], deduped: usize, prev: i32, i: usize) -> usize {
+        if let Some(current) = nums.get(i).copied() {
+            nums.swap(deduped, i);
+
+            if current == prev {
+                Self::remove_duplicates_double(nums, deduped + 1, prev, i + 1)
+            } else {
+                Self::remove_duplicates_single(nums, deduped + 1, current, i + 1)
+            }
+        } else {
+            deduped
+        }
+    }
+
+    fn remove_duplicates_double(nums: &mut [i32], deduped: usize, prev: i32, i: usize) -> usize {
+        if let Some(current) = nums.get(i).copied() {
+            if current == prev {
+                Self::remove_duplicates_double(nums, deduped, prev, i + 1)
+            } else {
+                nums.swap(deduped, i);
+
+                Self::remove_duplicates_single(nums, deduped + 1, current, i + 1)
+            }
+        } else {
+            deduped
+        }
+    }
+
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if let Some(first) = nums.first().copied() {
+            let new_length = Self::remove_duplicates_single(nums, 1, first, 1);
+
+            nums.truncate(new_length);
+        }
+
+        nums.len() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        Self::remove_duplicates(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
