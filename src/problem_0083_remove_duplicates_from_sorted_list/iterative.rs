25bc5f7256616c19b8dd3ca86b10ec6e0f52e1da
EFanZh <efanzh@gmail.com>
2020-06-07 10:03:16 +0800

    Add problem 83: Remove Duplicates from Sorted List

---
+++ src/problem_0083_remove_duplicates_from_sorted_list/iterative.rs
@@ -0,0 +1,36 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        head.map(|mut node| {
+            let mut tail = node.as_mut();
+            let mut head = tail.next.take();
+
+            while let Some(mut node) = head {
+                head = node.next.take();
+
+                if node.val != tail.val {
+                    tail = tail.next.get_or_insert(node);
+                }
+            }
+
+            node
+        })
+    }
+}
+
+impl super::Solution for Solution {
+    fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::delete_duplicates(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0083_remove_duplicates_from_sorted_list/recursive.rs
@@ -0,0 +1,41 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+impl Solution {
+    fn delete_duplicates_helper(tail: &mut ListNode, head: Option<Box<ListNode>>) {
+        if let Some(mut node) = head {
+            if node.val == tail.val {
+                Self::delete_duplicates_helper(tail, node.next)
+            } else {
+                let next = node.next.take();
+
+                Self::delete_duplicates_helper(tail.next.get_or_insert(node), next)
+            }
+        }
+    }
+
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        head.map(|mut node| {
+            let next = node.next.take();
+
+            Self::delete_duplicates_helper(node.as_mut(), next);
+
+            node
+        })
+    }
+}
+
+impl super::Solution for Solution {
+    fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::delete_duplicates(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
