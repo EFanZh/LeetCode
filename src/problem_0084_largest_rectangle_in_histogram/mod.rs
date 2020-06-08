8a1d3688bba68a7c58067024f8fdf02782bcc249
EFanZh <efanzh@gmail.com>
2020-06-08 21:06:39 +0800

    Add problem 84: Largest Rectangle in Histogram

---
+++ src/problem_0084_largest_rectangle_in_histogram/mod.rs
@@ -0,0 +1,30 @@
+pub mod stack;
+pub mod stack_2;
+
+pub trait Solution {
+    fn largest_rectangle_area(heights: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[2, 1, 5, 6, 2, 3] as &[_], 10),
+            (&[], 0),
+            (&[4], 4),
+            (&[0], 0),
+            (&[0, 0], 0),
+            (&[0, 2], 2),
+            (&[2, 0], 2),
+            (&[2, 3], 4),
+            (&[3, 2], 4),
+            (&[3, 2], 4),
+        ];
+
+        for (heights, expected) in test_cases.iter().copied() {
+            assert_eq!(S::largest_rectangle_area(heights.to_vec()), expected);
+        }
+    }
+}
