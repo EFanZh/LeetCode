94402b82c5931af2b029ed51a8f856b6b161e32b
EFanZh <efanzh@gmail.com>
2020-06-11 21:11:16 +0800

    Add a new iterative solution to problem 78: Subsets

---
+++ src/problem_0078_subsets/iterative_2.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut result = vec![Vec::new()];
+
+        result.reserve((1 << nums.len()) - 1);
+
+        for num in nums {
+            for i in 0..result.len() {
+                let mut item = result[i].clone();
+
+                item.push(num);
+
+                result.push(item);
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        Self::subsets(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
