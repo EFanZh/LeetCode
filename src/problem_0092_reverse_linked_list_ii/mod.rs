f75ff4ba54d5ddd270b57699e73669052274862c
EFanZh <efanzh@gmail.com>
2020-06-15 06:59:59 +0800

    Add problem 92: Reverse Linked List II

---
+++ src/problem_0092_reverse_linked_list_ii/mod.rs
@@ -0,0 +1,34 @@
+use super::data_structures::ListNode;
+
+pub mod iterative;
+
+pub trait Solution {
+    fn reverse_between(head: Option<Box<ListNode>>, m: i32, n: i32) -> Option<Box<ListNode>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5] as &[_], 2, 4), &[1, 4, 3, 2, 5] as &[_]),
+            ((&[1, 2, 3, 4, 5], 3, 4), &[1, 2, 4, 3, 5]),
+        ];
+
+        for ((head, m, n), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                test_utilities::iter_list(&S::reverse_between(
+                    test_utilities::make_list(head.iter().copied()),
+                    m,
+                    n
+                ))
+                .copied()
+                .collect::<Box<_>>()
+                .as_ref(),
+                expected
+            );
+        }
+    }
+}
