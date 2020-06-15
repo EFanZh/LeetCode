f25e49ef47b5ecaf77606db47373af117c68082b
EFanZh <efanzh@gmail.com>
2020-06-16 06:59:39 +0800

    Add problem 93: Restore IP Addresses

---
+++ src/problem_0093_restore_ip_addresses/mod.rs
@@ -0,0 +1,22 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn restore_ip_addresses(s: String) -> Vec<String>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("25525511135", &["255.255.11.135", "255.255.111.35"] as &[_]),
+            ("0000", &["0.0.0.0"]),
+            ("010010", &["0.10.0.10", "0.100.1.0"]),
+        ];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::restore_ip_addresses(s.to_string()), expected);
+        }
+    }
+}
