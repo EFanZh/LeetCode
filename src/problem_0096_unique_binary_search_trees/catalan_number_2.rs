aca8a305679325b979941973b66c48d7aee40535
EFanZh <efanzh@gmail.com>
2020-06-18 07:06:02 +0800

    Add problem 96: Unique Binary Search Trees

---
+++ src/problem_0096_unique_binary_search_trees/catalan_number_2.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn num_trees(n: i32) -> i32 {
+        // https://en.wikipedia.org/wiki/Catalan_number.
+
+        if n < 2 {
+            1
+        } else {
+            let n = i64::from(n);
+            let mut result = n + 2;
+
+            for i in 2..n {
+                result *= n + i + 1;
+                result /= i
+            }
+
+            (result / n) as _
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn num_trees(n: i32) -> i32 {
+        Self::num_trees(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0096_unique_binary_search_trees/mod.rs
@@ -0,0 +1,41 @@
+pub mod catalan_number;
+pub mod catalan_number_2;
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn num_trees(n: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (0, 1),
+            (1, 1),
+            (2, 2),
+            (3, 5),
+            (4, 14),
+            (5, 42),
+            (6, 132),
+            (7, 429),
+            (8, 1_430),
+            (9, 4_862),
+            (10, 16_796),
+            (11, 58_786),
+            (12, 208_012),
+            (13, 742_900),
+            (14, 2_674_440),
+            (15, 9_694_845),
+            (16, 35_357_670),
+            (17, 129_644_790),
+            (18, 477_638_700),
+            (19, 1_767_263_190),
+        ];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::num_trees(n), expected);
+        }
+    }
+}
