f13354ed6f1df03818904820591f71ab4133a990
EFanZh <efanzh@gmail.com>
2020-06-22 07:09:41 +0800

    Add problem 101: Symmetric Tree

---
+++ src/problem_0101_symmetric_tree/mod.rs
@@ -0,0 +1,32 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[Some(1), Some(2), Some(2), Some(3), Some(4), Some(4), Some(3)] as &[_],
+                true,
+            ),
+            (&[Some(1), Some(2), Some(2), None, Some(3), None, Some(3)], false),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::is_symmetric(test_utilities::make_tree(root.iter().copied()),),
+                expected
+            );
+        }
+    }
+}
