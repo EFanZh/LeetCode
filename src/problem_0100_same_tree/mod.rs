3901e97c0b9c9f1a8aab10f4a57a00968108f878
EFanZh <efanzh@gmail.com>
2020-06-21 09:05:38 +0800

    Add problem 100: Same Tree

---
+++ src/problem_0100_same_tree/mod.rs
@@ -0,0 +1,39 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod cheating;
+pub mod recursive;
+
+pub trait Solution {
+    fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[Some(1), Some(2), Some(3)] as &[_],
+                    &[Some(1), Some(2), Some(3)] as &[_],
+                ),
+                true,
+            ),
+            ((&[Some(1), Some(2)], &[Some(1), None, Some(2)]), false),
+        ];
+
+        for ((p, q), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::is_same_tree(
+                    test_utilities::make_tree(p.iter().copied()),
+                    test_utilities::make_tree(q.iter().copied())
+                ),
+                expected
+            );
+        }
+    }
+}
