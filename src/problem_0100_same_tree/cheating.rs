19adfc5ef4e8edf2f58861cef14dd13cafa0fafc
EFanZh <efanzh@gmail.com>
2020-06-21 09:05:38 +0800

    Add problem 100: Same Tree

---
+++ src/problem_0100_same_tree/cheating.rs
@@ -0,0 +1,26 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        p == q
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Self::is_same_tree(p, q)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0100_same_tree/recursive.rs
@@ -0,0 +1,41 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn is_same_tree_helper(p: Option<&RefCell<TreeNode>>, q: Option<&RefCell<TreeNode>>) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(p), Some(q)) => {
+                let p = p.borrow();
+                let q = q.borrow();
+
+                p.val == q.val
+                    && Self::is_same_tree_helper(p.left.as_deref(), q.left.as_deref())
+                    && Self::is_same_tree_helper(p.right.as_deref(), q.right.as_deref())
+            }
+            _ => false,
+        }
+    }
+
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Self::is_same_tree_helper(p.as_deref(), q.as_deref())
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Self::is_same_tree(p, q)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
