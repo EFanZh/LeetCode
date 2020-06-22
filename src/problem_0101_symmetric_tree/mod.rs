f854924a760ffc9b3677bcc2d5418d95900a4ad3
EFanZh <efanzh@gmail.com>
2020-06-23 06:49:20 +0800

    Remove extra comma

--- src/problem_0101_symmetric_tree/mod.rs
+++ src/problem_0101_symmetric_tree/mod.rs
@@ -24,7 +24,7 @@ mod tests {
 
         for (root, expected) in test_cases.iter().copied() {
             assert_eq!(
-                S::is_symmetric(test_utilities::make_tree(root.iter().copied()),),
+                S::is_symmetric(test_utilities::make_tree(root.iter().copied())),
                 expected
             );
         }
