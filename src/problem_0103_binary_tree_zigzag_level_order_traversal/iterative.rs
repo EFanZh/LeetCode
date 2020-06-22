9438cc17d8ab81107fed1e8e7eb6cd7bcb5f60b0
EFanZh <efanzh@gmail.com>
2020-06-23 06:47:51 +0800

    Add problem 103: Binary Tree Zigzag Level Order Traversal

---
+++ src/problem_0103_binary_tree_zigzag_level_order_traversal/iterative.rs
@@ -0,0 +1,86 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::iter;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+
+        if let Some(node) = root {
+            let mut queue = iter::once(node).collect::<VecDeque<_>>();
+
+            loop {
+                {
+                    let mut level = Vec::with_capacity(queue.len());
+
+                    for _ in 0..queue.len() {
+                        let node = queue.pop_front().unwrap();
+                        let node = node.borrow();
+
+                        level.push(node.val);
+
+                        if let Some(left) = node.left.clone() {
+                            queue.push_back(left);
+                        }
+
+                        if let Some(right) = node.right.clone() {
+                            queue.push_back(right);
+                        }
+                    }
+
+                    result.push(level);
+                }
+
+                if queue.is_empty() {
+                    break;
+                }
+
+                {
+                    let mut level = Vec::with_capacity(queue.len());
+
+                    for _ in 0..queue.len() {
+                        let node = queue.pop_back().unwrap();
+                        let node = node.borrow();
+
+                        level.push(node.val);
+
+                        if let Some(right) = node.right.clone() {
+                            queue.push_front(right);
+                        }
+
+                        if let Some(left) = node.left.clone() {
+                            queue.push_front(left);
+                        }
+                    }
+
+                    result.push(level);
+                }
+
+                if queue.is_empty() {
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        Self::zigzag_level_order(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0103_binary_tree_zigzag_level_order_traversal/mod.rs
@@ -0,0 +1,29 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod iterative;
+
+pub trait Solution {
+    fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(
+            &[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)] as &[_],
+            &[&[3] as &[_], &[20, 9], &[15, 7]] as &[_],
+        )];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::zigzag_level_order(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
