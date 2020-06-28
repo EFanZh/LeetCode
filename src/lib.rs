9278a4a2895338c63c30b883ef4d10132ea25b9c
EFanZh <efanzh@gmail.com>
2020-06-29 07:01:21 +0800

    Add problem 108: Convert Sorted Array to Binary Search Tree

--- src/lib.rs
+++ src/lib.rs
@@ -106,6 +106,7 @@ pub mod problem_0104_maximum_depth_of_binary_tree;
 pub mod problem_0105_construct_binary_tree_from_preorder_and_inorder_traversal;
 pub mod problem_0106_construct_binary_tree_from_inorder_and_postorder_traversal;
 pub mod problem_0107_binary_tree_level_order_traversal_ii;
+pub mod problem_0108_convert_sorted_array_to_binary_search_tree;
 pub mod problem_0136_single_number;
 pub mod problem_0144_binary_tree_preorder_traversal;
 pub mod problem_0145_binary_tree_postorder_traversal;
---
+++ src/problem_0108_convert_sorted_array_to_binary_search_tree/recursive.rs
@@ -0,0 +1,42 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn sorted_array_to_bst_helper(nums: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        if nums.is_empty() {
+            None
+        } else {
+            let middle = nums.len() / 2;
+            let (left, rest) = nums.split_at(middle);
+            let (&val, right) = rest.split_first().unwrap();
+
+            Some(Rc::new(RefCell::new(TreeNode {
+                val,
+                left: Self::sorted_array_to_bst_helper(left),
+                right: Self::sorted_array_to_bst_helper(right),
+            })))
+        }
+    }
+
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::sorted_array_to_bst_helper(&nums)
+    }
+}
+
+impl super::Solution for Solution {
+    fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::sorted_array_to_bst(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
