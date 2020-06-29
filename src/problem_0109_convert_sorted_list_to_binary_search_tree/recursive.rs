0bfed95b8c213f14e9c3530384298a1e7620fa5e
EFanZh <efanzh@gmail.com>
2020-06-30 06:55:52 +0800

    Add problem 109: Convert Sorted List to Binary Search Tree

---
+++ src/problem_0109_convert_sorted_list_to_binary_search_tree/recursive.rs
@@ -0,0 +1,46 @@
+use super::super::data_structures::{ListNode, TreeNode};
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::iter;
+use std::rc::Rc;
+
+impl Solution {
+    fn sorted_list_to_bst_helper<I: Iterator<Item = i32>>(
+        values: &mut I,
+        length: usize,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if length == 0 {
+            None
+        } else {
+            let half = length / 2;
+            let left = Self::sorted_list_to_bst_helper(values, half);
+            let val = values.next().unwrap();
+            let right = Self::sorted_list_to_bst_helper(values, length - half - 1);
+
+            Some(Rc::new(RefCell::new(TreeNode { val, left, right })))
+        }
+    }
+
+    pub fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let iterator = iter::successors(head.as_deref(), |node| node.next.as_deref());
+        let length = iterator.clone().count();
+
+        Self::sorted_list_to_bst_helper(&mut iterator.map(|node| node.val), length)
+    }
+}
+
+impl super::Solution for Solution {
+    fn sorted_list_to_bst(head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::sorted_list_to_bst(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
