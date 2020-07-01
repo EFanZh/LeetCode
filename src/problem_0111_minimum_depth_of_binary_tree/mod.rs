0a1bf2e80c43a95e244629728a49a45c471cfce1
EFanZh <efanzh@gmail.com>
2020-07-02 07:08:02 +0800

    Add problem 111: Minimum Depth of Binary Tree

---
+++ src/problem_0111_minimum_depth_of_binary_tree/mod.rs
@@ -0,0 +1,23 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn min_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)] as &[_], 2)];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(S::min_depth(test_utilities::make_tree(root.iter().copied())), expected);
+        }
+    }
+}
