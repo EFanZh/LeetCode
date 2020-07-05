63e7084318ed65f13d4a31eac26d6831097d0273
EFanZh <efanzh@gmail.com>
2020-07-05 13:55:58 +0800

    Add problem 114: Flatten Binary Tree to Linked List

---
+++ src/problem_0114_flatten_binary_tree_to_linked_list/iterative.rs
@@ -0,0 +1,56 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(mut prev) = root.clone() {
+            let (mut current, right) = {
+                let mut perv = prev.borrow_mut();
+
+                (perv.left.take(), perv.right.take())
+            };
+
+            let mut stack = vec![right];
+
+            loop {
+                if let Some(node) = current {
+                    let new_prev = prev.borrow_mut().right.get_or_insert(node).clone();
+
+                    prev = new_prev;
+
+                    let (left, right) = {
+                        let mut prev = prev.borrow_mut();
+
+                        (prev.left.take(), prev.right.take())
+                    };
+
+                    stack.push(right);
+
+                    current = left;
+                } else if let Some(right) = stack.pop() {
+                    current = right;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+        Self::flatten(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
