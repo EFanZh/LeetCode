49a2b0499dc3f67dc77898ba6b95c5a1d5f96d04
EFanZh <efanzh@gmail.com>
2020-07-06 07:05:21 +0800

    Add problem 118: Pascal's Triangle

---
+++ src/problem_0118_pascals_triangle/mod.rs
@@ -0,0 +1,21 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn generate(num_rows: i32) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(
+            5,
+            &[&[1] as &[_], &[1, 1], &[1, 2, 1], &[1, 3, 3, 1], &[1, 4, 6, 4, 1]] as &[&[_]],
+        )];
+
+        for (num_rows, expected) in test_cases.iter().copied() {
+            assert_eq!(S::generate(num_rows), expected);
+        }
+    }
+}
