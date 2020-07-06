ca57e3b7bc86937697bcd8fb530acf1633075460
EFanZh <efanzh@gmail.com>
2020-07-07 06:58:02 +0800

    Add problem 119: Pascal's Triangle II

---
+++ src/problem_0119_pascals_triangle_ii/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn get_row(row_index: i32) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(0, &[1] as &[_]), (3, &[1, 3, 3, 1]), (4, &[1, 4, 6, 4, 1])];
+
+        for (num_rows, expected) in test_cases.iter().copied() {
+            assert_eq!(S::get_row(num_rows), expected);
+        }
+    }
+}
