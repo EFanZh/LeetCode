ceb3c6c321e618c0e8f414ef0410b0e945bf67fa
EFanZh <efanzh@gmail.com>
2020-07-11 06:33:12 +0800

    Add problem 122: Best Time to Buy and Sell Stock II

---
+++ src/problem_0122_best_time_to_buy_and_sell_stock_ii/mod.rs
@@ -0,0 +1,22 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn max_profit(prices: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[7, 1, 5, 3, 6, 4] as &[_], 7),
+            (&[1, 2, 3, 4, 5], 4),
+            (&[7, 6, 4, 3, 1], 0),
+        ];
+
+        for (prices, expected) in test_cases.iter().copied() {
+            assert_eq!(S::max_profit(prices.to_vec()), expected);
+        }
+    }
+}
