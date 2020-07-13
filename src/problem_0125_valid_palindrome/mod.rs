95f1e3c9375c3fe6fdc41f944d26919a36dfa01e
EFanZh <efanzh@gmail.com>
2020-07-14 07:05:17 +0800

    Add problem 125: Valid Palindrome

---
+++ src/problem_0125_valid_palindrome/mod.rs
@@ -0,0 +1,22 @@
+pub mod bidirectional_search;
+
+pub trait Solution {
+    fn is_palindrome(s: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("A man, a plan, a canal: Panama", true),
+            ("race a car", false),
+            ("0P", false),
+        ];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_palindrome(s.to_string()), expected);
+        }
+    }
+}
