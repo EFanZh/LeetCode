c73ecdaee0fd57263f49ffc000e9fa6549759a50
EFanZh <efanzh@gmail.com>
2020-07-15 07:29:52 +0800

    Add problem 137: Single Number II

---
+++ src/problem_0137_single_number_ii/dynamic_programming.rs
@@ -0,0 +1,33 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn single_number(nums: Vec<i32>) -> i32 {
+        let mut bits_1 = 0;
+        let mut bits_2 = 0;
+
+        for num in nums {
+            // 00 => 01 => 01.
+            // 01 => 00 => 10.
+            // 10 => 10 => 00.
+
+            bits_1 = (bits_1 ^ num) & !bits_2;
+            bits_2 = (bits_2 ^ num) & !bits_1;
+        }
+
+        bits_1
+    }
+}
+
+impl super::Solution for Solution {
+    fn single_number(nums: Vec<i32>) -> i32 {
+        Self::single_number(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
