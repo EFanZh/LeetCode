63d99373845ba2d0d3b8791bcd54e256932c38df
EFanZh <efanzh@gmail.com>
2020-07-16 07:07:34 +0800

    Add problem 127: Word Ladder

---
+++ src/problem_0127_word_ladder/bfs.rs
@@ -0,0 +1,76 @@
+pub struct Solution {}
+
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::iter;
+
+impl Solution {
+    pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
+        let mut graph = HashMap::new();
+
+        // Build graph.
+
+        for word in word_list.iter().map(String::as_bytes) {
+            for i in 0..word.len() {
+                let key = (&word[..i], &word[i + 1..]);
+
+                match graph.entry(key) {
+                    Entry::Vacant(entry) => {
+                        entry.insert(vec![word]);
+                    }
+                    Entry::Occupied(entry) => {
+                        entry.into_mut().push(word);
+                    }
+                }
+            }
+        }
+
+        // BFS.
+
+        let mut queue = iter::once(begin_word.as_bytes()).collect::<VecDeque<_>>();
+        let mut visited = iter::once(begin_word.as_bytes()).collect::<HashSet<_>>();
+        let mut length = 1;
+
+        loop {
+            for _ in 0..queue.len() {
+                let current = queue.pop_front().unwrap();
+
+                if current == end_word.as_bytes() {
+                    return length;
+                } else {
+                    for i in 0..current.len() {
+                        if let Some(nexts) = graph.get(&(&current[..i], &current[i + 1..])) {
+                            for next in nexts {
+                                if visited.insert(next) {
+                                    queue.push_back(next);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if queue.is_empty() {
+                break;
+            } else {
+                length += 1;
+            }
+        }
+
+        0
+    }
+}
+
+impl super::Solution for Solution {
+    fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
+        Self::ladder_length(begin_word, end_word, word_list)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0127_word_ladder/mod.rs
@@ -0,0 +1,30 @@
+pub mod bfs;
+pub mod bidirectional_bfs;
+pub mod naive_bfs;
+
+pub trait Solution {
+    fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("hit", "cog", &["hot", "dot", "dog", "lot", "log", "cog"] as &[_]), 5),
+            (("hit", "cog", &["hot", "dot", "dog", "lot", "log"]), 0),
+        ];
+
+        for ((begin_word, end_word, word_list), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::ladder_length(
+                    begin_word.to_string(),
+                    end_word.to_string(),
+                    word_list.iter().map(ToString::to_string).collect()
+                ),
+                expected
+            );
+        }
+    }
+}
