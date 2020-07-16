15b34739728c861112ebff21865d13d9d6130cb6
EFanZh <efanzh@gmail.com>
2020-07-17 06:59:12 +0800

    Add problem 129: Sum Root to Leaf Numbers

---
+++ src/problem_0129_sum_root_to_leaf_numbers/mod.rs
@@ -0,0 +1,29 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[Some(1), Some(2), Some(3)] as &[_], 25),
+            (&[Some(4), Some(9), Some(0), Some(5), Some(1)] as &[_], 1026),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::sum_numbers(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0129_sum_root_to_leaf_numbers/recursive.rs
@@ -0,0 +1,43 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn sum_numbers_helper(root: &TreeNode, base: i32) -> i32 {
+        let base = base * 10 + root.val;
+
+        match (
+            root.left.as_deref().map(RefCell::borrow),
+            root.right.as_deref().map(RefCell::borrow),
+        ) {
+            (None, None) => base,
+            (None, Some(child)) | (Some(child), None) => Self::sum_numbers_helper(&child, base),
+            (Some(left), Some(right)) => Self::sum_numbers_helper(&left, base) + Self::sum_numbers_helper(&right, base),
+        }
+    }
+
+    pub fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(root) = root {
+            Self::sum_numbers_helper(&root.borrow(), 0)
+        } else {
+            0
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn sum_numbers(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::sum_numbers(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
