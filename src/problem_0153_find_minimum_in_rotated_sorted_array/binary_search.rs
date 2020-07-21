27382190deb027cce6f8c25b4367b706048b30ae
EFanZh <efanzh@gmail.com>
2020-07-22 06:59:15 +0800

    Add problem 153: Find Minimum in Rotated Sorted Array

---
+++ src/problem_0153_find_minimum_in_rotated_sorted_array/binary_search.rs
@@ -0,0 +1,29 @@
+pub struct Solution {}
+
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let first = nums[0];
+
+        let pivot = nums
+            .binary_search_by(|x| if *x < first { Ordering::Greater } else { Ordering::Less })
+            .unwrap_err();
+
+        nums[pivot % nums.len()]
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_min(nums: Vec<i32>) -> i32 {
+        Self::find_min(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0153_find_minimum_in_rotated_sorted_array/mod.rs
@@ -0,0 +1,18 @@
+pub mod binary_search;
+
+pub trait Solution {
+    fn find_min(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[3, 4, 5, 1, 2] as &[_], 1), (&[4, 5, 6, 7, 0, 1, 2], 0)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_min(nums.to_vec()), expected);
+        }
+    }
+}
