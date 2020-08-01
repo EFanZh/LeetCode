b9122f018b6ab3d2cf2ce2153f14bc52ef32a635
EFanZh <efanzh@gmail.com>
2020-08-01 08:19:52 +0800

    Add problem 143: Reorder List

---
+++ src/problem_0143_reorder_list/mod.rs
@@ -0,0 +1,31 @@
+use super::data_structures::ListNode;
+
+pub mod iterative;
+
+pub trait Solution {
+    fn reorder_list(head: &mut Option<Box<ListNode>>);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[1, 2, 3, 4] as &[_], &[1, 4, 2, 3] as &[_]),
+            (&[1, 2, 3, 4, 5], &[1, 5, 2, 4, 3]),
+        ];
+
+        for (head, expected) in test_cases.iter().copied() {
+            let mut head = test_utilities::make_list(head.iter().copied());
+
+            S::reorder_list(&mut head);
+
+            assert_eq!(
+                test_utilities::iter_list(&head).copied().collect::<Box<_>>().as_ref(),
+                expected
+            );
+        }
+    }
+}
