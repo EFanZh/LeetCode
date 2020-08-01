3c6add6ed426290ed3218e0385486731f8cbf13e
EFanZh <efanzh@gmail.com>
2020-08-02 07:43:24 +0800

    Add problem 146: LRU Cache

---
+++ src/problem_0146_lru_cache/map_to_doubly_linked_list.rs
@@ -0,0 +1,165 @@
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::mem;
+
+const INVALID_HANDLE: usize = usize::max_value();
+
+struct CacheEntry {
+    key: i32,
+    value: i32,
+    prev: usize,
+    next: usize,
+}
+
+struct LRUCache {
+    map: HashMap<i32, usize>,
+    memory: Vec<CacheEntry>,
+    newest: usize,
+    oldest: usize,
+}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        Self {
+            map: HashMap::new(),
+            memory: Vec::with_capacity(capacity as _),
+            newest: INVALID_HANDLE,
+            oldest: INVALID_HANDLE,
+        }
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        if let Some(&handle) = self.map.get(&key) {
+            let cache_entry = &mut self.memory[handle];
+            let result = cache_entry.value;
+
+            if cache_entry.prev != INVALID_HANDLE {
+                // Remove the entry.
+
+                let old_prev = mem::replace(&mut cache_entry.prev, INVALID_HANDLE);
+                let old_next = mem::replace(&mut cache_entry.next, self.newest);
+
+                self.memory[old_prev].next = old_next;
+
+                if let Some(next) = self.memory.get_mut(old_next) {
+                    next.prev = old_prev;
+                } else {
+                    self.oldest = old_prev;
+                }
+
+                // Insert to newest.
+
+                self.memory[self.newest].prev = handle;
+                self.newest = handle;
+            }
+
+            result
+        } else {
+            -1
+        }
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        let length = self.map.len();
+
+        match self.map.entry(key) {
+            Entry::Occupied(entry) => {
+                let handle = *entry.get();
+                let cache_entry = &mut self.memory[handle];
+
+                cache_entry.value = value;
+
+                if cache_entry.prev != INVALID_HANDLE {
+                    // Remove the entry.
+
+                    let old_prev = mem::replace(&mut cache_entry.prev, INVALID_HANDLE);
+                    let old_next = mem::replace(&mut cache_entry.next, self.newest);
+
+                    self.memory[old_prev].next = old_next;
+
+                    if let Some(next) = self.memory.get_mut(old_next) {
+                        next.prev = old_prev;
+                    } else {
+                        self.oldest = old_prev;
+                    }
+
+                    // Insert to newest.
+
+                    self.memory[self.newest].prev = handle;
+                    self.newest = handle;
+                }
+            }
+            Entry::Vacant(entry) => {
+                if length == self.memory.capacity() {
+                    let handle = self.oldest;
+
+                    // Remove old entry.
+
+                    let cache_entry = &mut self.memory[handle];
+
+                    entry.insert(handle);
+
+                    self.map.remove(&mem::replace(&mut cache_entry.key, key));
+                    cache_entry.value = value;
+
+                    let old_prev = mem::replace(&mut cache_entry.prev, INVALID_HANDLE);
+
+                    cache_entry.next = self.newest;
+
+                    if let Some(prev) = self.memory.get_mut(old_prev) {
+                        prev.next = INVALID_HANDLE;
+                        self.oldest = old_prev;
+                    }
+
+                    // Insert to newest.
+
+                    self.memory[self.newest].prev = handle;
+                    self.newest = handle;
+                } else {
+                    let handle = length;
+
+                    self.memory.push(CacheEntry {
+                        key,
+                        value,
+                        prev: INVALID_HANDLE,
+                        next: self.newest,
+                    });
+
+                    if let Some(newest) = self.memory.get_mut(self.newest) {
+                        newest.prev = handle;
+                    }
+
+                    self.newest = handle;
+
+                    if self.oldest == INVALID_HANDLE {
+                        self.oldest = handle;
+                    }
+
+                    entry.insert(handle);
+                }
+            }
+        }
+    }
+}
+
+impl super::LRUCache for LRUCache {
+    fn new(capacity: i32) -> Self {
+        Self::new(capacity)
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        self.get(key)
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        self.put(key, value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::LRUCache>();
+    }
+}
---
+++ src/problem_0146_lru_cache/map_to_doubly_linked_list_3.rs
@@ -0,0 +1,128 @@
+use std::cell::Cell;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::mem;
+use std::rc::{Rc, Weak};
+
+struct Node {
+    key: Cell<i32>,
+    value: Cell<i32>,
+    prev: Cell<Weak<Node>>,
+    next: Cell<Weak<Node>>,
+}
+
+struct LRUCache {
+    capacity: usize,
+    map: HashMap<i32, Rc<Node>>,
+    newest: Weak<Node>,
+    oldest: Weak<Node>,
+}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        Self {
+            capacity: capacity as _,
+            map: HashMap::new(),
+            newest: Weak::new(),
+            oldest: Weak::new(),
+        }
+    }
+
+    fn refresh(node: &Rc<Node>, newest: &mut Weak<Node>, oldest: &mut Weak<Node>) {
+        if let Some(prev) = node.prev.take().upgrade() {
+            newest.upgrade().unwrap().prev.set(Rc::downgrade(node));
+
+            let next = node.next.replace(mem::replace(newest, Rc::downgrade(node)));
+
+            if let Some(next) = next.upgrade() {
+                next.prev.set(Rc::downgrade(&prev));
+            } else {
+                *oldest = Rc::downgrade(&prev);
+            }
+
+            prev.next.set(next);
+        }
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        if let Some(node) = self.map.get(&key) {
+            Self::refresh(node, &mut self.newest, &mut self.oldest);
+
+            node.value.get()
+        } else {
+            -1
+        }
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        let length = self.map.len();
+
+        match self.map.entry(key) {
+            Entry::Occupied(entry) => {
+                let node = entry.get();
+
+                node.value.set(value);
+
+                Self::refresh(node, &mut self.newest, &mut self.oldest);
+            }
+            Entry::Vacant(entry) => {
+                if length == self.capacity {
+                    let node = self.oldest.upgrade().unwrap();
+                    let old_key = node.key.replace(key);
+
+                    node.value.set(value);
+
+                    if let Some(prev) = node.prev.take().upgrade() {
+                        self.newest.upgrade().unwrap().prev.set(Rc::downgrade(&node));
+
+                        node.next.set(mem::replace(&mut self.newest, Rc::downgrade(&node)));
+
+                        self.oldest = Rc::downgrade(&prev);
+
+                        prev.next.take();
+                    }
+
+                    entry.insert(node);
+                    self.map.remove(&old_key);
+                } else {
+                    let node = Rc::new(Node {
+                        key: Cell::new(key),
+                        value: Cell::new(value),
+                        prev: Cell::new(Weak::new()),
+                        next: Cell::new(self.newest.clone()),
+                    });
+
+                    if let Some(next) = mem::replace(&mut self.newest, Rc::downgrade(&node)).upgrade() {
+                        next.prev.set(Rc::downgrade(&node));
+                    } else {
+                        self.oldest = Rc::downgrade(&node);
+                    }
+
+                    entry.insert(node);
+                }
+            }
+        }
+    }
+}
+
+impl super::LRUCache for LRUCache {
+    fn new(capacity: i32) -> Self {
+        Self::new(capacity)
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        self.get(key)
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        self.put(key, value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::LRUCache>();
+    }
+}
