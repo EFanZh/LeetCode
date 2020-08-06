99902a5997e9efc942af67135eec23ebad3497c9
EFanZh <efanzh@gmail.com>
2020-08-07 07:01:42 +0800

    Add problem 149: Max Points on a Line

---
+++ src/problem_0149_max_points_on_a_line/mod.rs
@@ -0,0 +1,21 @@
+pub mod brute_force;
+
+pub trait Solution {
+    fn max_points(points: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[[1, 1], [2, 2], [3, 3]] as &[_], 3),
+            (&[[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]], 4),
+        ];
+
+        for (points, expected) in test_cases.iter().copied() {
+            assert_eq!(S::max_points(points.iter().map(|p| p.to_vec()).collect()), expected);
+        }
+    }
+}
