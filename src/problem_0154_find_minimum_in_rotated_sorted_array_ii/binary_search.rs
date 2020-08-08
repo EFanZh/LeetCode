4b40aaaf5b86a8a9dbd52540288a1eef7c97d7cf
EFanZh <efanzh@gmail.com>
2020-08-08 12:38:16 +0800

    Add problem 154: Find Minimum in Rotated Sorted Array II

---
+++ src/problem_0154_find_minimum_in_rotated_sorted_array_ii/binary_search.rs
@@ -0,0 +1,41 @@
+pub struct Solution {}
+
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn find_min(mut nums: Vec<i32>) -> i32 {
+        let first = nums[0];
+
+        loop {
+            if let Some(last) = nums.last() {
+                if *last == first {
+                    nums.pop();
+                } else {
+                    break;
+                }
+            } else {
+                return first;
+            }
+        }
+
+        let pivot = nums
+            .binary_search_by(|x| if *x < first { Ordering::Greater } else { Ordering::Less })
+            .unwrap_err();
+
+        nums[pivot % nums.len()]
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_min(nums: Vec<i32>) -> i32 {
+        Self::find_min(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0154_find_minimum_in_rotated_sorted_array_ii/mod.rs
@@ -0,0 +1,18 @@
+pub mod binary_search;
+
+pub trait Solution {
+    fn find_min(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 3, 5] as &[_], 1), (&[2, 2, 2, 0, 1], 0)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_min(nums.to_vec()), expected);
+        }
+    }
+}
