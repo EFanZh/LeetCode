e069686aa2d65f846c3e0fea96b304cc2066b905
EFanZh <efanzh@gmail.com>
2020-08-10 07:39:21 +0800

    Add problem 162: Find Peak Element

---
+++ src/problem_0162_find_peak_element/binary_search.rs
@@ -0,0 +1,36 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        let mut start = 0;
+        let mut length = nums.len();
+
+        while length > 1 {
+            let half = length / 2;
+            let middle = start + half;
+
+            if nums[middle - 1] < nums[middle] {
+                start = middle;
+                length -= half;
+            } else {
+                length = half;
+            }
+        }
+
+        start as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_peak_element(nums: Vec<i32>) -> i32 {
+        Self::find_peak_element(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0162_find_peak_element/mod.rs
@@ -0,0 +1,18 @@
+pub mod binary_search;
+
+pub trait Solution {
+    fn find_peak_element(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 2, 3, 1] as &[_], &[2] as &[_]), (&[1, 2, 1, 3, 5, 6, 4], &[1, 5])];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert!(expected.contains(&S::find_peak_element(nums.to_vec())));
+        }
+    }
+}
