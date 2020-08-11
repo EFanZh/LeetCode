1e530d530d0dbb481bec6c05a97be3ef73a0b0f7
EFanZh <efanzh@gmail.com>
2020-08-11 08:06:51 +0800

    Add problem 165: Compare Version Numbers

---
+++ src/problem_0165_compare_version_numbers/mod.rs
@@ -0,0 +1,24 @@
+pub mod iterator;
+
+pub trait Solution {
+    fn compare_version(version1: String, version2: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("0.1", "1.1"), -1),
+            (("1.0.1", "1"), 1),
+            (("7.5.2.4", "7.5.3"), -1),
+            (("1.01", "1.001"), 0),
+            (("1.0", "1.0.0"), 0),
+        ];
+
+        for ((version1, version2), expected) in test_cases.iter().copied() {
+            assert_eq!(S::compare_version(version1.to_string(), version2.to_string()), expected);
+        }
+    }
+}
