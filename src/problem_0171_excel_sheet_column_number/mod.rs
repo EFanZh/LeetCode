7d47204ab9305dcf48e0e68596069e460d1ebe81
EFanZh <efanzh@gmail.com>
2020-08-14 08:09:38 +0800

    Add problem 171: Excel Sheet Column Number

---
+++ src/problem_0171_excel_sheet_column_number/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn title_to_number(s: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [("A", 1), ("AB", 28), ("ZY", 701)];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::title_to_number(s.to_string()), expected);
+        }
+    }
+}
