2e837224cf9859626e44960c3fa93b41ef993881
EFanZh <efanzh@gmail.com>
2020-08-14 08:09:38 +0800

    Add problem 171: Excel Sheet Column Number

---
+++ src/problem_0171_excel_sheet_column_number/iterative.rs
@@ -0,0 +1,28 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn title_to_number(s: String) -> i32 {
+        let mut result = 0;
+
+        for c in s.bytes() {
+            result *= 26;
+            result += i32::from(c - (b'A' - 1));
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn title_to_number(s: String) -> i32 {
+        Self::title_to_number(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
