8d5b547b0a3c08bf8d7582d0a7ad0e0d70e99daa
EFanZh <efanzh@gmail.com>
2020-08-15 09:16:58 +0800

    Add problem 172: Factorial Trailing Zeroes

--- src/lib.rs
+++ src/lib.rs
@@ -156,6 +156,7 @@ pub mod problem_0167_two_sum_ii_input_array_is_sorted;
 pub mod problem_0168_excel_sheet_column_title;
 pub mod problem_0169_majority_element;
 pub mod problem_0171_excel_sheet_column_number;
+pub mod problem_0172_factorial_trailing_zeroes;
 pub mod problem_0188_best_time_to_buy_and_sell_stock_iv;
 pub mod problem_0215_kth_largest_element_in_an_array;
 pub mod problem_0225_implement_stack_using_queues;
---
+++ src/problem_0172_factorial_trailing_zeroes/iterative_2.rs
@@ -0,0 +1,31 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn trailing_zeroes(mut n: i32) -> i32 {
+        // https://oeis.org/A027868
+
+        let mut result = 0;
+
+        while n != 0 {
+            n /= 5;
+
+            result += n;
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn trailing_zeroes(n: i32) -> i32 {
+        Self::trailing_zeroes(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
