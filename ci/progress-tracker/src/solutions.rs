2a97048b5302557e20e6901fb194bd70b0734dd0
EFanZh <efanzh@gmail.com>
2020-08-15 13:05:57 +0800

    Refactor progress tracker

--- ci/progress-tracker/src/solutions.rs
+++ ci/progress-tracker/src/solutions.rs
@@ -1,20 +1,20 @@
 use git2::{Tree, TreeWalkMode, TreeWalkResult};
 
-pub fn get<F: FnMut(&str, &str)>(tree: &Tree, mut f: F) {
+pub fn get(tree: &Tree, mut f: impl FnMut(&str, &str)) {
     const ROOT_PREFIX: &str = "src/problem_";
     const ROOT_POSTFIX: &str = "/";
-    const SOLUTION_POST_FIX: &str = ".rs";
+    const SOLUTION_POSTFIX: &str = ".rs";
 
     tree.walk(TreeWalkMode::PreOrder, |root, entry| {
         let name = entry.name().unwrap();
 
         if root.starts_with(ROOT_PREFIX)
             && root.ends_with(ROOT_POSTFIX)
-            && name.ends_with(SOLUTION_POST_FIX)
+            && name.ends_with(SOLUTION_POSTFIX)
             && name != "mod.rs"
         {
             let problem_id = &root[ROOT_PREFIX.len()..root.len() - ROOT_POSTFIX.len()];
-            let solution_id = &name[..name.len() - SOLUTION_POST_FIX.len()];
+            let solution_id = &name[..name.len() - SOLUTION_POSTFIX.len()];
 
             f(problem_id, solution_id);
         }
