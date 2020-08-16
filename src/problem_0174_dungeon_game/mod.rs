463506492770721b6663c2c961f802950ea61405
EFanZh <efanzh@gmail.com>
2020-08-17 07:34:24 +0800

    Add problem 174: Dungeon Game

---
+++ src/problem_0174_dungeon_game/mod.rs
@@ -0,0 +1,24 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[&[-2, -3, 3] as &[_], &[-5, -10, 1], &[10, 30, -5]] as &[&[_]], 7),
+            (&[&[1, -2, 3], &[2, -2, -2]], 2),
+        ];
+
+        for (dungeon, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::calculate_minimum_hp(dungeon.iter().map(|p| p.to_vec()).collect()),
+                expected
+            );
+        }
+    }
+}
