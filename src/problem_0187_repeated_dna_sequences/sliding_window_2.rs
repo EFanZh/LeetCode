46ebc56167bd2e6802dd75d7657ad8be7a92ba3c
EFanZh <efanzh@gmail.com>
2020-08-19 22:26:44 +0800

    Add problem 187: Repeated DNA Sequences

---
+++ src/problem_0187_repeated_dna_sequences/sliding_window_2.rs
@@ -0,0 +1,67 @@
+pub struct Solution {}
+
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
+impl Solution {
+    fn get_digit(c: u8) -> u32 {
+        match c {
+            b'A' => 0,
+            b'C' => 1,
+            b'G' => 2,
+            _ => 3,
+        }
+    }
+
+    pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+        let mut result = Vec::new();
+
+        if s.len() > 10 {
+            let mut counts = HashMap::new();
+            let mut key = 0;
+            let (first, rest) = s.split_at(9);
+
+            for c in first.bytes() {
+                key <<= 2;
+                key |= Self::get_digit(c);
+            }
+
+            for (i, c) in rest.bytes().enumerate() {
+                key &= (1 << 18) - 1;
+                key <<= 2;
+                key |= Self::get_digit(c);
+
+                match counts.entry(key) {
+                    Entry::Vacant(entry) => {
+                        entry.insert(false);
+                    }
+                    Entry::Occupied(entry) => {
+                        let count = entry.into_mut();
+
+                        if !*count {
+                            *count = true;
+
+                            result.push(s[i..i + 10].to_string());
+                        }
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+        Self::find_repeated_dna_sequences(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
