435d59fa427e385e3ecee0080a665187ed554bc6
EFanZh <efanzh@gmail.com>
2020-08-24 07:15:55 +0800

    Add problem 206: Reverse Linked List

---
+++ src/problem_0206_reverse_linked_list/iterative.rs
@@ -0,0 +1,32 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+use std::mem;
+
+impl Solution {
+    pub fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut result = None;
+
+        while let Some(mut node) = head {
+            head = mem::replace(&mut node.next, result);
+            result = Some(node);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::reverse_list(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
