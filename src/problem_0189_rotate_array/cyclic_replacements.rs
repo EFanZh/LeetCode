ff6d251b3f97c12be06e127ade44c22872b63c87
EFanZh <efanzh@gmail.com>
2020-08-23 21:22:09 +0800

    Add another solution to problem 189: Rotate Array

---
+++ src/problem_0189_rotate_array/cyclic_replacements.rs
@@ -0,0 +1,52 @@
+pub struct Solution {}
+
+impl Solution {
+    fn gcd(mut x: usize, mut y: usize) -> usize {
+        while y != 0 {
+            let next_y = x % y;
+
+            x = y;
+            y = next_y;
+        }
+
+        x
+    }
+
+    pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+        let length = nums.len();
+        let k = (k as usize) % length;
+        let gcd = Self::gcd(length, k);
+
+        for i in 0..gcd {
+            let mut next = i;
+
+            loop {
+                next = if next < length - k {
+                    next + k
+                } else {
+                    next.wrapping_add(k).wrapping_sub(length)
+                };
+
+                if next == i {
+                    break;
+                } else {
+                    nums.swap(i, next);
+                }
+            }
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn rotate(nums: &mut Vec<i32>, k: i32) {
+        Self::rotate(nums, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
