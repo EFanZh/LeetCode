2ac2022243146440a65b861fb87a29366878105a
EFanZh <efanzh@gmail.com>
2020-08-23 20:01:42 +0800

    Add problem 198: House Robber

---
+++ src/problem_0198_house_robber/dynamic_programming.rs
@@ -0,0 +1,32 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        let mut max_if_skip = 0;
+        let mut max_if_rob = 0;
+
+        for num in nums {
+            let new_max_if_skip = max_if_skip.max(max_if_rob);
+            let new_max_if_rob = max_if_skip + num;
+
+            max_if_skip = new_max_if_skip;
+            max_if_rob = new_max_if_rob;
+        }
+
+        max_if_skip.max(max_if_rob)
+    }
+}
+
+impl super::Solution for Solution {
+    fn rob(nums: Vec<i32>) -> i32 {
+        Self::rob(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
