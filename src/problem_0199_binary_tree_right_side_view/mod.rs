ba9b9c978afab1ac7d4ec05947a8edbdc5fcc46b
EFanZh <efanzh@gmail.com>
2020-08-25 07:11:05 +0800

    Add problem 199: Binary Tree Right Side View

---
+++ src/problem_0199_binary_tree_right_side_view/mod.rs
@@ -0,0 +1,29 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(
+            &[Some(1), Some(2), Some(3), None, Some(5), None, Some(4)] as &[_],
+            &[1, 3, 4] as &[_],
+        )];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::right_side_view(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0199_binary_tree_right_side_view/recursive.rs
@@ -0,0 +1,47 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution {}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn right_side_view_helper(root: Option<Rc<RefCell<TreeNode>>>, depth: usize, result: &mut Vec<i32>) {
+        if let Some(node) = root {
+            let (left, right, next_depth) = {
+                let node = node.borrow();
+
+                if depth >= result.len() {
+                    result.push(node.val);
+                }
+
+                (node.left.clone(), node.right.clone(), depth + 1)
+            };
+
+            Self::right_side_view_helper(right, next_depth, result);
+            Self::right_side_view_helper(left, next_depth, result);
+        }
+    }
+
+    pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut result = Vec::new();
+
+        Self::right_side_view_helper(root, 0, &mut result);
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        Self::right_side_view(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
