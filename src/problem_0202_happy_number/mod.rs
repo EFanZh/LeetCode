7288f4ba6187ff80ab26691c70269f8c649b5b78
EFanZh <efanzh@gmail.com>
2020-08-28 07:17:30 +0800

    Add problem 202: Happy Number

---
+++ src/problem_0202_happy_number/mod.rs
@@ -0,0 +1,19 @@
+pub mod hash_set;
+pub mod two_runners;
+
+pub trait Solution {
+    fn is_happy(n: i32) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(19, true)];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_happy(n), expected);
+        }
+    }
+}
