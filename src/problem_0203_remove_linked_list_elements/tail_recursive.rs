0d2eacab7a3af53134b5e88acbec3f1a06197659
EFanZh <efanzh@gmail.com>
2020-08-29 06:41:56 +0800

    Add problem 203: Remove Linked List Elements

---
+++ src/problem_0203_remove_linked_list_elements/tail_recursive.rs
@@ -0,0 +1,37 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+impl Solution {
+    fn remove_elements_helper(maybe_node: &mut Option<Box<ListNode>>, val: i32) {
+        if let Some(node) = maybe_node.as_deref_mut() {
+            if node.val == val {
+                *maybe_node = node.next.take();
+
+                Self::remove_elements_helper(maybe_node, val);
+            } else {
+                Self::remove_elements_helper(&mut node.next, val);
+            }
+        }
+    }
+
+    pub fn remove_elements(mut head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        Self::remove_elements_helper(&mut head, val);
+
+        head
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        Self::remove_elements(head, val)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
