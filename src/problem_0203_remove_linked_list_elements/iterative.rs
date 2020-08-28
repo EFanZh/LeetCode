f4b41b00c5b5754a2c174569470422a57ed35874
EFanZh <efanzh@gmail.com>
2020-08-29 06:41:56 +0800

    Add problem 203: Remove Linked List Elements

---
+++ src/problem_0203_remove_linked_list_elements/iterative.rs
@@ -0,0 +1,35 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution {}
+
+impl Solution {
+    fn remove_elements_helper(mut maybe_node: &mut Option<Box<ListNode>>, val: i32) {
+        while let Some(node) = maybe_node.as_deref_mut() {
+            if node.val == val {
+                *maybe_node = node.next.take();
+            } else {
+                maybe_node = &mut node.next;
+            }
+        }
+    }
+
+    pub fn remove_elements(mut head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        Self::remove_elements_helper(&mut head, val);
+
+        head
+    }
+}
+
+impl super::Solution for Solution {
+    fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>> {
+        Self::remove_elements(head, val)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0203_remove_linked_list_elements/mod.rs
@@ -0,0 +1,31 @@
+use super::data_structures::ListNode;
+
+// pub mod iterative; // TODO: One day this one will work.
+pub mod tail_recursive;
+
+pub trait Solution {
+    fn remove_elements(head: Option<Box<ListNode>>, val: i32) -> Option<Box<ListNode>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [((&[1, 2, 6, 3, 4, 5, 6] as &[_], 6), &[1, 2, 3, 4, 5] as &[_])];
+
+        for ((head, val), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                test_utilities::iter_list(&S::remove_elements(
+                    test_utilities::make_list(head.iter().copied()),
+                    val
+                ))
+                .copied()
+                .collect::<Box<_>>()
+                .as_ref(),
+                expected
+            );
+        }
+    }
+}
