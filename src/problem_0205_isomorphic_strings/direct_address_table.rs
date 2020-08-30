704d68d8b978cff27eae03d6409dd246ec626373
EFanZh <efanzh@gmail.com>
2020-08-31 07:10:34 +0800

    Add problem 205: Isomorphic Strings

---
+++ src/problem_0205_isomorphic_strings/direct_address_table.rs
@@ -0,0 +1,40 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        let mut mapping = [0; 256];
+        let mut used = [false; 256];
+
+        for (left, right) in s.bytes().zip(t.bytes()) {
+            let mapping_slot = &mut mapping[usize::from(left)];
+
+            if *mapping_slot == 0 {
+                if let used_slot @ false = &mut used[usize::from(right)] {
+                    *used_slot = true;
+                } else {
+                    return false;
+                }
+
+                *mapping_slot = right;
+            } else if *mapping_slot != right {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_isomorphic(s: String, t: String) -> bool {
+        Self::is_isomorphic(s, t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0205_isomorphic_strings/mod.rs
@@ -0,0 +1,24 @@
+pub mod direct_address_table;
+pub mod hash_map;
+
+pub trait Solution {
+    fn is_isomorphic(s: String, t: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("egg", "add"), true),
+            (("foo", "bar"), false),
+            (("paper", "title"), true),
+            (("ab", "aa"), false),
+        ];
+
+        for ((s, t), expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_isomorphic(s.to_string(), t.to_string()), expected);
+        }
+    }
+}
