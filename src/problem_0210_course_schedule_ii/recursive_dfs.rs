2950d2720e8f69099386e1c08ed8325934265d80
EFanZh <efanzh@gmail.com>
2020-09-04 07:30:24 +0800

    Add problem 210: Course Schedule II

---
+++ src/problem_0210_course_schedule_ii/recursive_dfs.rs
@@ -0,0 +1,64 @@
+pub struct Solution {}
+
+impl Solution {
+    fn is_acyclic(graph: &[Vec<i32>], node: i32, states: &mut [u8], result: &mut Vec<i32>) -> bool {
+        match &mut states[node as usize] {
+            state @ 0 => {
+                *state = 1;
+
+                for &next in &graph[node as usize] {
+                    if !Self::is_acyclic(graph, next, states, result) {
+                        return false;
+                    }
+                }
+
+                states[node as usize] = 2;
+
+                result.push(node);
+
+                true
+            }
+            1 => false,
+            _ => true,
+        }
+    }
+
+    pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut graph = vec![Vec::new(); num_courses as _];
+
+        for edge in prerequisites {
+            match edge.as_slice() {
+                [from, to] => {
+                    graph[*from as usize].push(*to);
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        let mut states = vec![0; num_courses as _];
+        let mut result = Vec::new();
+
+        for node in 0..num_courses {
+            if !Self::is_acyclic(&graph, node, &mut states, &mut result) {
+                result.clear();
+                break;
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
+        Self::find_order(num_courses, prerequisites)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
