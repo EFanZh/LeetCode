f378b3de43a5f0735088804a2f0091a38eda19d7
EFanZh <efanzh@gmail.com>
2020-09-07 07:20:43 +0800

    Add problem 217: Contains Duplicate

---
+++ src/problem_0217_contains_duplicate/hash_set.rs
@@ -0,0 +1,31 @@
+pub struct Solution {}
+
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut visited = HashSet::with_capacity(nums.len());
+
+        for num in nums {
+            if !visited.insert(num) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+impl super::Solution for Solution {
+    fn contains_duplicate(nums: Vec<i32>) -> bool {
+        Self::contains_duplicate(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
