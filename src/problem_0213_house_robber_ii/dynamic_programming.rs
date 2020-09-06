bac44fc36b8877109217e6bac74394e72762cd37
EFanZh <efanzh@gmail.com>
2020-09-06 22:34:51 +0800

    Add problem 213: House Robber II

---
+++ src/problem_0213_house_robber_ii/dynamic_programming.rs
@@ -0,0 +1,40 @@
+pub struct Solution {}
+
+impl Solution {
+    fn rob_helper(nums: &[i32]) -> i32 {
+        let mut prev = 0;
+        let mut current = 0;
+
+        for num in nums {
+            let new_current = current.max(prev + num);
+
+            prev = current;
+            current = new_current;
+        }
+
+        current
+    }
+
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        match nums.len() {
+            0 => 0,
+            1 => nums[0],
+            2 => nums[0].max(nums[1]),
+            _ => (nums[0] + Self::rob_helper(&nums[2..nums.len() - 1])).max(Self::rob_helper(&nums[1..])),
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn rob(nums: Vec<i32>) -> i32 {
+        Self::rob(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0213_house_robber_ii/mod.rs
@@ -0,0 +1,18 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn rob(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[2, 3, 2] as &[_], 3), (&[1, 2, 3, 1], 4)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::rob(nums.to_vec()), expected);
+        }
+    }
+}
