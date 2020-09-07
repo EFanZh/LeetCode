09baf4435d02c8f43c73dadba8c338d54cd59b6b
EFanZh <efanzh@gmail.com>
2020-09-07 22:50:18 +0800

    Add KMP solution to problem 28: Implement strStr()

---
+++ src/problem_0028_implement_strstr/kmp.rs
@@ -0,0 +1,71 @@
+pub struct Solution {}
+
+impl Solution {
+    fn compute_prefix_function(needle: &[u8]) -> Box<[usize]> {
+        let mut prefix_function = vec![0; needle.len()];
+        let mut j = 0_usize;
+
+        for (i, c) in needle.iter().copied().enumerate().skip(1) {
+            loop {
+                if needle[j] == c {
+                    j += 1;
+
+                    break;
+                } else if let Some(next_j) = prefix_function.get(j.wrapping_sub(1)) {
+                    j = *next_j;
+                } else {
+                    break;
+                }
+            }
+
+            prefix_function[i] = j;
+        }
+
+        prefix_function.into_boxed_slice()
+    }
+
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        let haystack = haystack.as_bytes();
+        let needle = needle.as_bytes();
+        let prefix_function = Self::compute_prefix_function(needle);
+        let mut i = 0;
+        let mut j = 0;
+
+        while let Some(mut c_2) = needle.get(j) {
+            if let Some(c) = haystack.get(i) {
+                loop {
+                    if c_2 == c {
+                        j += 1;
+
+                        break;
+                    } else if let Some(next_j) = prefix_function.get(j.wrapping_sub(1)) {
+                        j = *next_j;
+                        c_2 = &needle[j];
+                    } else {
+                        break;
+                    }
+                }
+
+                i += 1;
+            } else {
+                return -1;
+            }
+        }
+
+        (i - j) as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn str_str(haystack: String, needle: String) -> i32 {
+        Self::str_str(haystack, needle)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
