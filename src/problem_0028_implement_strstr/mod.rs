1089333f41831d98fa1843bcaeba4a1b6da7b278
EFanZh <efanzh@gmail.com>
2020-09-07 22:50:18 +0800

    Add KMP solution to problem 28: Implement strStr()

--- src/problem_0028_implement_strstr/mod.rs
+++ src/problem_0028_implement_strstr/mod.rs
@@ -1,4 +1,5 @@
 pub mod cheating;
+pub mod kmp;
 pub mod naive;
 
 pub trait Solution {
@@ -10,7 +11,13 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(("hello", "ll"), 2), (("aaaaa", "bba"), -1)];
+        let test_cases = [
+            (("hello", "ll"), 2),
+            (("aaaaa", "bba"), -1),
+            (("mississippi", "issip"), 4),
+            (("aabaaabaaac", "aabaaac"), 4),
+            (("", ""), 0),
+        ];
 
         for ((haystack, needle), expected) in test_cases.iter().copied() {
             assert_eq!(S::str_str(haystack.to_string(), needle.to_string()), expected);
