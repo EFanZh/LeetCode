100def311b3b6708912d89f908c87fefea777bcd
EFanZh <efanzh@gmail.com>
2020-09-11 07:06:37 +0800

    Add problem 226: Invert Binary Tree

---
+++ src/problem_0226_invert_binary_tree/recursive.rs
@@ -0,0 +1,40 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::mem;
+use std::rc::Rc;
+
+impl Solution {
+    fn invert_tree_helper(root: Option<&RefCell<TreeNode>>) {
+        if let Some(node) = root {
+            let node_ref = &mut *node.borrow_mut();
+
+            mem::swap(&mut node_ref.left, &mut node_ref.right);
+
+            Self::invert_tree_helper(node_ref.left.as_deref());
+            Self::invert_tree_helper(node_ref.right.as_deref());
+        }
+    }
+
+    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::invert_tree_helper(root.as_deref());
+
+        root
+    }
+}
+
+impl super::Solution for Solution {
+    fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::invert_tree(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
