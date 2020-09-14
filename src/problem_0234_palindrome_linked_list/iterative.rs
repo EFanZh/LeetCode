d3c4ae35731a8566bcb7323b0e2c31e0ac8b17ac
EFanZh <efanzh@gmail.com>
2020-09-15 07:16:54 +0800

    Add problem 234: Palindrome Linked List

---
+++ src/problem_0234_palindrome_linked_list/iterative.rs
@@ -0,0 +1,41 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution;
+
+use std::iter;
+use std::mem;
+
+impl Solution {
+    pub fn is_palindrome(mut head: Option<Box<ListNode>>) -> bool {
+        let length = iter::successors(head.as_deref(), |node| node.next.as_deref()).count();
+        let half = length / 2;
+        let mut reversed = None;
+
+        for _ in 0..half {
+            let mut node = head.unwrap();
+
+            head = mem::replace(&mut node.next, reversed);
+            reversed = Some(node);
+        }
+
+        if length % 2 != 0 {
+            head = head.unwrap().next;
+        }
+
+        reversed == head
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+        Self::is_palindrome(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0234_palindrome_linked_list/mod.rs
@@ -0,0 +1,24 @@
+use super::data_structures::ListNode;
+
+pub mod iterative;
+
+pub trait Solution {
+    fn is_palindrome(head: Option<Box<ListNode>>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 2] as &[_], false), (&[1, 2, 2, 1], true)];
+
+        for (head, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::is_palindrome(test_utilities::make_list(head.iter().copied())),
+                expected
+            );
+        }
+    }
+}
