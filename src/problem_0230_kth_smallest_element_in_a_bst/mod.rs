4f9489a62010bcd9cb5152881a6b059711828273
EFanZh <efanzh@gmail.com>
2020-09-16 07:01:24 +0800

    Add problem 230: Kth Smallest Element in a BST

---
+++ src/problem_0230_kth_smallest_element_in_a_bst/mod.rs
@@ -0,0 +1,33 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod iterative;
+pub mod recursive;
+
+pub trait Solution {
+    fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[Some(3), Some(1), Some(4), None, Some(2)] as &[_], 1), 1),
+            (
+                (&[Some(5), Some(3), Some(6), Some(2), Some(4), None, None, Some(1)], 3),
+                3,
+            ),
+        ];
+
+        for ((root, k), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::kth_smallest(test_utilities::make_tree(root.iter().copied()), k),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0230_kth_smallest_element_in_a_bst/recursive.rs
@@ -0,0 +1,42 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn kth_smallest_helper(root: Option<&RefCell<TreeNode>>, k: i32) -> Result<i32, i32> {
+        if let Some(node) = root {
+            let node = node.borrow();
+
+            Self::kth_smallest_helper(node.left.as_deref(), k).or_else(|x| {
+                if x == 0 {
+                    Ok(node.val)
+                } else {
+                    Self::kth_smallest_helper(node.right.as_deref(), x - 1)
+                }
+            })
+        } else {
+            Err(k)
+        }
+    }
+
+    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        Self::kth_smallest_helper(root.as_deref(), k - 1).unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        Self::kth_smallest(root, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
