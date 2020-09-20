2952dac7527e2f7737a0641961f3b87838347182
EFanZh <efanzh@gmail.com>
2020-09-21 06:51:52 +0800

    Add problem 258: Add Digits

---
+++ src/problem_0258_add_digits/mod.rs
@@ -0,0 +1,18 @@
+pub mod formula;
+
+pub trait Solution {
+    fn add_digits(num: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(38, 2)];
+
+        for (num, expected) in test_cases.iter().copied() {
+            assert_eq!(S::add_digits(num), expected);
+        }
+    }
+}
