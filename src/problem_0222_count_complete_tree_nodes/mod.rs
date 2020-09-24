59fb11a73d608926bf53c3fefa3d88e7708c092a
EFanZh <efanzh@gmail.com>
2020-09-24 08:14:29 +0800

    Add problem 222: Count Complete Tree Nodes

---
+++ src/problem_0222_count_complete_tree_nodes/mod.rs
@@ -0,0 +1,29 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[Some(1), Some(2), Some(3), Some(4), Some(5), Some(6)] as &[_], 6),
+            (&[Some(1), Some(2), Some(3)], 3),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::count_nodes(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
