45cd7b12a16fc95e4ff3324a31ac851e0efddc1f
EFanZh <efanzh@gmail.com>
2020-10-01 21:48:58 +0800

    Add problem 263: Ugly Number

--- src/lib.rs
+++ src/lib.rs
@@ -204,6 +204,7 @@ pub mod problem_0240_search_a_2d_matrix_ii;
 pub mod problem_0242_valid_anagram;
 pub mod problem_0257_binary_tree_paths;
 pub mod problem_0258_add_digits;
+pub mod problem_0263_ugly_number;
 pub mod problem_0274_h_index;
 pub mod problem_0275_h_index_ii;
 pub mod problem_0289_game_of_life;
