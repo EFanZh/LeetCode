12b9bb3c3796015edf48265d1637b485713d8f20
EFanZh <efanzh@gmail.com>
2020-10-03 07:36:25 +0800

    Add problem 227: Basic Calculator II

---
+++ src/problem_0227_basic_calculator_ii/iterative.rs
@@ -0,0 +1,82 @@
+pub struct Solution;
+
+use std::iter::Peekable;
+use std::mem;
+
+impl Solution {
+    fn whitespaces(iter: &mut Peekable<impl Iterator<Item = u8>>) {
+        while let Some(b' ') = iter.peek() {
+            iter.next();
+        }
+    }
+
+    fn op(iter: &mut Peekable<impl Iterator<Item = u8>>) -> Option<u8> {
+        Self::whitespaces(iter);
+
+        iter.next()
+    }
+
+    fn number(iter: &mut Peekable<impl Iterator<Item = u8>>) -> i32 {
+        Self::whitespaces(iter);
+
+        let mut num = i32::from(iter.next().unwrap() - b'0');
+
+        while let Some(&c @ b'0'..=b'9') = iter.peek() {
+            num *= 10;
+            num += i32::from(c - b'0');
+
+            iter.next();
+        }
+
+        num
+    }
+
+    pub fn calculate(s: String) -> i32 {
+        let mut iter = s.bytes().peekable();
+        let mut first = Self::number(&mut iter);
+
+        let (mut op, mut second) = loop {
+            match Self::op(&mut iter) {
+                Some(op @ b'+') | Some(op @ b'-') => break (op, Self::number(&mut iter)),
+                Some(b'*') => first *= Self::number(&mut iter),
+                Some(b'/') => first /= Self::number(&mut iter),
+                _ => return first,
+            }
+        };
+
+        loop {
+            match Self::op(&mut iter) {
+                Some(op_2 @ b'+') | Some(op_2 @ b'-') => {
+                    if mem::replace(&mut op, op_2) == b'+' {
+                        first += mem::replace(&mut second, Self::number(&mut iter));
+                    } else {
+                        first -= mem::replace(&mut second, Self::number(&mut iter));
+                    }
+                }
+                Some(b'*') => second *= Self::number(&mut iter),
+                Some(b'/') => second /= Self::number(&mut iter),
+                _ => break,
+            }
+        }
+
+        if op == b'+' {
+            first + second
+        } else {
+            first - second
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn calculate(s: String) -> i32 {
+        Self::calculate(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0227_basic_calculator_ii/mod.rs
@@ -0,0 +1,25 @@
+pub mod iterative;
+pub mod recursive_descent;
+pub mod stack;
+
+pub trait Solution {
+    fn calculate(s: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("3+2*2", 7),
+            (" 3/2 ", 1),
+            (" 3+5 / 2 ", 5),
+            ("1*2-3/4+5*6-7*8+9/10", -24),
+        ];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::calculate(s.to_string()), expected);
+        }
+    }
+}
