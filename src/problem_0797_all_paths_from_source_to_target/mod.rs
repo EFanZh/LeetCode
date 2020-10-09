ea8ead42296aa3b5c32da06486d8f47bb4fe9c23
EFanZh <efanzh@gmail.com>
2020-10-09 08:12:49 +0800

    Add problem 797: All Paths From Source to Target

---
+++ src/problem_0797_all_paths_from_source_to_target/mod.rs
@@ -0,0 +1,36 @@
+pub mod dfs;
+
+pub trait Solution {
+    fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[&[1, 2] as &[_], &[3], &[3], &[]] as &[&[_]],
+                &[&[0, 1, 3] as &[_], &[0, 2, 3]] as &[&[_]],
+            ),
+            (
+                &[&[4, 3, 1], &[3, 2, 4], &[3], &[4], &[]],
+                &[&[0, 1, 2, 3, 4], &[0, 1, 3, 4], &[0, 1, 4], &[0, 3, 4], &[0, 4]],
+            ),
+            (&[&[1], &[]], &[&[0, 1]]),
+            (&[&[1, 2, 3], &[2], &[3], &[]], &[&[0, 1, 2, 3], &[0, 2, 3], &[0, 3]]),
+            (&[&[1, 3], &[2], &[3], &[]], &[&[0, 1, 2, 3], &[0, 3]]),
+        ];
+
+        for (graph, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                test_utilities::unstable_sorted(S::all_paths_source_target(
+                    graph.iter().map(|nexts| nexts.to_vec()).collect()
+                )),
+                expected
+            );
+        }
+    }
+}
