15277b5e4b7740ad67499cdc28f1f188f6f7679c
EFanZh <efanzh@gmail.com>
2020-10-10 08:52:44 +0800

    Fix some clippy warnings

--- src/problem_1143_longest_common_subsequence/memoized_dynamic_programming_1.rs
+++ src/problem_1143_longest_common_subsequence/memoized_dynamic_programming_1.rs
@@ -4,10 +4,7 @@ impl Solution {
     fn helper<T: Eq>(text_1: &[T], text_2: &[T], cache: &mut [Option<i32>], columns: usize) -> i32 {
         let cache_index = columns * text_1.len() + text_2.len();
 
-        #[allow(clippy::option_if_let_else)]
-        if let Some(result) = cache[cache_index] {
-            result
-        } else {
+        cache[cache_index].unwrap_or_else(|| {
             let result = if let Some((tail_1, head_1)) = text_1.split_last() {
                 if let Some((tail_2, head_2)) = text_2.split_last() {
                     if tail_1 == tail_2 {
@@ -25,7 +22,7 @@ impl Solution {
             cache[cache_index] = Some(result);
 
             result
-        }
+        })
     }
 
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
