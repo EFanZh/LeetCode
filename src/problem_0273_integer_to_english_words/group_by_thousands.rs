ce7c44fddc3f674f43fc437df365c05485c088cf
EFanZh <efanzh@gmail.com>
2020-10-13 07:13:14 +0800

    Add problem 273: Integer to English Words

---
+++ src/problem_0273_integer_to_english_words/group_by_thousands.rs
@@ -0,0 +1,99 @@
+pub struct Solution;
+
+impl Solution {
+    fn less_than_thousand(num: i32, result: &mut String) {
+        const SINGLES: [&str; 19] = [
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Fifteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen",
+        ];
+
+        const TENS: [&str; 8] = [
+            "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety",
+        ];
+
+        match num {
+            1..=19 => result.push_str(SINGLES[(num - 1) as usize]),
+            20..=99 => {
+                result.push_str(TENS[(num / 10 - 2) as usize]);
+
+                let remainder = num % 10;
+
+                if remainder != 0 {
+                    result.push_str(" ");
+                    Self::less_than_thousand(remainder, result);
+                }
+            }
+            _ => {
+                Self::less_than_thousand(num / 100, result);
+
+                result.push_str(" Hundred");
+
+                let remainder = num % 100;
+
+                if remainder != 0 {
+                    result.push_str(" ");
+                    Self::less_than_thousand(remainder, result);
+                }
+            }
+        }
+    }
+
+    pub fn number_to_words(mut num: i32) -> String {
+        let mut result = String::new();
+
+        if num == 0 {
+            result.push_str("Zero");
+        } else {
+            for &(name, base) in &[("Billion", 1_000_000_000), ("Million", 1_000_000), ("Thousand", 1_000)] {
+                if num >= base {
+                    Self::less_than_thousand(num / base, &mut result);
+                    result.push_str(" ");
+                    result.push_str(name);
+
+                    num %= base;
+
+                    if num == 0 {
+                        return result;
+                    } else {
+                        result.push_str(" ");
+                    }
+                }
+            }
+
+            Self::less_than_thousand(num, &mut result);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn number_to_words(num: i32) -> String {
+        Self::number_to_words(num)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0273_integer_to_english_words/mod.rs
@@ -0,0 +1,23 @@
+pub mod group_by_thousands;
+
+pub trait Solution {
+    fn number_to_words(num: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (123, "One Hundred Twenty Three"),
+            (12345, "Twelve Thousand Three Hundred Forty Five"),
+            (1_234_567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"),
+            (1_234_567_891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"),
+        ];
+
+        for (num, expected) in test_cases.iter().copied() {
+            assert_eq!(S::number_to_words(num), expected);
+        }
+    }
+}
