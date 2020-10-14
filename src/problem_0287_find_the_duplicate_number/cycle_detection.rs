f537eafa720678c536a25f8bbf653e0f6f810b4d
EFanZh <efanzh@gmail.com>
2020-10-15 07:03:41 +0800

    Add problem 287: Find the Duplicate Number

---
+++ src/problem_0287_find_the_duplicate_number/cycle_detection.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+        let start = nums.len() as i32;
+        let mut slow = start;
+        let mut fast = start;
+
+        loop {
+            slow = nums[(slow - 1) as usize];
+            fast = nums[(nums[(fast - 1) as usize] - 1) as usize];
+
+            if slow == fast {
+                break;
+            }
+        }
+
+        slow = start;
+
+        while slow != fast {
+            slow = nums[(slow - 1) as usize];
+            fast = nums[(fast - 1) as usize];
+        }
+
+        slow
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_duplicate(nums: Vec<i32>) -> i32 {
+        Self::find_duplicate(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
