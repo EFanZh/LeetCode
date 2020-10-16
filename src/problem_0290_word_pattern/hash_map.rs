a95ccd4eabae94eab38a83c673688b047364c8a7
EFanZh <efanzh@gmail.com>
2020-10-17 07:18:47 +0800

    Add problem 290: Word Pattern

---
+++ src/problem_0290_word_pattern/hash_map.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        let mut pattern_to_word = HashMap::new();
+        let mut word_to_pattern = HashMap::new();
+        let mut pattern_iter = pattern.bytes();
+        let mut word_iter = s.split(' ');
+
+        loop {
+            match (pattern_iter.next(), word_iter.next()) {
+                (None, None) => break true,
+                (Some(p), Some(word)) => {
+                    if *pattern_to_word.entry(p).or_insert(word) != word
+                        || *word_to_pattern.entry(word).or_insert(p) != p
+                    {
+                        return false;
+                    }
+                }
+                _ => break false,
+            }
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn word_pattern(pattern: String, s: String) -> bool {
+        Self::word_pattern(pattern, s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0290_word_pattern/mod.rs
@@ -0,0 +1,24 @@
+pub mod hash_map;
+pub mod index_hash_map;
+
+pub trait Solution {
+    fn word_pattern(pattern: String, s: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("abba", "dog cat cat dog"), true),
+            (("abba", "dog cat cat fish"), false),
+            (("aaaa", "dog cat cat dog"), false),
+            (("abba", "dog dog dog dog"), false),
+        ];
+
+        for ((pattern, s), expected) in test_cases.iter().copied() {
+            assert_eq!(S::word_pattern(pattern.to_string(), s.to_string()), expected);
+        }
+    }
+}
