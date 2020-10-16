efcd89f2626f7de658ba29674718ea5b0a6c7291
EFanZh <efanzh@gmail.com>
2020-10-17 07:17:51 +0800

    Add a division solution to problem 231: Power of Two

---
+++ src/problem_0231_power_of_two/division.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        n > 0 && ((1 << 30) % n == 0)
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_power_of_two(n: i32) -> bool {
+        Self::is_power_of_two(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
