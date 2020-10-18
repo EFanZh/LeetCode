a2db304c31b278f5ed7162d8e6d0c90cc867034d
EFanZh <efanzh@gmail.com>
2020-10-18 16:10:38 +0800

    Add problem 326: Power of Three

---
+++ src/problem_0326_power_of_three/division.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn is_power_of_three(n: i32) -> bool {
+        n > 0 && (1_162_261_467 % n == 0)
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_power_of_three(n: i32) -> bool {
+        Self::is_power_of_three(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
