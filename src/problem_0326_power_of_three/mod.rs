c5a1ad05894bdd3baef1c099cddb094120d01901
EFanZh <efanzh@gmail.com>
2020-10-18 16:10:38 +0800

    Add problem 326: Power of Three

---
+++ src/problem_0326_power_of_three/mod.rs
@@ -0,0 +1,18 @@
+pub mod division;
+
+pub trait Solution {
+    fn is_power_of_three(n: i32) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(27, true), (0, false), (9, true), (45, false)];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_power_of_three(n), expected);
+        }
+    }
+}
