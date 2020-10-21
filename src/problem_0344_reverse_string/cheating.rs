0441f825139c15d06127a73a8601ab621c5334b8
EFanZh <efanzh@gmail.com>
2020-10-22 07:01:57 +0800

    Add problem 344: Reverse String

---
+++ src/problem_0344_reverse_string/cheating.rs
@@ -0,0 +1,21 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        s.reverse()
+    }
+}
+
+impl super::Solution for Solution {
+    fn reverse_string(s: &mut Vec<char>) {
+        Self::reverse_string(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0344_reverse_string/indices.rs
@@ -0,0 +1,25 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        let length = s.len();
+
+        for i in 0..length / 2 {
+            s.swap(i, length - 1 - i);
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn reverse_string(s: &mut Vec<char>) {
+        Self::reverse_string(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
