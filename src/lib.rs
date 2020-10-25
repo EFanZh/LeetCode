cd936c23d9e9963852d9404eceb8e722604b3a5f
EFanZh <efanzh@gmail.com>
2020-10-26 07:57:20 +0800

    Add problem 295: Find Median from Data Stream

--- src/lib.rs
+++ src/lib.rs
@@ -221,6 +221,7 @@ pub mod problem_0287_find_the_duplicate_number;
 pub mod problem_0289_game_of_life;
 pub mod problem_0290_word_pattern;
 pub mod problem_0292_nim_game;
+pub mod problem_0295_find_median_from_data_stream;
 pub mod problem_0299_bulls_and_cows;
 pub mod problem_0300_longest_increasing_subsequence;
 pub mod problem_0318_maximum_product_of_word_lengths;
---
+++ src/problem_0295_find_median_from_data_stream/two_heaps.rs
@@ -0,0 +1,67 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::mem;
+
+pub struct MedianFinder {
+    left: BinaryHeap<i32>,
+    right: BinaryHeap<Reverse<i32>>,
+}
+
+impl MedianFinder {
+    fn new() -> Self {
+        Self {
+            left: BinaryHeap::new(),
+            right: BinaryHeap::new(),
+        }
+    }
+
+    fn add_num(&mut self, num: i32) {
+        if self.left.len() == self.right.len() {
+            self.left.push(self.right.peek_mut().map_or(num, |mut right| {
+                if num <= right.0 {
+                    num
+                } else {
+                    mem::replace(&mut right.0, num)
+                }
+            }));
+        } else {
+            self.right.push(Reverse(self.left.peek_mut().map_or(num, |mut left| {
+                if num >= *left {
+                    num
+                } else {
+                    mem::replace(&mut left, num)
+                }
+            })));
+        }
+    }
+
+    fn find_median(&self) -> f64 {
+        if self.left.len() == self.right.len() {
+            f64::from(self.left.peek().unwrap() + self.right.peek().unwrap().0) / 2.0
+        } else {
+            f64::from(*self.left.peek().unwrap())
+        }
+    }
+}
+
+impl super::MedianFinder for MedianFinder {
+    fn new() -> Self {
+        Self::new()
+    }
+
+    fn add_num(&mut self, num: i32) {
+        self.add_num(num)
+    }
+
+    fn find_median(&self) -> f64 {
+        self.find_median()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_median_finder() {
+        super::super::tests::run::<super::MedianFinder>();
+    }
+}
