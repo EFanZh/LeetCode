7ab5f692669687fe8e1fc9d02aa3cfcaa4f6b6ac
EFanZh <efanzh@gmail.com>
2020-10-27 07:44:06 +0800

    Add problem 297: Serialize and Deserialize Binary Tree

---
+++ src/problem_0297_serialize_and_deserialize_binary_tree/level_order_traversal.rs
@@ -0,0 +1,116 @@
+use super::super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+pub struct Codec;
+
+impl Codec {
+    fn new() -> Self {
+        Self
+    }
+
+    #[allow(clippy::unused_self)]
+    fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        use std::fmt::Write;
+
+        let mut result = String::from("[");
+
+        if let Some(mut node) = root {
+            let mut queue = VecDeque::new();
+            let mut non_null_nodes = 0;
+
+            'outer: loop {
+                let node_ref = node.borrow();
+
+                write!(&mut result, "{}", node_ref.val).unwrap();
+
+                queue.push_back(node_ref.left.clone());
+                queue.push_back(node_ref.right.clone());
+                non_null_nodes += usize::from(node_ref.left.is_some()) + usize::from(node_ref.right.is_some());
+
+                drop(node_ref);
+
+                while non_null_nodes != 0 {
+                    loop {
+                        if let Some(next) = queue.pop_front().unwrap() {
+                            node = next;
+                            non_null_nodes -= 1;
+
+                            result.push(',');
+
+                            continue 'outer;
+                        } else {
+                            result.push_str(",null");
+                        }
+                    }
+                }
+
+                break;
+            }
+        }
+
+        result.push(']');
+
+        result
+    }
+
+    #[allow(clippy::unused_self)]
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut iter = data[1..data.len() - 1].split_terminator(',');
+
+        iter.next().map(|val| {
+            let root = Rc::new(RefCell::new(TreeNode::new(val.parse().unwrap())));
+            let mut queue = VecDeque::from(vec![root.clone()]);
+
+            while let Some(item) = iter.next() {
+                let target = queue.pop_front().unwrap();
+                let mut target_ref = target.borrow_mut();
+
+                if let Ok(val) = item.parse() {
+                    let left = Rc::new(RefCell::new(TreeNode::new(val)));
+
+                    target_ref.left = Some(left.clone());
+
+                    queue.push_back(left);
+                }
+
+                if let Some(item) = iter.next() {
+                    if let Ok(val) = item.parse() {
+                        let right = Rc::new(RefCell::new(TreeNode::new(val)));
+
+                        target_ref.right = Some(right.clone());
+
+                        queue.push_back(right);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            root
+        })
+    }
+}
+
+impl super::Codec for Codec {
+    fn new() -> Self {
+        Self::new()
+    }
+
+    fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        self.serialize(root)
+    }
+
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
+        self.deserialize(data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_median_finder() {
+        super::super::tests::run::<super::Codec>();
+    }
+}
---
+++ src/problem_0297_serialize_and_deserialize_binary_tree/mod.rs
@@ -0,0 +1,36 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod level_order_traversal;
+pub mod preorder_traversal;
+
+pub trait Codec {
+    fn new() -> Self;
+    fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String;
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Codec;
+
+    pub fn run<C: Codec>() {
+        let test_cases = [
+            &[Some(1), Some(2), Some(3), None, None, Some(4), Some(5)] as &[_],
+            &[],
+            &[Some(1)],
+            &[Some(1), Some(2)],
+        ];
+
+        for root in test_cases.iter().copied() {
+            let codec = C::new();
+            let root = test_utilities::make_tree(root.iter().copied());
+            let data = codec.serialize(root.clone());
+            let recovered_root = codec.deserialize(data);
+
+            assert_eq!(recovered_root, root);
+        }
+    }
+}
