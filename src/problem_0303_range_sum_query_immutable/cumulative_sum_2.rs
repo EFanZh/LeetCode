79273e88e0d26f6750227f3e7a7e32e64ec18ffb
EFanZh <efanzh@gmail.com>
2020-11-02 07:13:02 +0800

    Add problem 303: Range Sum Query - Immutable

---
+++ src/problem_0303_range_sum_query_immutable/cumulative_sum_2.rs
@@ -0,0 +1,43 @@
+use std::mem;
+
+struct NumArray {
+    sums: Box<[i32]>,
+}
+
+impl NumArray {
+    fn new(mut nums: Vec<i32>) -> Self {
+        let mut sum = 0;
+
+        for num in &mut nums {
+            sum += mem::replace(num, sum);
+        }
+
+        nums.push(sum);
+
+        Self {
+            sums: nums.into_boxed_slice(),
+        }
+    }
+
+    fn sum_range(&self, i: i32, j: i32) -> i32 {
+        self.sums[(j as usize) + 1] - self.sums[i as usize]
+    }
+}
+
+impl super::NumArray for NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        Self::new(nums)
+    }
+
+    fn sum_range(&self, i: i32, j: i32) -> i32 {
+        self.sum_range(i, j)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::NumArray>();
+    }
+}
---
+++ src/problem_0303_range_sum_query_immutable/mod.rs
@@ -0,0 +1,27 @@
+pub mod cumulative_sum;
+pub mod cumulative_sum_2;
+
+pub trait NumArray {
+    fn new(nums: Vec<i32>) -> Self;
+    fn sum_range(&self, i: i32, j: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::NumArray;
+
+    pub fn run<N: NumArray>() {
+        let test_cases = [(
+            &[-2, 0, 3, -5, 2, -1] as &[_],
+            &[((0, 2), 1), ((2, 5), -1), ((0, 5), -3)] as &[_],
+        )];
+
+        for (nums, sums) in test_cases.iter().copied() {
+            let num_array = N::new(nums.to_vec());
+
+            for ((i, j), expected) in sums.iter().copied() {
+                assert_eq!(num_array.sum_range(i, j), expected);
+            }
+        }
+    }
+}
