fda041ec665c4b75605cc27121e386e83d8e2a08
EFanZh <efanzh@gmail.com>
2020-11-10 07:04:14 +0800

    Add problem 236: Lowest Common Ancestor of a Binary Tree

---
+++ src/problem_0236_lowest_common_ancestor_of_a_binary_tree/mod.rs
@@ -0,0 +1,89 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::data_structures::TreeNode;
+    use super::super::test_utilities;
+    use super::Solution;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    fn find_node(root: &Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        root.as_ref().and_then(|root| {
+            let root_ref = root.borrow();
+
+            if root_ref.val == val {
+                Some(root.clone())
+            } else {
+                find_node(&root_ref.left, val).or_else(|| find_node(&root_ref.right, val))
+            }
+        })
+    }
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[
+                        Some(3),
+                        Some(5),
+                        Some(1),
+                        Some(6),
+                        Some(2),
+                        Some(0),
+                        Some(8),
+                        None,
+                        None,
+                        Some(7),
+                        Some(4),
+                    ] as &[_],
+                    5,
+                    1,
+                ),
+                3,
+            ),
+            (
+                (
+                    &[
+                        Some(3),
+                        Some(5),
+                        Some(1),
+                        Some(6),
+                        Some(2),
+                        Some(0),
+                        Some(8),
+                        None,
+                        None,
+                        Some(7),
+                        Some(4),
+                    ],
+                    5,
+                    4,
+                ),
+                5,
+            ),
+            ((&[Some(1), Some(2)], 1, 2), 1),
+        ];
+
+        for ((root, p, q), expected) in test_cases.iter().copied() {
+            let root = test_utilities::make_tree(root.iter().copied());
+            let p = find_node(&root, p);
+            let q = find_node(&root, q);
+            let expected = find_node(&root, expected);
+
+            assert_eq!(S::lowest_common_ancestor(root, p, q), expected);
+        }
+    }
+}
---
+++ src/problem_0236_lowest_common_ancestor_of_a_binary_tree/recursive.rs
@@ -0,0 +1,106 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn is_same_object<T>(a: *const T, b: *const T) -> bool {
+        a == b
+    }
+
+    fn has_node(
+        root: Option<&RefCell<TreeNode>>,
+        node: &RefCell<TreeNode>,
+        yes: &dyn Fn() -> Rc<RefCell<TreeNode>>,
+        no: &dyn Fn() -> Rc<RefCell<TreeNode>>,
+    ) -> Rc<RefCell<TreeNode>> {
+        root.map_or_else(no, |root| {
+            if Self::is_same_object(root, node) {
+                yes()
+            } else {
+                let root_ref = root.borrow();
+
+                Self::has_node(root_ref.left.as_deref(), node, yes, &|| {
+                    Self::has_node(root_ref.right.as_deref(), node, yes, no)
+                })
+            }
+        })
+    }
+
+    fn find_nodes(
+        root: &Option<Rc<RefCell<TreeNode>>>,
+        p: &RefCell<TreeNode>,
+        q: &RefCell<TreeNode>,
+        nothing: &dyn Fn() -> Rc<RefCell<TreeNode>>,
+        only_p: &dyn Fn() -> Rc<RefCell<TreeNode>>,
+        only_q: &dyn Fn() -> Rc<RefCell<TreeNode>>,
+    ) -> Rc<RefCell<TreeNode>> {
+        root.as_ref().map_or_else(nothing, |root| {
+            let root_ref = root.borrow();
+
+            if Self::is_same_object(root.as_ref(), p) {
+                Self::has_node(root_ref.left.as_deref(), q, &|| root.clone(), &|| {
+                    Self::has_node(root_ref.right.as_deref(), q, &|| root.clone(), only_p)
+                })
+            } else if Self::is_same_object(root.as_ref(), q) {
+                Self::has_node(root_ref.left.as_deref(), p, &|| root.clone(), &|| {
+                    Self::has_node(root_ref.right.as_deref(), p, &|| root.clone(), only_q)
+                })
+            } else {
+                Self::find_nodes(
+                    &root_ref.left,
+                    p,
+                    q,
+                    &|| Self::find_nodes(&root_ref.right, p, q, nothing, only_p, only_q),
+                    &|| Self::has_node(root_ref.right.as_deref(), q, &|| root.clone(), only_p),
+                    &|| Self::has_node(root_ref.right.as_deref(), p, &|| root.clone(), only_q),
+                )
+            }
+        })
+    }
+
+    fn helper(root: &Rc<RefCell<TreeNode>>, p: &RefCell<TreeNode>, q: &RefCell<TreeNode>) -> Rc<RefCell<TreeNode>> {
+        if Self::is_same_object(root.as_ref(), p) || Self::is_same_object(root.as_ref(), q) {
+            root.clone()
+        } else {
+            let root_ref = root.borrow();
+
+            Self::find_nodes(
+                &root_ref.left,
+                p,
+                q,
+                &|| Self::helper(root_ref.right.as_ref().unwrap(), p, q),
+                &|| root.clone(),
+                &|| root.clone(),
+            )
+        }
+    }
+
+    pub fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Self::helper(&root.unwrap(), p.unwrap().as_ref(), q.unwrap().as_ref()))
+    }
+}
+
+impl super::Solution for Solution {
+    fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::lowest_common_ancestor(root, p, q)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
