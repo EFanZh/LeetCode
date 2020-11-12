b4eaf557b2768c6c7340de44f69ebcce7b24c98a
EFanZh <efanzh@gmail.com>
2020-11-13 07:10:01 +0800

    Add problem 278: First Bad Version

---
+++ src/problem_0278_first_bad_version/binary_search.rs
@@ -0,0 +1,48 @@
+use super::Solution as _;
+
+pub struct Solution {
+    bad: i32,
+}
+
+impl Solution {
+    pub fn first_bad_version(&self, n: i32) -> i32 {
+        let mut start = 1;
+        let mut count = n;
+
+        while count != 0 {
+            let half = count / 2;
+            let middle = start + half;
+
+            if self.isBadVersion(middle) {
+                count = half;
+            } else {
+                start = middle + 1;
+                count -= half + 1;
+            }
+        }
+
+        start
+    }
+}
+
+impl super::Solution for Solution {
+    fn new(bad: i32) -> Self {
+        Self { bad }
+    }
+
+    fn isBadVersion(&self, version: i32) -> bool {
+        version >= self.bad
+    }
+
+    fn first_bad_version(&self, n: i32) -> i32 {
+        self.first_bad_version(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
