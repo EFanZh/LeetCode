2ce0e0902cb85351168f22b5f5404e9727d70739
EFanZh <efanzh@gmail.com>
2020-11-17 07:11:54 +0800

    Add problem 1192: Critical Connections in a Network

---
+++ src/problem_1192_critical_connections_in_a_network/dfs.rs
@@ -0,0 +1,78 @@
+pub struct Solution;
+
+use std::convert::TryFrom;
+
+impl Solution {
+    fn dfs(
+        graph: &[Vec<i32>],
+        vertex: i32,
+        parent: i32,
+        time: &mut u32,
+        states: &mut [u32],
+        result: &mut Vec<Vec<i32>>,
+    ) -> u32 {
+        let discovery_time = *time;
+        let mut low = discovery_time;
+
+        *time += 1;
+        states[vertex as usize] = discovery_time;
+
+        for &next in &graph[vertex as usize] {
+            if next != parent {
+                let state = &mut states[next as usize];
+
+                if *state == u32::max_value() {
+                    let next_low = Self::dfs(graph, next, vertex, time, states, result);
+
+                    if next_low > discovery_time {
+                        result.push(vec![vertex, next])
+                    }
+
+                    low = low.min(next_low);
+                } else {
+                    low = low.min(*state);
+                }
+            }
+        }
+
+        states[vertex as usize] = low;
+
+        low
+    }
+
+    pub fn critical_connections(n: i32, connections: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut graph = vec![Vec::new(); n as _];
+
+        for [from, to] in connections.iter().map(Vec::as_slice).flat_map(<[i32; 2]>::try_from) {
+            graph[from as usize].push(to);
+            graph[to as usize].push(from);
+        }
+
+        let mut result = Vec::new();
+
+        Self::dfs(
+            &graph,
+            0,
+            -1,
+            &mut 0,
+            &mut vec![u32::max_value(); graph.len()],
+            &mut result,
+        );
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn critical_connections(n: i32, connections: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        Self::critical_connections(n, connections)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
