b3e994b1eb2ecb9ecf9dc74aca2f17995efb1138
EFanZh <efanzh@gmail.com>
2020-11-18 07:10:03 +0800

    Add problem 328: Odd Even Linked List

---
+++ src/problem_0328_odd_even_linked_list/iterative.rs
@@ -0,0 +1,42 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn odd_even_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut odd = None;
+        let mut odd_tail = &mut odd;
+        let mut even = None;
+        let mut even_tail = &mut even;
+
+        while let Some(mut first) = head {
+            if let Some(mut second) = first.next.take() {
+                head = second.next.take();
+                odd_tail = &mut odd_tail.get_or_insert(first).next;
+                even_tail = &mut even_tail.get_or_insert(second).next;
+            } else {
+                odd_tail = &mut odd_tail.get_or_insert(first).next;
+
+                break;
+            }
+        }
+
+        *odd_tail = even;
+
+        odd
+    }
+}
+
+impl super::Solution for Solution {
+    fn odd_even_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::odd_even_list(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
