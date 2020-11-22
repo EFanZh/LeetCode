14fb8669d948f4e554aec34f7f02826c9c611d43
EFanZh <efanzh@gmail.com>
2020-11-23 06:56:47 +0800

    Add problem 334: Increasing Triplet Subsequence

---
+++ src/problem_0334_increasing_triplet_subsequence/iterative.rs
@@ -0,0 +1,43 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn increasing_triplet(nums: Vec<i32>) -> bool {
+        let mut iter = nums.into_iter();
+
+        if let Some(mut first) = iter.next() {
+            while let Some(mut second) = iter.next() {
+                if second > first {
+                    for third in iter {
+                        if third > second {
+                            return true;
+                        } else if third > first {
+                            second = third;
+                        } else {
+                            first = third;
+                        }
+                    }
+
+                    break;
+                } else {
+                    first = second;
+                }
+            }
+        }
+
+        false
+    }
+}
+
+impl super::Solution for Solution {
+    fn increasing_triplet(nums: Vec<i32>) -> bool {
+        Self::increasing_triplet(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0334_increasing_triplet_subsequence/iterative_2.rs
@@ -0,0 +1,34 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn increasing_triplet(nums: Vec<i32>) -> bool {
+        let mut first = i32::max_value();
+        let mut second = i32::max_value();
+
+        for num in nums {
+            if num <= first {
+                first = num;
+            } else if num <= second {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+impl super::Solution for Solution {
+    fn increasing_triplet(nums: Vec<i32>) -> bool {
+        Self::increasing_triplet(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
