42d23fa03ff60a8b4216e0f11d134a57e8cd5f50
EFanZh <efanzh@gmail.com>
2020-11-27 06:59:00 +0800

    Add a new test case

--- src/problem_0330_patching_array/mod.rs
+++ src/problem_0330_patching_array/mod.rs
@@ -9,7 +9,12 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((&[1, 3] as &[_], 6), 1), ((&[1, 5, 10], 20), 2), ((&[1, 2, 2], 5), 0)];
+        let test_cases = [
+            ((&[1, 3] as &[_], 6), 1),
+            ((&[1, 5, 10], 20), 2),
+            ((&[1, 2, 2], 5), 0),
+            ((&[1, 2, 31, 33], 2_147_483_647), 28),
+        ];
 
         for ((nums, n), expected) in test_cases.iter().copied() {
             assert_eq!(S::min_patches(nums.to_vec(), n), expected);
