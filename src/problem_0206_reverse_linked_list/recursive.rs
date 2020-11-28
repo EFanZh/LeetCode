723b9c7ca4d8a8b90514bc8961f2acd376e6cf40
EFanZh <efanzh@gmail.com>
2020-11-28 13:51:10 +0800

    Add a recursive solution to problem 206: Reverse Linked List

---
+++ src/problem_0206_reverse_linked_list/recursive.rs
@@ -0,0 +1,36 @@
+use super::super::data_structures::ListNode;
+
+pub struct Solution;
+
+use std::mem;
+
+impl Solution {
+    #[allow(clippy::clippy::option_if_let_else)]
+    fn helper(reversed: Option<Box<ListNode>>, head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if let Some(mut node) = head {
+            let new_head = mem::replace(&mut node.next, reversed);
+
+            Self::helper(Some(node), new_head)
+        } else {
+            reversed
+        }
+    }
+
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::helper(None, head)
+    }
+}
+
+impl super::Solution for Solution {
+    fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::reverse_list(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
