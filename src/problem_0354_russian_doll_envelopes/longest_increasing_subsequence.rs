7042883b026e752c577d7877dab70428f7db0dad
EFanZh <efanzh@gmail.com>
2020-12-03 07:04:50 +0800

    Add problem 354: Russian Doll Envelopes

---
+++ src/problem_0354_russian_doll_envelopes/longest_increasing_subsequence.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+use std::cmp::Reverse;
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn max_envelopes(mut envelopes: Vec<Vec<i32>>) -> i32 {
+        envelopes.sort_unstable_by_key(|envelope| {
+            let [w, h]: [_; 2] = envelope.as_slice().try_into().unwrap();
+
+            (w, Reverse(h))
+        });
+
+        let mut cache = Vec::with_capacity(envelopes.len());
+
+        for height in envelopes.into_iter().map(|envelope| envelope[1]) {
+            if let Err(index) = cache.binary_search(&height) {
+                if let Some(value) = cache.get_mut(index) {
+                    *value = height;
+                } else {
+                    cache.push(height);
+                }
+            }
+        }
+
+        cache.len() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn max_envelopes(envelopes: Vec<Vec<i32>>) -> i32 {
+        Self::max_envelopes(envelopes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
