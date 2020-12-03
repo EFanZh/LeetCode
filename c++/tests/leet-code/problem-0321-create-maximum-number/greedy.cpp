4bd4d6bcf704c5b80ca5b8319ab971e3d9169565
EFanZh <efanzh@gmail.com>
2020-12-03 23:13:57 +0800

    Update C++ code format

--- c++/tests/leet-code/problem-0321-create-maximum-number/greedy.cpp
+++ c++/tests/leet-code/problem-0321-create-maximum-number/greedy.cpp
@@ -1,5 +1,5 @@
-#include <leet-code/problem-0321-create-maximum-number/greedy.h>
 #include "tests.h"
+#include <leet-code/problem-0321-create-maximum-number/greedy.h>
 
 namespace leet_code::problem_0321_create_maximum_number::tests {
 TEST(CreateMaximumNumber, Greedy) {
