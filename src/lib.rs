284670563c56f509864c2c14b32690b9f406e273
EFanZh <efanzh@gmail.com>
2020-12-07 06:54:25 +0800

    Add problem 389: Find the Difference

--- src/lib.rs
+++ src/lib.rs
@@ -266,6 +266,7 @@ pub mod problem_0367_valid_perfect_square;
 pub mod problem_0371_sum_of_two_integers;
 pub mod problem_0383_ransom_note;
 pub mod problem_0387_first_unique_character_in_a_string;
+pub mod problem_0389_find_the_difference;
 pub mod problem_0392_is_subsequence;
 pub mod problem_0393_utf_8_validation;
 pub mod problem_0401_binary_watch;
---
+++ src/problem_0389_find_the_difference/mod.rs
@@ -0,0 +1,23 @@
+pub mod reduce_xor;
+
+pub trait Solution {
+    fn find_the_difference(s: String, t: String) -> char;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("abcd", "abcde"), 'e'),
+            (("", "y"), 'y'),
+            (("a", "aa"), 'a'),
+            (("ae", "aea"), 'a'),
+        ];
+
+        for ((s, t), expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_the_difference(s.to_string(), t.to_string()), expected);
+        }
+    }
+}
