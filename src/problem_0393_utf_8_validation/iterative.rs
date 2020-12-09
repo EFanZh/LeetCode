e17b5399e88b4cf94082610ff7da024fbcd1a088
EFanZh <efanzh@gmail.com>
2020-12-09 20:10:48 +0800

    Reduce the size for a few bytes

--- src/problem_0393_utf_8_validation/iterative.rs
+++ src/problem_0393_utf_8_validation/iterative.rs
@@ -4,25 +4,25 @@ impl Solution {
     pub fn valid_utf8(data: Vec<i32>) -> bool {
         let mut i = 0;
 
-        while let Some(&first) = data.get(i) {
+        while let Some(first) = data.get(i) {
             match first {
                 0..=127 => i += 1,
                 192..=223 => {
-                    if let Some(&[128..=191]) = data.get(i + 1..i + 2) {
+                    if let Some([128..=191]) = data.get(i + 1..i + 2) {
                         i += 2;
                     } else {
                         return false;
                     }
                 }
                 224..=239 => {
-                    if let Some(&[128..=191, 128..=191]) = data.get(i + 1..i + 3) {
+                    if let Some([128..=191, 128..=191]) = data.get(i + 1..i + 3) {
                         i += 3;
                     } else {
                         return false;
                     }
                 }
                 240..=247 => {
-                    if let Some(&[128..=191, 128..=191, 128..=191]) = data.get(i + 1..i + 4) {
+                    if let Some([128..=191, 128..=191, 128..=191]) = data.get(i + 1..i + 4) {
                         i += 4;
                     } else {
                         return false;
