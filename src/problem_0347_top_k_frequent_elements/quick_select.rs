df8698e6b60f63b4ab409327146eafc7271a5a9a
EFanZh <efanzh@gmail.com>
2020-12-11 07:15:05 +0800

    Add problem 347: Top K Frequent Elements

---
+++ src/problem_0347_top_k_frequent_elements/quick_select.rs
@@ -0,0 +1,62 @@
+pub struct Solution;
+
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let k = k as _;
+        let mut counts = HashMap::new();
+
+        for num in nums {
+            counts.entry(num).and_modify(|count| *count += 1).or_insert(1);
+        }
+
+        let mut counts = counts.into_iter().collect::<Box<_>>();
+        let mut start = 0;
+        let mut end = counts.len();
+
+        while end - start > 1 {
+            let mut i = start;
+            let mut j = end - 1;
+            let pivot = counts[j].1;
+
+            loop {
+                if counts[i].1 > pivot {
+                    i += 1;
+                } else {
+                    j -= 1;
+                    counts.swap(i, j);
+                }
+
+                if i == j {
+                    break;
+                }
+            }
+
+            counts.swap(i, end - 1);
+
+            match i.cmp(&k) {
+                Ordering::Less => start = i + 1,
+                Ordering::Equal => break,
+                Ordering::Greater => end = i,
+            }
+        }
+
+        counts[..k].iter().map(|&(num, _)| num).collect()
+    }
+}
+
+impl super::Solution for Solution {
+    fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        Self::top_k_frequent(nums, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
