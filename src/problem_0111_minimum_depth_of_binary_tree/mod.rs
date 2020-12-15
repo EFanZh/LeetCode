4dc4299290b418fdc674723f8e19daac33ee3275
EFanZh <efanzh@gmail.com>
2020-12-15 20:34:19 +0800

    Add some test cases to problem 111: Minimum Depth of Binary Tree

--- src/problem_0111_minimum_depth_of_binary_tree/mod.rs
+++ src/problem_0111_minimum_depth_of_binary_tree/mod.rs
@@ -14,7 +14,14 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)] as &[_], 2)];
+        let test_cases = [
+            (&[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)] as &[_], 2),
+            (
+                &[Some(2), None, Some(3), None, Some(4), None, Some(5), None, Some(6)],
+                5,
+            ),
+            (&[Some(0)], 1),
+        ];
 
         for (root, expected) in test_cases.iter().copied() {
             assert_eq!(S::min_depth(test_utilities::make_tree(root.iter().copied())), expected);
