1c2717c9a85954c85dba9f8a1944e33b9d221eaf
EFanZh <efanzh@gmail.com>
2020-12-15 20:24:57 +0800

    Refactor

--- src/problem_0002_add_two_numbers/simple.rs
+++ src/problem_0002_add_two_numbers/simple.rs
@@ -7,35 +7,35 @@ impl Solution {
         let mut result = l1;
         let mut lhs = &mut result;
         let mut rhs = l2;
-        let mut carry = false;
+        let mut carry = 0;
 
         loop {
             if let Some(lhs_node) = lhs {
                 if let Some(rhs_node) = rhs {
-                    lhs_node.val += rhs_node.val + (carry as i32);
-                    carry = lhs_node.val >= 10;
+                    lhs_node.val += rhs_node.val + carry;
+                    carry = lhs_node.val / 10;
                     lhs_node.val %= 10;
 
                     lhs = &mut lhs_node.next;
                     rhs = rhs_node.next;
                 } else {
-                    lhs_node.val += carry as i32;
-                    carry = lhs_node.val >= 10;
+                    lhs_node.val += carry;
+                    carry = lhs_node.val / 10;
                     lhs_node.val %= 10;
 
                     lhs = &mut lhs_node.next;
                 }
             } else if let Some(mut rhs_node) = rhs {
-                rhs_node.val += carry as i32;
-                carry = rhs_node.val >= 10;
+                rhs_node.val += carry;
+                carry = rhs_node.val / 10;
                 rhs_node.val %= 10;
 
                 rhs = rhs_node.next.take();
                 *lhs = Some(rhs_node);
                 lhs = &mut lhs.as_mut().unwrap().next;
             } else {
-                if carry {
-                    *lhs = Some(Box::new(ListNode::new(1)));
+                if carry != 0 {
+                    *lhs = Some(Box::new(ListNode::new(carry)));
                 }
 
                 break;
