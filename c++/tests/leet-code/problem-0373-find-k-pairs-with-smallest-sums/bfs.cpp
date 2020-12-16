7c652057d03fbda1d02c4ef52388bf69d4683447
EFanZh <efanzh@gmail.com>
2020-12-16 22:21:33 +0800

    Add problem 373: Find K Pairs with Smallest Sums (C++)

---
+++ c++/tests/leet-code/problem-0373-find-k-pairs-with-smallest-sums/bfs.cpp
@@ -0,0 +1,8 @@
+#include "tests.h"
+#include <leet-code/problem-0373-find-k-pairs-with-smallest-sums/bfs.h>
+
+namespace leet_code::problem_0373_find_k_pairs_with_smallest_sums::tests {
+TEST(Problem0373FindKPairsWithSmallestSums, Bfs) {
+    tests::run<bfs::Solution>();
+}
+} // namespace leet_code::problem_0373_find_k_pairs_with_smallest_sums::tests
