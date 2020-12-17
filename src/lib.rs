13ec7e1b6f43a86285155cbd0e1869e1c27e5f87
EFanZh <efanzh@gmail.com>
2020-12-18 07:13:58 +0800

    Add problem 405: Convert a Number to Hexadecimal

--- src/lib.rs
+++ src/lib.rs
@@ -280,6 +280,7 @@ pub mod problem_0394_decode_string;
 pub mod problem_0397_integer_replacement;
 pub mod problem_0401_binary_watch;
 pub mod problem_0402_remove_k_digits;
+pub mod problem_0405_convert_a_number_to_hexadecimal;
 pub mod problem_0435_non_overlapping_intervals;
 pub mod problem_0476_number_complement;
 pub mod problem_0509_fibonacci_number;
