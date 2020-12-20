cfd2cfca32530923aaae54f4b933ef57398ef783
EFanZh <efanzh@gmail.com>
2020-12-20 08:47:10 +0800

    Add problem 415: Add Strings

--- src/lib.rs
+++ src/lib.rs
@@ -282,6 +282,7 @@ pub mod problem_0401_binary_watch;
 pub mod problem_0402_remove_k_digits;
 pub mod problem_0405_convert_a_number_to_hexadecimal;
 pub mod problem_0412_fizz_buzz;
+pub mod problem_0415_add_strings;
 pub mod problem_0435_non_overlapping_intervals;
 pub mod problem_0476_number_complement;
 pub mod problem_0509_fibonacci_number;
---
+++ src/problem_0415_add_strings/mod.rs
@@ -0,0 +1,24 @@
+pub mod naive;
+
+pub trait Solution {
+    fn add_strings(num1: String, num2: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("0", "0"), "0"),
+            (("999", "1"), "1000"),
+            (("1", "999"), "1000"),
+            (("899", "3"), "902"),
+            (("999", "999"), "1998"),
+        ];
+
+        for ((num1, num2), expected) in test_cases.iter().copied() {
+            assert_eq!(S::add_strings(num1.to_string(), num2.to_string()), expected);
+        }
+    }
+}
