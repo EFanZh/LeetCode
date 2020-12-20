fe45e6bcd3f89f51164867c4486d3ae50b331e98
EFanZh <efanzh@gmail.com>
2020-12-20 08:47:10 +0800

    Add problem 415: Add Strings

---
+++ src/problem_0415_add_strings/naive.rs
@@ -0,0 +1,55 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn add_strings(num1: String, num2: String) -> String {
+        let (num1, mut num2) = if num2.len() < num1.len() {
+            (num2.into_bytes(), num1.into_bytes())
+        } else {
+            (num1.into_bytes(), num2.into_bytes())
+        };
+
+        let mut carry = 0;
+
+        for (lhs, rhs) in num1.iter().rev().zip(num2.iter_mut().rev()) {
+            *rhs += lhs + carry - b'0';
+
+            if *rhs < b'0' + 10 {
+                carry = 0;
+            } else {
+                *rhs -= 10;
+                carry = 1;
+            }
+        }
+
+        for rhs in num2.iter_mut().rev().skip(num1.len()) {
+            *rhs += carry;
+
+            if *rhs < b'0' + 10 {
+                carry = 0;
+            } else {
+                *rhs -= 10;
+                carry = 1;
+            }
+        }
+
+        if carry != 0 {
+            num2.insert(0, b'1');
+        }
+
+        String::from_utf8(num2).unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn add_strings(num1: String, num2: String) -> String {
+        Self::add_strings(num1, num2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
