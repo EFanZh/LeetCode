55957963372e07dd21922a52ba969e14d5f256f1
EFanZh <efanzh@gmail.com>
2020-12-30 08:57:41 +0800

    Add problem 375: Guess Number Higher or Lower II

---
+++ src/problem_0375_guess_number_higher_or_lower_ii/dynamic_programming.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn get_money_amount(n: i32) -> i32 {
+        let n = n as usize;
+        let mut cache = vec![0; n * n];
+
+        for row in 1..n {
+            for column in 0..n - row {
+                let end = column + row;
+                let mut money = column + cache[n * (row - 1) + (column + 1)];
+
+                for i in column + 1..end {
+                    money =
+                        money.min(i + cache[n * (i - (column + 1)) + column].max(cache[n * (end - 1 - i) + (i + 1)]));
+                }
+
+                money = money.min(end + cache[n * (row - 1) + column]);
+
+                cache[n * row + column] = money + 1;
+            }
+        }
+
+        cache[n * (n - 1)] as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn get_money_amount(n: i32) -> i32 {
+        Self::get_money_amount(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0375_guess_number_higher_or_lower_ii/mod.rs
@@ -0,0 +1,29 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn get_money_amount(n: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (1, 0),
+            (2, 1),
+            (3, 2),
+            (4, 4),
+            (5, 6),
+            (6, 8),
+            (7, 10),
+            (8, 12),
+            (9, 14),
+            (10, 16),
+        ];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::get_money_amount(n), expected);
+        }
+    }
+}
