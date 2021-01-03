4a3253449fa40b3a63e5bc6b40e2768b2a034a7d
EFanZh <efanzh@gmail.com>
2021-01-04 06:55:59 +0800

    Add problem 377: Combination Sum IV

---
+++ src/problem_0377_combination_sum_iv/dynamic_programming.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn combination_sum4(mut nums: Vec<i32>, target: i32) -> i32 {
+        let mut cache = Vec::with_capacity((target + 1) as _);
+
+        nums.sort_unstable();
+        cache.push(1);
+
+        for i in 1..=target {
+            let mut result = 0;
+
+            for num in &nums {
+                if let Some(value) = cache.get((i - num) as usize) {
+                    result += value;
+                } else {
+                    break;
+                }
+            }
+
+            cache.push(result);
+        }
+
+        *cache.last().unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
+        Self::combination_sum4(nums, target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0377_combination_sum_iv/mod.rs
@@ -0,0 +1,19 @@
+pub mod dynamic_programming;
+pub mod memoized_dynamic_programming;
+
+pub trait Solution {
+    fn combination_sum4(nums: Vec<i32>, target: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [((&[1, 2, 3] as &[_], 4), 7), ((&[2, 1, 3], 35), 1_132_436_852)];
+
+        for ((nums, target), expected) in test_cases.iter().copied() {
+            assert_eq!(S::combination_sum4(nums.iter().copied().collect(), target), expected);
+        }
+    }
+}
