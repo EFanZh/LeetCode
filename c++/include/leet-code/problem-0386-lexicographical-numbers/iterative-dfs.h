eb23e335845b291b98035155ee1e96bc1bee56b1
EFanZh <efanzh@gmail.com>
2021-01-04 22:02:04 +0800

    Add problem 386: Lexicographical Numbers (C++)

---
+++ c++/include/leet-code/problem-0386-lexicographical-numbers/iterative-dfs.h
@@ -0,0 +1,46 @@
+#ifndef LEET_CODE_PROBLEM_0386_LEXICOGRAPHICAL_NUMBERS_ITERATIVE_DFS_H
+#define LEET_CODE_PROBLEM_0386_LEXICOGRAPHICAL_NUMBERS_ITERATIVE_DFS_H
+
+#include <vector>
+
+namespace leet_code::problem_0386_lexicographical_numbers::iterative_dfs {
+using std::vector;
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        auto result = vector<int>{};
+        auto value = 1;
+
+        result.reserve(static_cast<size_t>(n));
+
+        for (;;) {
+            result.emplace_back(value);
+
+            const auto next = value * 10;
+
+            if (next <= n) {
+                value = next;
+            } else {
+                if (value == n) {
+                    value /= 10;
+                }
+
+                ++value;
+
+                while (value % 10 == 0) {
+                    value /= 10;
+                }
+
+                if (value == 1) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+} // namespace leet_code::problem_0386_lexicographical_numbers::iterative_dfs
+
+#endif // LEET_CODE_PROBLEM_0386_LEXICOGRAPHICAL_NUMBERS_ITERATIVE_DFS_H
