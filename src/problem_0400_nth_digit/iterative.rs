92e00d5fcd6e56fa1c01e771ffa6a5a38977deca
EFanZh <efanzh@gmail.com>
2021-01-06 07:11:52 +0800

    Add problem 400: Nth Digit

---
+++ src/problem_0400_nth_digit/iterative.rs
@@ -0,0 +1,48 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_nth_digit(n: i32) -> i32 {
+        let n = n as u64;
+        let mut total_digits = 1;
+        let mut number_length = 1;
+        let mut number_count = 9;
+        let mut base = 1;
+
+        loop {
+            let next_total_digits = total_digits + number_length * number_count;
+
+            if next_total_digits > n {
+                break;
+            } else {
+                total_digits = next_total_digits;
+                number_length += 1;
+                number_count *= 10;
+                base *= 10
+            }
+        }
+
+        let remaining_digits = n - total_digits;
+        let mut number = base + remaining_digits / number_length;
+        let digit_index = remaining_digits % number_length;
+
+        for _ in 1..number_length - digit_index {
+            number /= 10;
+        }
+
+        (number % 10) as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_nth_digit(n: i32) -> i32 {
+        Self::find_nth_digit(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0400_nth_digit/mod.rs
@@ -0,0 +1,27 @@
+pub mod binary_search;
+pub mod iterative;
+
+pub trait Solution {
+    fn find_nth_digit(n: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (1, 1),
+            (5, 5),
+            (9, 9),
+            (10, 1),
+            (11, 0),
+            (1_000_000_000, 1),
+            (2_147_483_647, 2),
+        ];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_nth_digit(n), expected);
+        }
+    }
+}
