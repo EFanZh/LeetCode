9ad56610a3cd158a478e97ce809e52d9ccaf9500
EFanZh <efanzh@gmail.com>
2021-01-12 11:24:02 +0800

    Fix solution and test cases

--- src/problem_0376_wiggle_subsequence/mod.rs
+++ src/problem_0376_wiggle_subsequence/mod.rs
@@ -9,7 +9,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[3, 1, 5, 8] as &[_], 167)];
+        let test_cases = [
+            (&[1, 7, 4, 9, 2, 5] as &[_], 6),
+            (&[1, 17, 5, 10, 13, 15, 10, 5, 16, 8], 7),
+            (&[1, 2, 3, 4, 5, 6, 7, 8, 9], 2),
+        ];
 
         for (nums, expected) in test_cases.iter().copied() {
             assert_eq!(S::wiggle_max_length(nums.to_vec()), expected);
