10bd4a2c90df39d478de3d030aee09b176ea8c18
EFanZh <efanzh@gmail.com>
2021-01-12 12:31:11 +0800

    Add a new solution to problem 352: Data Stream as Disjoint Intervals

--- src/problem_0352_data_stream_as_disjoint_intervals/mod.rs
+++ src/problem_0352_data_stream_as_disjoint_intervals/mod.rs
@@ -1,3 +1,4 @@
+pub mod ordered_map;
 pub mod union_find;
 
 pub trait SummaryRanges {
@@ -19,18 +20,42 @@ mod tests {
     pub fn run<S: SummaryRanges>() {
         use Operation::{AddNum, GetIntervals};
 
-        let test_cases = [&[
-            AddNum(1),
-            GetIntervals(&[[1, 1]]),
-            AddNum(3),
-            GetIntervals(&[[1, 1], [3, 3]]),
-            AddNum(7),
-            GetIntervals(&[[1, 1], [3, 3], [7, 7]]),
-            AddNum(2),
-            GetIntervals(&[[1, 3], [7, 7]]),
-            AddNum(6),
-            GetIntervals(&[[1, 3], [6, 7]]),
-        ] as &[_]];
+        let test_cases = [
+            &[
+                AddNum(1),
+                GetIntervals(&[[1, 1]]),
+                AddNum(3),
+                GetIntervals(&[[1, 1], [3, 3]]),
+                AddNum(7),
+                GetIntervals(&[[1, 1], [3, 3], [7, 7]]),
+                AddNum(2),
+                GetIntervals(&[[1, 3], [7, 7]]),
+                AddNum(6),
+                GetIntervals(&[[1, 3], [6, 7]]),
+            ] as &[_],
+            &[
+                AddNum(6),
+                GetIntervals(&[[6, 6]]),
+                AddNum(6),
+                GetIntervals(&[[6, 6]]),
+                AddNum(0),
+                GetIntervals(&[[0, 0], [6, 6]]),
+                AddNum(4),
+                GetIntervals(&[[0, 0], [4, 4], [6, 6]]),
+                AddNum(8),
+                GetIntervals(&[[0, 0], [4, 4], [6, 6], [8, 8]]),
+                AddNum(7),
+                GetIntervals(&[[0, 0], [4, 4], [6, 8]]),
+                AddNum(6),
+                GetIntervals(&[[0, 0], [4, 4], [6, 8]]),
+                AddNum(4),
+                GetIntervals(&[[0, 0], [4, 4], [6, 8]]),
+                AddNum(7),
+                GetIntervals(&[[0, 0], [4, 4], [6, 8]]),
+                AddNum(5),
+                GetIntervals(&[[0, 0], [4, 8]]),
+            ],
+        ];
 
         for operations in test_cases.iter().copied() {
             let mut summary_ranges = S::new();
---
+++ src/problem_0352_data_stream_as_disjoint_intervals/ordered_map.rs
@@ -0,0 +1,77 @@
+use std::cmp::Ordering;
+use std::collections::BTreeMap;
+
+pub struct SummaryRanges {
+    intervals: BTreeMap<i32, i32>,
+}
+
+impl SummaryRanges {
+    fn new() -> Self {
+        Self {
+            intervals: BTreeMap::new(),
+        }
+    }
+
+    fn add_num(&mut self, val: i32) {
+        if let Some((right_from, right_to)) =
+            self.intervals
+                .range(val + 1..)
+                .next()
+                .and_then(|(&from, &to)| if from == val + 1 { Some((from, to)) } else { None })
+        {
+            if let Some(left_to) =
+                self.intervals.range_mut(..val).next_back().and_then(
+                    |(_, to)| {
+                        if *to == val - 1 {
+                            Some(to)
+                        } else {
+                            None
+                        }
+                    },
+                )
+            {
+                *left_to = right_to;
+            } else {
+                self.intervals.insert(val, right_to);
+            }
+
+            self.intervals.remove(&right_from);
+        } else if let Some((_, left_to)) = self.intervals.range_mut(..val).next_back() {
+            match (*left_to).cmp(&(val - 1)) {
+                Ordering::Less => {
+                    self.intervals.entry(val).or_insert(val);
+                }
+                Ordering::Equal => *left_to = val,
+                Ordering::Greater => {}
+            }
+        } else {
+            self.intervals.entry(val).or_insert(val);
+        }
+    }
+
+    fn get_intervals(&self) -> Vec<Vec<i32>> {
+        self.intervals.iter().map(|(&from, &to)| vec![from, to]).collect()
+    }
+}
+
+impl super::SummaryRanges for SummaryRanges {
+    fn new() -> Self {
+        Self::new()
+    }
+
+    fn add_num(&mut self, val: i32) {
+        self.add_num(val)
+    }
+
+    fn get_intervals(&self) -> Vec<Vec<i32>> {
+        self.get_intervals()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::SummaryRanges>();
+    }
+}
