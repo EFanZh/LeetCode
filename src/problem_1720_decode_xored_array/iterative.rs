4db7ec1443ba35f7eba1e46816418e29ad5eb583
EFanZh <efanzh@gmail.com>
2021-01-19 08:21:20 +0800

    Add problem 1720: Decode XORed Array

---
+++ src/problem_1720_decode_xored_array/iterative.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+        let mut result = Vec::with_capacity(encoded.len() + 1);
+        let mut prev = first;
+        let mut iter = encoded.into_iter();
+
+        loop {
+            result.push(prev);
+
+            if let Some(value) = iter.next() {
+                prev ^= value;
+            } else {
+                break;
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32> {
+        Self::decode(encoded, first)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1720_decode_xored_array/mod.rs
@@ -0,0 +1,22 @@
+pub mod iterative;
+pub mod iterative_2;
+
+pub trait Solution {
+    fn decode(encoded: Vec<i32>, first: i32) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3] as &[_], 1), &[1, 0, 2, 1] as &[_]),
+            ((&[6, 2, 7, 3], 4), &[4, 2, 0, 7, 4]),
+        ];
+
+        for ((encoded, first), expected) in test_cases.iter().copied() {
+            assert_eq!(S::decode(encoded.to_vec(), first), expected);
+        }
+    }
+}
