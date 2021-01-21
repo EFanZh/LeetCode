1be8354bb7c59e5c356b081fa76ad6835942a366
EFanZh <efanzh@gmail.com>
2021-01-21 08:59:00 +0800

    Add problem 433: Minimum Genetic Mutation

---
+++ src/problem_0433_minimum_genetic_mutation/bidirectional_bfs.rs
@@ -0,0 +1,104 @@
+pub struct Solution;
+
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, VecDeque};
+use std::iter;
+
+impl Solution {
+    fn gene_to_u16(gene: &str) -> u16 {
+        let mut result = 0;
+
+        for (i, byte) in (0..15).step_by(2).rev().zip(gene.bytes()) {
+            match byte {
+                b'A' => {}
+                b'C' => result |= 1 << i,
+                b'G' => result |= 2 << i,
+                _ => result |= 3 << i,
+            }
+        }
+
+        result
+    }
+
+    fn iterator_groups(gene: u16) -> impl Iterator<Item = (u16, u16)> {
+        (0..15).step_by(2).map(move |i| (i, gene & !(3 << i)))
+    }
+
+    pub fn min_mutation(start: String, end: String, bank: Vec<String>) -> i32 {
+        if bank.contains(&end) {
+            let start = Self::gene_to_u16(&start);
+            let end = Self::gene_to_u16(&end);
+            let mut graph = HashMap::new();
+
+            // Build graph.
+
+            for gene in bank.iter().map(String::as_str).map(Self::gene_to_u16) {
+                for key in Self::iterator_groups(gene) {
+                    match graph.entry(key) {
+                        Entry::Vacant(entry) => {
+                            entry.insert(vec![gene]);
+                        }
+                        Entry::Occupied(entry) => {
+                            entry.into_mut().push(gene);
+                        }
+                    }
+                }
+            }
+
+            // BFS.
+
+            let mut left_queue = iter::once((start, 0)).collect::<VecDeque<_>>();
+            let mut right_queue = iter::once((end, 1)).collect::<VecDeque<_>>();
+            let mut left_visited = iter::once((start, 0)).collect::<HashMap<_, _>>();
+            let mut right_visited = iter::once((end, 1)).collect::<HashMap<_, _>>();
+
+            while let Some((left, left_length)) = left_queue.pop_front() {
+                for key in Self::iterator_groups(left) {
+                    if let Some(nexts) = graph.get(&key) {
+                        for &next in nexts {
+                            if let Some(right_length) = right_visited.get(&next) {
+                                return left_length + right_length;
+                            } else if let Entry::Vacant(entry) = left_visited.entry(next) {
+                                entry.insert(left_length + 1);
+                                left_queue.push_back((next, left_length + 1));
+                            }
+                        }
+                    }
+                }
+
+                if let Some((right, right_length)) = right_queue.pop_front() {
+                    for key in Self::iterator_groups(right) {
+                        if let Some(nexts) = graph.get(&key) {
+                            for &next in nexts {
+                                if let Some(left_length) = left_visited.get(&next) {
+                                    return left_length + right_length;
+                                } else if let Entry::Vacant(entry) = right_visited.entry(next) {
+                                    entry.insert(right_length + 1);
+                                    right_queue.push_back((next, right_length + 1));
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        -1
+    }
+}
+
+impl super::Solution for Solution {
+    fn min_mutation(start: String, end: String, bank: Vec<String>) -> i32 {
+        Self::min_mutation(start, end, bank)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0433_minimum_genetic_mutation/mod.rs
@@ -0,0 +1,37 @@
+pub mod bidirectional_bfs;
+
+pub trait Solution {
+    fn min_mutation(start: String, end: String, bank: Vec<String>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("AACCGGTT", "AACCGGTA", &["AACCGGTA"] as &[_]), 1),
+            (("AACCGGTT", "AAACGGTA", &["AACCGGTA", "AACCGCTA", "AAACGGTA"]), 2),
+            (("AAAAACCC", "AACCCCCC", &["AAAACCCC", "AAACCCCC", "AACCCCCC"]), 3),
+            (
+                (
+                    "AGCAAAAA",
+                    "GACAAAAA",
+                    &["AGTAAAAA", "GGTAAAAA", "GATAAAAA", "GACAAAAA"],
+                ),
+                4,
+            ),
+        ];
+
+        for ((start, end, bank), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::min_mutation(
+                    start.to_string(),
+                    end.to_string(),
+                    bank.iter().copied().map(str::to_string).collect()
+                ),
+                expected
+            );
+        }
+    }
+}
