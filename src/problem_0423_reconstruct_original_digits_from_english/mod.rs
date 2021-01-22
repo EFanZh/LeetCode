e14f56cb3df3305f44599393229d74a02a9641ec
EFanZh <efanzh@gmail.com>
2021-01-22 09:07:25 +0800

    Add problem 423: Reconstruct Original Digits from English

---
+++ src/problem_0423_reconstruct_original_digits_from_english/mod.rs
@@ -0,0 +1,18 @@
+pub mod solve_equations;
+
+pub trait Solution {
+    fn original_digits(s: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [("owoztneoer", "012"), ("fviefuro", "45"), ("xsi", "6"), ("nnei", "9")];
+
+        for (s, expected) in test_cases.iter().copied() {
+            assert_eq!(S::original_digits(s.to_string()), expected);
+        }
+    }
+}
