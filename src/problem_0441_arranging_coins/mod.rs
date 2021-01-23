4c2e9ec848d82250d3dea18bc700c517ef8cfd86
EFanZh <efanzh@gmail.com>
2021-01-23 10:08:50 +0800

    Add problem 441: Arranging Coins

---
+++ src/problem_0441_arranging_coins/mod.rs
@@ -0,0 +1,44 @@
+pub mod newtons_method;
+pub mod smart_newtons_method;
+pub mod smarter_newtons_method;
+
+pub trait Solution {
+    fn arrange_coins(n: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (0, 0),
+            (1, 1),
+            (2, 1),
+            (3, 2),
+            (4, 2),
+            (5, 2),
+            (6, 3),
+            (7, 3),
+            (8, 3),
+            (9, 3),
+            (10, 4),
+            (11, 4),
+            (12, 4),
+            (13, 4),
+            (14, 4),
+            (15, 5),
+            (8254, 127),
+            (0x0200_0FFE, 8191),
+            (0x0800_0000, 16383),
+            (0x1FFF_FFFD, 32767),
+            (0x79AB_CDEF, 63894),
+            (0x7FED_CBA9, 65517),
+            (0x7FFF_FFFF, 65535),
+        ];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::arrange_coins(n), expected);
+        }
+    }
+}
---
+++ src/problem_0441_arranging_coins/newtons_method.rs
@@ -0,0 +1,28 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn arrange_coins(n: i32) -> i32 {
+        let n = i64::from(n);
+        let mut guess = n;
+
+        while guess * (guess + 1) > n * 2 {
+            guess = (guess * guess + n * 2) / (guess * 2 + 1);
+        }
+
+        guess as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn arrange_coins(n: i32) -> i32 {
+        Self::arrange_coins(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
