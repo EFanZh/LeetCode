5084406bf8d3d5f7bc8cbcd6339f46bc1042fa3d
EFanZh <efanzh@gmail.com>
2021-01-26 09:18:45 +0800

    Add a new solution to problem 69: Sqrt(x)

---
+++ src/problem_0069_sqrtx/newtons_method_with_f32.rs
@@ -0,0 +1,36 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {
+        let x = x as u32;
+        let mut guess = f32::from_bits((x as f32).to_bits() / 2 + 0x1FC0_0000) as u32;
+
+        if guess * guess > x {
+            guess = (guess + x / guess) / 2;
+
+            if guess * guess > x {
+                guess = (guess + x / guess) / 2;
+
+                if guess * guess > x {
+                    guess = (guess + x / guess) / 2;
+                }
+            }
+        }
+
+        guess as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn my_sqrt(x: i32) -> i32 {
+        Self::my_sqrt(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
