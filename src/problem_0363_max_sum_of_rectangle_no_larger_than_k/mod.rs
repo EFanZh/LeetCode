e6a4a94e33d666ccff398c0e92026abb7068c349
EFanZh <efanzh@gmail.com>
2021-01-27 20:42:29 +0800

    Cover more codes

--- src/problem_0363_max_sum_of_rectangle_no_larger_than_k/mod.rs
+++ src/problem_0363_max_sum_of_rectangle_no_larger_than_k/mod.rs
@@ -9,7 +9,12 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((&[&[1, 0, 1] as &[_], &[0, -2, 3]] as &[&[_]], 2), 2)];
+        let test_cases = [
+            ((&[&[1, 0, 1] as &[_], &[0, -2, 3]] as &[&[_]], 2), 2),
+            ((&[&[5, -4, -3, 4], &[-3, -4, 4, 5], &[5, 1, 5, -4]], 10), 10),
+            ((&[&[5, -3, 5], &[-4, -4, 1], &[-3, 4, 5], &[4, 5, 4]], 10), 10),
+            ((&[&[2, 2, -1]], 3), 3),
+        ];
 
         for ((matrix, k), expected) in test_cases.iter().copied() {
             assert_eq!(
