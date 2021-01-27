b1f1cec8579feb0a296c1b4a41032c1afbb84964
EFanZh <efanzh@gmail.com>
2021-01-27 09:04:46 +0800

    Add problem 442: Find All Duplicates in an Array

---
+++ src/problem_0442_find_all_duplicates_in_an_array/highest_bit_as_marker.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
+        const MARKER: i32 = 1 << 31;
+        let mut result = Vec::new();
+
+        for i in 0..nums.len() {
+            let num = nums[i] & !MARKER;
+            let target = &mut nums[(num - 1) as usize];
+
+            if *target & MARKER == 0 {
+                *target |= MARKER;
+            } else {
+                result.push(num);
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_duplicates(nums: Vec<i32>) -> Vec<i32> {
+        Self::find_duplicates(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0442_find_all_duplicates_in_an_array/mod.rs
@@ -0,0 +1,27 @@
+pub mod highest_bit_as_marker;
+pub mod pigeonhole;
+
+pub trait Solution {
+    fn find_duplicates(nums: Vec<i32>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[4, 3, 2, 7, 8, 2, 3, 1] as &[_], &[2, 3] as &[_]),
+            (&[1, 1], &[1]),
+            (&[3, 4, 2, 4], &[4]),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                test_utilities::unstable_sorted(S::find_duplicates(nums.to_vec())),
+                expected
+            );
+        }
+    }
+}
