5ead8007d05b4b2813c51c7a89bad2862eb6019f
EFanZh <efanzh@gmail.com>
2021-01-28 08:58:56 +0800

    Add problem 424: Longest Repeating Character Replacement

---
+++ src/problem_0424_longest_repeating_character_replacement/mod.rs
@@ -0,0 +1,19 @@
+pub mod sliding_window;
+pub mod sliding_window_2;
+
+pub trait Solution {
+    fn character_replacement(s: String, k: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(("ABAB", 2), 4), (("AABABBA", 1), 4), (("ABCDEFG", 6), 7)];
+
+        for ((s, k), expected) in test_cases.iter().copied() {
+            assert_eq!(S::character_replacement(s.to_string(), k), expected);
+        }
+    }
+}
