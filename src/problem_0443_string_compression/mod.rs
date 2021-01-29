78bfb2a63f206d08ec31c67ee510b6ae8c9eddbb
EFanZh <efanzh@gmail.com>
2021-01-29 08:56:41 +0800

    Add problem 443: String Compression

---
+++ src/problem_0443_string_compression/mod.rs
@@ -0,0 +1,28 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn compress(chars: &mut Vec<char>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("aabbccc", "a2b2c3"),
+            ("a", "a"),
+            ("abbbbbbbbbbbb", "ab12"),
+            ("aaabbaa", "a3b2a2"),
+            ("aaabaa", "a3ba2"),
+            ("aaabba", "a3b2a"),
+        ];
+
+        for (s, expected) in test_cases.iter().copied() {
+            let mut s = s.chars().collect();
+            let result_length = S::compress(&mut s);
+
+            assert_eq!(s[..result_length as _].iter().copied().collect::<String>(), expected);
+        }
+    }
+}
