b8a1cb8d2a563ad1a100cff8d2e165d0bd276836
EFanZh <efanzh@gmail.com>
2021-01-31 21:53:45 +0800

    Add coverage task

---
+++ xtask/src/main.rs
@@ -0,0 +1,28 @@
+use crate::coverage::CoverageCommand;
+use std::env;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod coverage;
+mod tools;
+
+#[derive(StructOpt)]
+enum Config {
+    Coverage(CoverageCommand),
+}
+
+fn get_project_root() -> PathBuf {
+    let mut path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
+    path.pop();
+
+    path
+}
+
+fn main() {
+    assert_eq!(env::current_dir().unwrap(), get_project_root());
+
+    match Config::from_args() {
+        Config::Coverage(command) => command.run(),
+    }
+}
