c870be3b600039ed5388129324589aa7b7d36606
EFanZh <efanzh@gmail.com>
2021-02-03 00:18:07 +0800

    Add some test cases to problem 146: LRU Cache

--- src/problem_0146_lru_cache/mod.rs
+++ src/problem_0146_lru_cache/mod.rs
@@ -53,6 +53,31 @@ mod tests {
                 ],
             ),
             (2, &[Put(1, 2), Get(1, 2), Put(1, 3), Get(1, 3)]),
+            (
+                10,
+                &[
+                    Put(7, 28),
+                    Put(7, 1),
+                    Put(8, 15),
+                    Get(6, -1),
+                    Put(10, 27),
+                    Put(8, 10),
+                    Get(8, 10),
+                    Put(6, 29),
+                    Put(1, 9),
+                    Get(6, 29),
+                    Put(10, 7),
+                    Get(1, 9),
+                    Get(2, -1),
+                    Get(13, -1),
+                    Put(8, 30),
+                    Put(1, 5),
+                    Get(1, 5),
+                    Put(13, 2),
+                    Get(12, -1),
+                ],
+            ),
+            (2, &[Put(2, 1), Put(1, 1), Put(2, 3), Put(4, 1), Get(1, -1), Get(2, 3)]),
         ];
 
         for (capacity, operations) in test_cases.iter().copied() {
