828a82c7806c274ff43b8c381eafda6d92f7cbfe
EFanZh <efanzh@gmail.com>
2021-02-03 09:01:51 +0800

    Add problem 1743: Restore the Array From Adjacent Pairs

---
+++ src/problem_1743_restore_the_array_from_adjacent_pairs/mod.rs
@@ -0,0 +1,24 @@
+pub mod dfs;
+
+pub trait Solution {
+    fn restore_array(adjacent_pairs: Vec<Vec<i32>>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[[2, 1], [3, 4], [3, 2]] as &[_], &[1, 2, 3, 4] as &[_]),
+            (&[[4, -2], [1, 4], [-3, 1]], &[-2, 4, 1, -3]),
+            (&[[100_000, -100_000]], &[100_000, -100_000]),
+        ];
+
+        for (adjacent_pairs, expected) in test_cases.iter().copied() {
+            let result = S::restore_array(adjacent_pairs.iter().map(|pair| pair.to_vec()).collect());
+
+            assert!(result == expected || result.iter().eq(expected.iter().rev()));
+        }
+    }
+}
