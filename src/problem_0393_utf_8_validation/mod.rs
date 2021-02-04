ae12429bb5874d17733f759c05c4fa08b2a83db7
EFanZh <efanzh@gmail.com>
2021-02-04 23:44:09 +0800

    Add some test cases to problem 393: UTF-8 Validation

--- src/problem_0393_utf_8_validation/mod.rs
+++ src/problem_0393_utf_8_validation/mod.rs
@@ -9,7 +9,15 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[197, 130, 1] as &[_], true), (&[235, 140, 4], false)];
+        let test_cases = [
+            (&[197, 130, 1] as &[_], true),
+            (&[235, 140, 4], false),
+            (&[206, 210, 189, 208, 197, 163, 182, 171, 212, 243, 10, 0, 10], false),
+            (&[230, 136, 145], true),
+            (&[240, 162, 138, 147], true),
+            (&[240, 255], false),
+            (&[255], false),
+        ];
 
         for (data, expected) in test_cases.iter().copied() {
             assert_eq!(S::valid_utf8(data.to_vec()), expected);
