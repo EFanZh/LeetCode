d7d0dfb004abc67f3505ba164194ea395b075f0e
EFanZh <efanzh@gmail.com>
2021-02-04 23:26:42 +0800

    Add some test cases

--- src/problem_0094_binary_tree_inorder_traversal/mod.rs
+++ src/problem_0094_binary_tree_inorder_traversal/mod.rs
@@ -18,7 +18,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[Some(1), None, Some(2), Some(3)] as &[_], &[1, 3, 2] as &[_])];
+        let test_cases = [
+            (&[Some(1), None, Some(2), Some(3)] as &[_], &[1, 3, 2] as &[_]),
+            (&[Some(3), Some(1), None, None, Some(2)], &[1, 2, 3]),
+            (&[], &[]),
+        ];
 
         for (root, expected) in test_cases.iter().copied() {
             assert_eq!(
