530d97da95070343df58616b1bf2f8b2ab961568
EFanZh <efanzh@gmail.com>
2021-02-05 08:59:47 +0800

    Add problem 449: Serialize and Deserialize BST

---
+++ src/problem_0449_serialize_and_deserialize_bst/mod.rs
@@ -0,0 +1,30 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod preorder_traversal;
+
+pub trait Codec {
+    fn new() -> Self;
+    fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String;
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Codec;
+
+    pub fn run<C: Codec>() {
+        let test_cases = [&[Some(2), Some(1), Some(3)] as &[_], &[]];
+
+        for root in test_cases.iter().copied() {
+            let codec = C::new();
+            let root = test_utilities::make_tree(root.iter().copied());
+            let data = codec.serialize(root.clone());
+            let recovered_root = codec.deserialize(data);
+
+            assert_eq!(recovered_root, root);
+        }
+    }
+}
