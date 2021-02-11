6e66b21fef1dabb383d04e35bf10298b3ff7e9dd
EFanZh <efanzh@gmail.com>
2021-02-11 09:06:17 +0800

    Add problem 455: Assign Cookies

---
+++ src/problem_0455_assign_cookies/greedy.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
+        g.sort_unstable();
+        s.sort_unstable();
+
+        let mut s = s.into_iter();
+        let mut result = 0;
+
+        for greed in g {
+            loop {
+                if let Some(size) = s.next() {
+                    if greed <= size {
+                        result += 1;
+
+                        break;
+                    }
+                } else {
+                    return result;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_content_children(g: Vec<i32>, s: Vec<i32>) -> i32 {
+        Self::find_content_children(g, s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
