629ddce6af6f16646dac82ede2261ed916b1ab77
EFanZh <efanzh@gmail.com>
2021-02-12 10:08:48 +0800

    Add problem 461: Hamming Distance

---
+++ src/problem_0461_hamming_distance/mod.rs
@@ -0,0 +1,18 @@
+pub mod xor;
+
+pub trait Solution {
+    fn hamming_distance(x: i32, y: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [((1, 4), 2)];
+
+        for ((x, y), expected) in test_cases.iter().copied() {
+            assert_eq!(S::hamming_distance(x, y), expected);
+        }
+    }
+}
