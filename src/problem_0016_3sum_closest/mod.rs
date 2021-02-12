5015e9c60a121b0275d79a2b630249722ea7e7c8
EFanZh <efanzh@gmail.com>
2021-02-12 13:46:45 +0800

    Add some test cases

--- src/problem_0016_3sum_closest/mod.rs
+++ src/problem_0016_3sum_closest/mod.rs
@@ -9,7 +9,12 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((&[-1, 2, 1, -4] as &[_], 1), 2)];
+        let test_cases = [
+            ((&[-1, 2, 1, -4] as &[_], 1), 2),
+            ((&[1, 1, 1, 1], 0), 3),
+            ((&[1, 1, -1, -1, 3], 1), 1),
+            ((&[0, 2, 1, -3], 1), 0),
+        ];
 
         for ((nums, target), expected) in test_cases.iter().copied() {
             assert_eq!(S::three_sum_closest(nums.iter().copied().collect(), target), expected);
