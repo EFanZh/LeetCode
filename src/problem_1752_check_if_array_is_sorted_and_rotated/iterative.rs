e9debd33e35512e471f277a60ac32d769bffa03d
EFanZh <efanzh@gmail.com>
2021-02-13 12:16:57 +0800

    Add problem 1752: Check if Array Is Sorted and Rotated

---
+++ src/problem_1752_check_if_array_is_sorted_and_rotated/iterative.rs
@@ -0,0 +1,50 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn check(nums: Vec<i32>) -> bool {
+        nums.split_first().map_or(true, |(&first, rest)| {
+            let mut prev = first;
+            let mut iter = rest.iter().copied();
+
+            while let Some(mut num) = iter.next() {
+                if num < prev {
+                    loop {
+                        if num > first {
+                            return false;
+                        }
+
+                        prev = num;
+
+                        if let Some(next) = iter.next() {
+                            if next < prev {
+                                return false;
+                            }
+
+                            num = next;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+
+                prev = num
+            }
+
+            true
+        })
+    }
+}
+
+impl super::Solution for Solution {
+    fn check(nums: Vec<i32>) -> bool {
+        Self::check(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
