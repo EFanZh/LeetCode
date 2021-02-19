1d762de214e4a2cf6930af12b66181607b1413f0
EFanZh <efanzh@gmail.com>
2021-02-19 08:13:28 +0800

    Add problem 739: Daily Temperatures

---
+++ src/problem_0739_daily_temperatures/stack.rs
@@ -0,0 +1,38 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
+        let mut result = vec![0; t.len()];
+        let mut stack = Vec::new();
+
+        for (i, (target, &value)) in result.iter_mut().zip(&t).enumerate().rev() {
+            while let Some(&top) = stack.last() {
+                if t[top] <= value {
+                    stack.pop();
+                } else {
+                    *target = (top - i) as _;
+
+                    break;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
+        Self::daily_temperatures(t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
