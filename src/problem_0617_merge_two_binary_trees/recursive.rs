80e175bbdd55f5cc817de846a6aeaf8cf5d427b6
EFanZh <efanzh@gmail.com>
2021-02-21 08:22:29 +0800

    Add problem 617: Merge Two Binary Trees

---
+++ src/problem_0617_merge_two_binary_trees/recursive.rs
@@ -0,0 +1,50 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn merge_to(target: &mut Option<Rc<RefCell<TreeNode>>>, tree: Option<Rc<RefCell<TreeNode>>>) {
+        if let Some(tree) = tree {
+            if let Some(target) = target {
+                let mut target = target.borrow_mut();
+                let mut tree = tree.borrow_mut();
+
+                target.val += tree.val;
+
+                Self::merge_to(&mut target.left, tree.left.take());
+                Self::merge_to(&mut target.right, tree.right.take());
+            } else {
+                *target = Some(tree);
+            }
+        }
+    }
+
+    pub fn merge_trees(
+        mut root1: Option<Rc<RefCell<TreeNode>>>,
+        root2: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::merge_to(&mut root1, root2);
+
+        root1
+    }
+}
+
+impl super::Solution for Solution {
+    fn merge_trees(
+        root1: Option<Rc<RefCell<TreeNode>>>,
+        root2: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::merge_trees(root1, root2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
