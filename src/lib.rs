63b30345e71e77a8a539b7a9b956d696769a0c63
EFanZh <efanzh@gmail.com>
2021-02-24 08:38:16 +0800

    Add problem 463: Island Perimeter

--- src/lib.rs
+++ src/lib.rs
@@ -344,6 +344,7 @@ pub mod problem_0453_minimum_moves_to_equal_array_elements;
 pub mod problem_0454_4sum_ii;
 pub mod problem_0455_assign_cookies;
 pub mod problem_0461_hamming_distance;
+pub mod problem_0463_island_perimeter;
 pub mod problem_0476_number_complement;
 pub mod problem_0494_target_sum;
 pub mod problem_0509_fibonacci_number;
---
+++ src/problem_0463_island_perimeter/brute_force.rs
@@ -0,0 +1,45 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let mut result = 0;
+
+        for (i, row) in grid.iter().enumerate() {
+            for (j, &cell) in row.iter().enumerate() {
+                if cell != 0 {
+                    if grid.get(i.wrapping_sub(1)).map_or(true, |r| r[j] == 0) {
+                        result += 1;
+                    }
+
+                    if row.get(j.wrapping_sub(1)).map_or(true, |&c| c == 0) {
+                        result += 1;
+                    }
+
+                    if row.get(j + 1).map_or(true, |&c| c == 0) {
+                        result += 1;
+                    }
+
+                    if grid.get(i + 1).map_or(true, |r| r[j] == 0) {
+                        result += 1;
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        Self::island_perimeter(grid)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
