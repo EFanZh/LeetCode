5acfc8a7022a6d5fae68b05cb1467e1778f92f28
EFanZh <efanzh@gmail.com>
2021-03-01 08:48:51 +0800

    Add problem 504: Base 7

---
+++ src/problem_0504_base_7/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn convert_to_base7(num: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(100, "202"), (-7, "-10")];
+
+        for (num, expected) in test_cases.iter().copied() {
+            assert_eq!(S::convert_to_base7(num), expected);
+        }
+    }
+}
