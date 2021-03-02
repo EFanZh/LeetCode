659106497dfdf99f8d159c14fd30dc2ba34e4049
EFanZh <efanzh@gmail.com>
2021-03-03 00:04:17 +0800

    Add some test cases

--- src/problem_0167_two_sum_ii_input_array_is_sorted/mod.rs
+++ src/problem_0167_two_sum_ii_input_array_is_sorted/mod.rs
@@ -9,7 +9,12 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((&[2, 7, 11, 15] as &[_], 9), [1, 2])];
+        let test_cases = [
+            ((&[2, 7, 11, 15] as &[_], 9), [1, 2]),
+            ((&[2, 3, 4], 6), [1, 3]),
+            ((&[-1, 0], -1), [1, 2]),
+            ((&[5, 25, 75], 100), [2, 3]),
+        ];
 
         for ((numbers, target), expected) in test_cases.iter().copied() {
             assert_eq!(S::two_sum(numbers.to_vec(), target), expected);
