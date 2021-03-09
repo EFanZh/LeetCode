8bde43bd60247b3d537ba5bea8e269c24c2e4a4a
EFanZh <efanzh@gmail.com>
2021-03-09 08:39:35 +0800

    Add problem 1773: Count Items Matching a Rule

---
+++ src/problem_1773_count_items_matching_a_rule/iterative.rs
@@ -0,0 +1,27 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+        let rule_index = match rule_key.as_str() {
+            "type" => 0,
+            "color" => 1,
+            _ => 2,
+        };
+
+        items.iter().filter(|item| item[rule_index] == rule_value).count() as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+        Self::count_matches(items, rule_key, rule_value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
