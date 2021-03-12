84bb7ca5b300bcc316a1adb30147159f9a597bb2
EFanZh <efanzh@gmail.com>
2021-03-12 08:44:14 +0800

    Add problem 541: Reverse String II

---
+++ src/problem_0541_reverse_string_ii/mod.rs
@@ -0,0 +1,18 @@
+pub mod reverse_by_chunks;
+
+pub trait Solution {
+    fn reverse_str(s: String, k: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(("abcdefg", 2), "bacdfeg"), (("abcd", 2), "bacd")];
+
+        for ((s, k), expected) in test_cases.iter().copied() {
+            assert_eq!(S::reverse_str(s.to_string(), k), expected);
+        }
+    }
+}
