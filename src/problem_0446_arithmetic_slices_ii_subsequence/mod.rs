032db405f42fb8f5b0ad99596556fad680a1bf60
EFanZh <efanzh@gmail.com>
2021-03-21 08:47:09 +0800

    Add problem 446: Arithmetic Slices II - Subsequence

---
+++ src/problem_0446_arithmetic_slices_ii_subsequence/mod.rs
@@ -0,0 +1,23 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn number_of_arithmetic_slices(a: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[2, 4, 6, 8, 10] as &[_], 7),
+            (&[2, 2, 3, 4], 2),
+            (&[0, 2_000_000_000, -294_967_296], 0),
+            (&[1, 1, 1, 1, 1], 16),
+        ];
+
+        for (a, expected) in test_cases.iter().copied() {
+            assert_eq!(S::number_of_arithmetic_slices(a.to_vec()), expected);
+        }
+    }
+}
