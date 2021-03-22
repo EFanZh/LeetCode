417b7a79b775dbc52e9abea8fae1ebc4c442682a
EFanZh <efanzh@gmail.com>
2021-03-22 08:31:51 +0800

    Add problem 456: 132 Pattern

---
+++ src/problem_0456_132_pattern/mod.rs
@@ -0,0 +1,27 @@
+pub mod stack;
+pub mod stack_2;
+
+pub trait Solution {
+    fn find132pattern(nums: Vec<i32>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[1, 2, 3, 4] as &[_], false),
+            (&[3, 1, 4, 2], true),
+            (&[-1, 3, 2, 0], true),
+            (&[40, 50, 25, 35, 15, 35, 20], true),
+            (&[3, 5, 0, 3, 4], true),
+            (&[2, 2, 1, 3], false),
+            (&[26, 16, 16, 36, 36, 16, 16, 36, 36, 10, 10, 80, 80, 10, 10, 6], false),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find132pattern(nums.to_vec()), expected);
+        }
+    }
+}
---
+++ src/problem_0456_132_pattern/stack.rs
@@ -0,0 +1,70 @@
+pub struct Solution;
+
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn find132pattern(nums: Vec<i32>) -> bool {
+        nums.split_first().map_or(false, |(&first, rest)| {
+            let mut min = first;
+            let mut stack = Vec::with_capacity(nums.len() / 2);
+
+            for &num in rest {
+                loop {
+                    if let Some((top_low, top_high)) = stack.last() {
+                        match num.cmp(top_low) {
+                            Ordering::Less => {
+                                match num.cmp(&min) {
+                                    Ordering::Less => min = num,
+                                    Ordering::Equal => {}
+                                    Ordering::Greater => {
+                                        if num > min + 1 {
+                                            stack.push((min, num));
+                                        }
+                                    }
+                                }
+
+                                break;
+                            }
+                            Ordering::Equal => break,
+                            Ordering::Greater => {
+                                if num < *top_high {
+                                    return true;
+                                }
+
+                                stack.pop();
+                            }
+                        }
+                    } else {
+                        match num.cmp(&min) {
+                            Ordering::Less => min = num,
+                            Ordering::Equal => {}
+                            Ordering::Greater => {
+                                if num > min + 1 {
+                                    stack.push((min, num));
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            false
+        })
+    }
+}
+
+impl super::Solution for Solution {
+    fn find132pattern(nums: Vec<i32>) -> bool {
+        Self::find132pattern(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
