3c624b97127e08c6e5ed3e905999e28a111a1157
EFanZh <efanzh@gmail.com>
2021-03-25 08:44:42 +0800

    Add problem 563: Binary Tree Tilt

---
+++ src/problem_0563_binary_tree_tilt/recursive.rs
@@ -0,0 +1,39 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn helper(node: Option<&RefCell<TreeNode>>) -> (i32, i32) {
+        node.map_or((0, 0), |node| {
+            let node = node.borrow();
+            let (left_sum, left_tilt) = Self::helper(node.left.as_deref());
+            let (right_sum, right_tilt) = Self::helper(node.right.as_deref());
+
+            (
+                left_sum + right_sum + node.val,
+                left_tilt + right_tilt + (right_sum - left_sum).abs(),
+            )
+        })
+    }
+
+    pub fn find_tilt(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::helper(root.as_deref()).1
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_tilt(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::find_tilt(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
