9ef70d576eab95d929c651235d28d4ef5273bbc3
EFanZh <efanzh@gmail.com>
2021-03-26 13:47:36 +0800

    Add problem 661: Image Smoother

---
+++ src/problem_0661_image_smoother/mod.rs
@@ -0,0 +1,21 @@
+pub mod brute_force;
+
+pub trait Solution {
+    fn image_smoother(m: Vec<Vec<i32>>) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(
+            &[&[1, 1, 1] as &[_], &[1, 0, 1], &[1, 1, 1]] as &[&[_]],
+            &[&[0, 0, 0] as &[_], &[0, 0, 0], &[0, 0, 0]] as &[&[_]],
+        )];
+
+        for (m, expected) in test_cases.iter().copied() {
+            assert_eq!(S::image_smoother(m.iter().map(|row| row.to_vec()).collect()), expected);
+        }
+    }
+}
