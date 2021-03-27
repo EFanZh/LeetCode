fdbcbca5173a4bd3a8aaace3e1cd20c1a08af683
EFanZh <efanzh@gmail.com>
2021-03-27 10:30:05 +0800

    Add problem 566: Reshape the Matrix

---
+++ src/problem_0566_reshape_the_matrix/iterative.rs
@@ -0,0 +1,32 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn matrix_reshape(nums: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+        let r = r as usize;
+        let c = c as usize;
+        let old_rows = nums.len();
+        let old_columns = nums.first().map_or(0, Vec::len);
+
+        if old_rows * old_columns == r * c && old_rows != r {
+            let mut iter = nums.into_iter().flatten();
+
+            (0..r).map(|_| iter.by_ref().take(c).collect()).collect()
+        } else {
+            nums
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn matrix_reshape(nums: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+        Self::matrix_reshape(nums, r, c)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0566_reshape_the_matrix/mod.rs
@@ -0,0 +1,27 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn matrix_reshape(nums: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (&[&[1, 2] as &[_], &[3, 4]] as &[&[_]], 1, 4),
+                &[&[1, 2, 3, 4] as &[_]] as &[&[_]],
+            ),
+            ((&[&[1, 2], &[3, 4]], 2, 4), &[&[1, 2], &[3, 4]]),
+        ];
+
+        for ((nums, r, c), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::matrix_reshape(nums.iter().copied().map(<[_]>::to_vec).collect(), r, c),
+                expected
+            );
+        }
+    }
+}
