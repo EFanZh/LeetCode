77aebfa925c8670bf64bdb790eff0201e5af60b2
EFanZh <efanzh@gmail.com>
2021-03-30 08:46:39 +0800

    Add problem 572: Subtree of Another Tree

---
+++ src/problem_0572_subtree_of_another_tree/hashing.rs
@@ -0,0 +1,70 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::rc::Rc;
+
+impl Solution {
+    fn hash<T: Hash>(value: &T) -> u64 {
+        let mut hasher = DefaultHasher::default();
+
+        value.hash(&mut hasher);
+
+        hasher.finish()
+    }
+
+    fn hash_tree(tree: Option<&RefCell<TreeNode>>) -> u64 {
+        Self::hash(&tree.map(|node| {
+            let node = node.borrow();
+
+            (
+                node.val,
+                Self::hash_tree(node.left.as_deref()),
+                Self::hash_tree(node.right.as_deref()),
+            )
+        }))
+    }
+
+    fn helper(s: Option<&RefCell<TreeNode>>, t: Option<&RefCell<TreeNode>>, target_hash: u64) -> Option<u64> {
+        let hash = Self::hash(&if let Some(node) = s {
+            let node = node.borrow();
+
+            Some((
+                node.val,
+                Self::helper(node.left.as_deref(), t, target_hash)?,
+                Self::helper(node.right.as_deref(), t, target_hash)?,
+            ))
+        } else {
+            None
+        });
+
+        if hash == target_hash && s == t {
+            None
+        } else {
+            Some(hash)
+        }
+    }
+
+    pub fn is_subtree(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        let target_hash = Self::hash_tree(t.as_deref());
+
+        Self::helper(s.as_deref(), t.as_deref(), target_hash).is_none()
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_subtree(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Self::is_subtree(s, t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0572_subtree_of_another_tree/mod.rs
@@ -0,0 +1,44 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod hashing;
+
+pub trait Solution {
+    fn is_subtree(s: Option<Rc<RefCell<TreeNode>>>, t: Option<Rc<RefCell<TreeNode>>>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[Some(3), Some(4), Some(5), Some(1), Some(2)] as &[_],
+                    &[Some(4), Some(1), Some(2)] as &[_],
+                ),
+                true,
+            ),
+            (
+                (
+                    &[Some(3), Some(4), Some(5), Some(1), Some(2), None, None, None, Some(0)],
+                    &[Some(4), Some(1), Some(2)],
+                ),
+                false,
+            ),
+        ];
+
+        for ((s, t), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::is_subtree(
+                    test_utilities::make_tree(s.iter().copied()),
+                    test_utilities::make_tree(t.iter().copied())
+                ),
+                expected
+            );
+        }
+    }
+}
