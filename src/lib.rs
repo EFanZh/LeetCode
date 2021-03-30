b1034f725d5f42913c33a3d933faadd7c4ee334f
EFanZh <efanzh@gmail.com>
2021-03-30 08:46:39 +0800

    Add problem 572: Subtree of Another Tree

--- src/lib.rs
+++ src/lib.rs
@@ -385,6 +385,7 @@ pub mod problem_0560_subarray_sum_equals_k;
 pub mod problem_0561_array_partition_i;
 pub mod problem_0563_binary_tree_tilt;
 pub mod problem_0566_reshape_the_matrix;
+pub mod problem_0572_subtree_of_another_tree;
 pub mod problem_0617_merge_two_binary_trees;
 pub mod problem_0621_task_scheduler;
 pub mod problem_0647_palindromic_substrings;
