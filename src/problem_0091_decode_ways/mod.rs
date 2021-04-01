41dbee2faf9d386f0788e98f7e9c128e19ad8767
EFanZh <efanzh@gmail.com>
2021-04-01 23:20:59 +0800

    Add some test cases

--- src/problem_0091_decode_ways/mod.rs
+++ src/problem_0091_decode_ways/mod.rs
@@ -10,7 +10,7 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [("12", 2), ("226", 3), ("0", 0), ("", 1)];
+        let test_cases = [("12", 2), ("226", 3), ("0", 0), ("06", 0), ("", 1), ("27", 1)];
 
         for (s, expected) in test_cases.iter().copied() {
             assert_eq!(S::num_decodings(s.to_string()), expected);
