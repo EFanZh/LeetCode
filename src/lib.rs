ed0a8dd054e5ff657e9c5bd922a3829614f14c9d
EFanZh <efanzh@gmail.com>
2021-04-03 08:39:53 +0800

    Add problem 606: Construct String from Binary Tree

--- src/lib.rs
+++ src/lib.rs
@@ -389,6 +389,7 @@ pub mod problem_0572_subtree_of_another_tree;
 pub mod problem_0575_distribute_candies;
 pub mod problem_0594_longest_harmonious_subsequence;
 pub mod problem_0605_can_place_flowers;
+pub mod problem_0606_construct_string_from_binary_tree;
 pub mod problem_0617_merge_two_binary_trees;
 pub mod problem_0621_task_scheduler;
 pub mod problem_0647_palindromic_substrings;
