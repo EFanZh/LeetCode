0101dfcd2670a41b57cb38b1a03b31a31d8c3590
EFanZh <efanzh@gmail.com>
2021-04-05 10:52:04 +0800

    Add problem 628: Maximum Product of Three Numbers

---
+++ src/problem_0628_maximum_product_of_three_numbers/mod.rs
@@ -0,0 +1,18 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn maximum_product(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 2, 3] as &[_], 6), (&[1, 2, 3, 4], 24), (&[-1, -2, -3], -6)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::maximum_product(nums.to_vec()), expected);
+        }
+    }
+}
