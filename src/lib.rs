7aede5cc079c0bef6c53d3db929c5e6c0e4bbe91
EFanZh <efanzh@gmail.com>
2021-04-05 10:52:04 +0800

    Add problem 628: Maximum Product of Three Numbers

--- src/lib.rs
+++ src/lib.rs
@@ -393,6 +393,7 @@ pub mod problem_0606_construct_string_from_binary_tree;
 pub mod problem_0609_find_duplicate_file_in_system;
 pub mod problem_0617_merge_two_binary_trees;
 pub mod problem_0621_task_scheduler;
+pub mod problem_0628_maximum_product_of_three_numbers;
 pub mod problem_0647_palindromic_substrings;
 pub mod problem_0661_image_smoother;
 pub mod problem_0704_binary_search;
---
+++ src/problem_0628_maximum_product_of_three_numbers/dynamic_programming.rs
@@ -0,0 +1,51 @@
+pub struct Solution;
+
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn maximum_product(nums: Vec<i32>) -> i32 {
+        let (left, right) = nums.split_at(2);
+        let [first, second]: [i32; 2] = left.try_into().unwrap();
+
+        let (mut min_1, mut max_1) = if first < second {
+            (first, second)
+        } else {
+            (second, first)
+        };
+
+        let mut min_2 = first * second;
+        let mut max_2 = min_2;
+        let mut max_3 = i32::min_value();
+
+        for &num in right {
+            if num < 0 {
+                max_3 = max_3.max(min_2 * num);
+                min_2 = min_2.min(max_1 * num);
+                max_2 = max_2.max(min_1 * num);
+            } else {
+                max_3 = max_3.max(max_2 * num);
+                min_2 = min_2.min(min_1 * num);
+                max_2 = max_2.max(max_1 * num);
+            }
+
+            max_1 = max_1.max(num);
+            min_1 = min_1.min(num);
+        }
+
+        max_3
+    }
+}
+
+impl super::Solution for Solution {
+    fn maximum_product(nums: Vec<i32>) -> i32 {
+        Self::maximum_product(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
