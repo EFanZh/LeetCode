8fd9e7d8977d15debfca5aba20e0c7ed0ec28b7c
EFanZh <efanzh@gmail.com>
2021-04-09 08:49:22 +0800

    Add problem 515: Find Largest Value in Each Tree Row

---
+++ src/problem_0515_find_largest_value_in_each_tree_row/bfs.rs
@@ -0,0 +1,58 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut result = Vec::new();
+
+        if let Some(node) = root {
+            let mut queue = VecDeque::from(vec![node]);
+
+            loop {
+                let mut max_value = i32::min_value();
+
+                for _ in 0..queue.len() {
+                    let node = queue.pop_front().unwrap();
+                    let node = node.borrow();
+
+                    max_value = max_value.max(node.val);
+
+                    if let Some(left) = node.left.clone() {
+                        queue.push_back(left);
+                    }
+
+                    if let Some(right) = node.right.clone() {
+                        queue.push_back(right)
+                    };
+                }
+
+                result.push(max_value);
+
+                if queue.is_empty() {
+                    break;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        Self::largest_values(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
