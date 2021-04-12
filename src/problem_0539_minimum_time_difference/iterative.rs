9aece45117bd9ff81d4bb0cf7329f66a2c1fbc9a
EFanZh <efanzh@gmail.com>
2021-04-12 08:47:06 +0800

    Add problem 539: Minimum Time Difference

---
+++ src/problem_0539_minimum_time_difference/iterative.rs
@@ -0,0 +1,43 @@
+pub struct Solution;
+
+use std::convert::TryInto;
+
+impl Solution {
+    fn parse_time(s: &str) -> u16 {
+        let [h1, h2, _, m1, m2]: [u8; 5] = s.as_bytes().try_into().unwrap();
+
+        600 * u16::from(h1) + 60 * u16::from(h2) + 10 * u16::from(m1) + u16::from(m2) - 671 * u16::from(b'0')
+    }
+
+    pub fn find_min_difference(mut time_points: Vec<String>) -> i32 {
+        time_points.sort_unstable();
+
+        let mut result = u16::max_value();
+        let (first, rest) = time_points.split_first().unwrap();
+        let first_time = Self::parse_time(first);
+        let mut prev = first_time;
+
+        for time in rest {
+            let time = Self::parse_time(time);
+
+            result = result.min(time - prev);
+            prev = time;
+        }
+
+        result.min(first_time + (1440 - prev)).into()
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_min_difference(time_points: Vec<String>) -> i32 {
+        Self::find_min_difference(time_points)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
