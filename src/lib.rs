d6c81d9a5ca937aa01e8a7ea8244c3b99d0c1383
EFanZh <efanzh@gmail.com>
2021-04-15 08:38:03 +0800

    Add problem 477: Total Hamming Distance

--- src/lib.rs
+++ src/lib.rs
@@ -374,6 +374,7 @@ pub mod problem_0473_matchsticks_to_square;
 pub mod problem_0474_ones_and_zeroes;
 pub mod problem_0475_heaters;
 pub mod problem_0476_number_complement;
+pub mod problem_0477_total_hamming_distance;
 pub mod problem_0482_license_key_formatting;
 pub mod problem_0485_max_consecutive_ones;
 pub mod problem_0486_predict_the_winner;
---
+++ src/problem_0477_total_hamming_distance/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn total_hamming_distance(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[4, 14, 2] as &[_], 6), (&[4, 14, 4], 4)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::total_hamming_distance(nums.to_vec()), expected);
+        }
+    }
+}
