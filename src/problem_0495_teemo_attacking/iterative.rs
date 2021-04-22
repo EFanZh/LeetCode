73948a23844ab226b08c23cd989a5101d3ccf921
EFanZh <efanzh@gmail.com>
2021-04-22 08:40:35 +0800

    Add problem 495: Teemo Attacking

---
+++ src/problem_0495_teemo_attacking/iterative.rs
@@ -0,0 +1,34 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
+        let mut result = 0;
+        let mut poisoned = i32::min_value();
+        let mut healed = i32::min_value();
+
+        for time_point in time_series {
+            if time_point > healed {
+                result += healed - poisoned;
+                poisoned = time_point;
+            }
+
+            healed = time_point + duration;
+        }
+
+        result + healed - poisoned
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
+        Self::find_poisoned_duration(time_series, duration)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
