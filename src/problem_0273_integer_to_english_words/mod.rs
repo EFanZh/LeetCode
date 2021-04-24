5f36d837e8750f399715caa7db451356ee1ef7b4
EFanZh <efanzh@gmail.com>
2021-04-24 19:50:30 +0800

    Add some test cases

--- src/problem_0273_integer_to_english_words/mod.rs
+++ src/problem_0273_integer_to_english_words/mod.rs
@@ -14,6 +14,8 @@ mod tests {
             (12345, "Twelve Thousand Three Hundred Forty Five"),
             (1_234_567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"),
             (1_234_567_891, "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"),
+            (0, "Zero"),
+            (1000, "One Thousand")
         ];
 
         for (num, expected) in test_cases.iter().copied() {
