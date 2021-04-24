778273874be4746c83fd0c6b467455c4a969eaba
EFanZh <efanzh@gmail.com>
2021-04-24 20:09:36 +0800

    Refactor tree builder algorithm

--- src/test_utilities.rs
+++ src/test_utilities.rs
@@ -4,59 +4,44 @@ use std::collections::VecDeque;
 use std::iter;
 use std::rc::Rc;
 
-struct UniqueTreeNode {
-    val: i32,
-    left: Option<Box<UniqueTreeNode>>,
-    right: Option<Box<UniqueTreeNode>>,
-}
-
-fn make_unique_tree<I: IntoIterator<Item = Option<i32>>>(values: I) -> Option<Box<UniqueTreeNode>> {
-    let mut result = None;
-    let mut queue = VecDeque::new();
-    let mut current_node_ref = &mut result;
-
-    for maybe_value in values {
-        if let Some(value) = maybe_value {
-            *current_node_ref = Some(Box::new(UniqueTreeNode {
-                val: value,
-                left: None,
-                right: None,
-            }));
-
-            let (left_ref, right_ref) = current_node_ref
-                .as_mut()
-                .map(|node| (&mut node.left, &mut node.right))
-                .unwrap();
-
-            queue.push_back(left_ref);
-            queue.push_back(right_ref);
-        }
-
-        if let Some(new_current_node_ref) = queue.pop_front() {
-            current_node_ref = new_current_node_ref;
-        } else {
-            break;
-        }
-    }
-
-    result
-}
-
-fn unique_tree_to_tree(node: Option<Box<UniqueTreeNode>>) -> Option<Rc<RefCell<TreeNode>>> {
-    node.map(|real_node| {
-        let mut node = TreeNode::new(real_node.val);
-
-        node.left = unique_tree_to_tree(real_node.left);
-        node.right = unique_tree_to_tree(real_node.right);
-
-        Rc::new(RefCell::new(node))
-    })
-}
-
 pub fn make_tree<I: IntoIterator<Item = Option<i32>>>(values: I) -> Option<Rc<RefCell<TreeNode>>> {
-    let unique_treee = make_unique_tree(values);
+    let mut iter = values.into_iter();
 
-    unique_tree_to_tree(unique_treee)
+    iter.next().flatten().map(|first_value| {
+        let root = Rc::new(RefCell::new(TreeNode::new(first_value)));
+        let mut target = Rc::clone(&root);
+        let mut queue = VecDeque::new();
+
+        while let Some(item) = iter.next() {
+            let mut target_ref = target.borrow_mut();
+
+            if let Some(value) = item {
+                let left = Rc::new(RefCell::new(TreeNode::new(value)));
+
+                target_ref.left = Some(Rc::clone(&left));
+
+                queue.push_back(left);
+            }
+
+            if let Some(item) = iter.next() {
+                if let Some(value) = item {
+                    let right = Rc::new(RefCell::new(TreeNode::new(value)));
+
+                    target_ref.right = Some(Rc::clone(&right));
+
+                    queue.push_back(right);
+                }
+            } else {
+                break;
+            }
+
+            drop(target_ref);
+
+            target = queue.pop_front().unwrap();
+        }
+
+        root
+    })
 }
 
 pub fn make_list<I: IntoIterator<Item = i32>>(values: I) -> Option<Box<ListNode>> {
