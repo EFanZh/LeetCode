08081ea6f82918c987924322db20fe99ddc5b03b
EFanZh <efanzh@gmail.com>
2021-04-28 09:00:39 +0800

    Add problem 508: Most Frequent Subtree Sum

---
+++ src/problem_0508_most_frequent_subtree_sum/mod.rs
@@ -0,0 +1,31 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn find_frequent_tree_sum(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[Some(5), Some(2), Some(-3)] as &[_], &[-3, 2, 4] as &[_]),
+            (&[Some(5), Some(2), Some(-5)], &[2]),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                test_utilities::unstable_sorted(S::find_frequent_tree_sum(test_utilities::make_tree(
+                    root.iter().copied()
+                ))),
+                expected
+            );
+        }
+    }
+}
