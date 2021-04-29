6b16538c3ff6a7f37474e7b52e404113fc89f913
EFanZh <efanzh@gmail.com>
2021-04-29 08:54:16 +0800

    Add problem 513: Find Bottom Left Tree Value

---
+++ src/problem_0513_find_bottom_left_tree_value/bfs_2.rs
@@ -0,0 +1,48 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut node = root.unwrap();
+        let mut queue = VecDeque::new();
+
+        loop {
+            let node_ref = node.borrow();
+
+            if let Some(right) = node_ref.right.clone() {
+                queue.push_back(right);
+            }
+
+            if let Some(left) = node_ref.left.clone() {
+                queue.push_back(left);
+            }
+
+            if let Some(next) = queue.pop_front() {
+                drop(node_ref);
+
+                node = next;
+            } else {
+                return node_ref.val;
+            }
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::find_bottom_left_value(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
