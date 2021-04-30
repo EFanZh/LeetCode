89867e37ba980a3b9b93256a615470f9d8ae1f4c
EFanZh <efanzh@gmail.com>
2021-04-30 08:46:38 +0800

    Add problem 523: Continuous Subarray Sum

--- src/lib.rs
+++ src/lib.rs
@@ -401,6 +401,7 @@ pub mod problem_0516_longest_palindromic_subsequence;
 pub mod problem_0518_coin_change_2;
 pub mod problem_0520_detect_capital;
 pub mod problem_0521_longest_uncommon_subsequence_i;
+pub mod problem_0523_continuous_subarray_sum;
 pub mod problem_0535_encode_and_decode_tinyurl;
 pub mod problem_0537_complex_number_multiplication;
 pub mod problem_0539_minimum_time_difference;
