4ab015a4bbcec7ba45a9820a00ca564ecca2d549
EFanZh <efanzh@gmail.com>
2021-05-01 08:52:25 +0800

    Add problem 524: Longest Word in Dictionary through Deleting

---
+++ src/problem_0524_longest_word_in_dictionary_through_deleting/mod.rs
@@ -0,0 +1,24 @@
+pub mod trie;
+
+pub trait Solution {
+    fn find_longest_word(s: String, dictionary: Vec<String>) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (("abpcplea", &["ale", "apple", "monkey", "plea"] as &[_]), "apple"),
+            (("abpcplea", &["a", "b", "c"]), "a"),
+        ];
+
+        for ((s, dictionary), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::find_longest_word(s.to_string(), dictionary.iter().copied().map(str::to_string).collect()),
+                expected
+            );
+        }
+    }
+}
