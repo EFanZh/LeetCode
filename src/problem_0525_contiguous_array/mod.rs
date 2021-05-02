fdcc3cd779a2ad9be7041caf5b325db1eff4ffb5
EFanZh <efanzh@gmail.com>
2021-05-02 09:39:08 +0800

    Add problem 525: Contiguous Array

---
+++ src/problem_0525_contiguous_array/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn find_max_length(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[0, 1] as &[_], 2), (&[0, 1, 0], 2)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_max_length(nums.to_vec()), expected);
+        }
+    }
+}
