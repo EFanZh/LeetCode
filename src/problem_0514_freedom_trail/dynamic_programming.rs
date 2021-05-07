4f454219015e3d992e746d176b95fa1c4e89e5b4
EFanZh <efanzh@gmail.com>
2021-05-07 08:55:36 +0800

    Add problem 514: Freedom Trail

---
+++ src/problem_0514_freedom_trail/dynamic_programming.rs
@@ -0,0 +1,56 @@
+pub struct Solution;
+
+use std::mem;
+
+impl Solution {
+    pub fn find_rotate_steps(ring: String, key: String) -> i32 {
+        let ring_size = ring.len() as i32;
+        let mut indices = <[Vec<_>; 26]>::default();
+
+        for (i, c) in (0..).zip(ring.into_bytes()) {
+            indices[usize::from(c - b'a')].push(i);
+        }
+
+        let mut cache = vec![(0, 0)];
+        let mut temp = Vec::new();
+
+        for c in key.into_bytes() {
+            temp.extend(indices[usize::from(c - b'a')].iter().map(|&i| {
+                (
+                    i,
+                    cache
+                        .iter()
+                        .map(|&(j, value)| {
+                            let (x, y) = if j < i { (j, i) } else { (i, j) };
+                            let d = y - x;
+
+                            value + d.min(ring_size - d)
+                        })
+                        .min()
+                        .unwrap()
+                        + 1,
+                )
+            }));
+
+            mem::swap(&mut cache, &mut temp);
+
+            temp.clear();
+        }
+
+        cache.into_iter().map(|(_, value)| value).min().unwrap()
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_rotate_steps(ring: String, key: String) -> i32 {
+        Self::find_rotate_steps(ring, key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0514_freedom_trail/mod.rs
@@ -0,0 +1,18 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn find_rotate_steps(ring: String, key: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(("godding", "gd"), 4), (("godding", "godding"), 13)];
+
+        for ((ring, key), expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_rotate_steps(ring.to_string(), key.to_string()), expected);
+        }
+    }
+}
