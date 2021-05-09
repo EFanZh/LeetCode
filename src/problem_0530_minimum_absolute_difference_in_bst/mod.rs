5385d45d35e30059e3f356582e856235cf0fcb5e
EFanZh <efanzh@gmail.com>
2021-05-09 09:25:59 +0800

    Add problem 530: Minimum Absolute Difference in BST

---
+++ src/problem_0530_minimum_absolute_difference_in_bst/mod.rs
@@ -0,0 +1,29 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[Some(4), Some(2), Some(6), Some(1), Some(3)] as &[_], 1),
+            (&[Some(1), Some(0), Some(48), None, None, Some(12), Some(49)], 1),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::get_minimum_difference(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0530_minimum_absolute_difference_in_bst/recursive.rs
@@ -0,0 +1,44 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn helper(node: Option<&RefCell<TreeNode>>, prev: &mut Option<i32>, result: &mut i32) {
+        if let Some(node) = node.map(RefCell::borrow) {
+            Self::helper(node.left.as_deref(), prev, result);
+
+            if let Some(prev_val) = prev {
+                *result = (*result).min(node.val - *prev_val);
+            }
+
+            *prev = Some(node.val);
+
+            Self::helper(node.right.as_deref(), prev, result);
+        }
+    }
+
+    pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut result = i32::max_value();
+
+        Self::helper(root.as_deref(), &mut None, &mut result);
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::get_minimum_difference(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
