c0c3db2d120bebed9dc8c59fcb87f1d61d3b9ae0
EFanZh <efanzh@gmail.com>
2021-05-16 09:35:43 +0800

    Add problem 517: Super Washing Machines

---
+++ src/problem_0517_super_washing_machines/iterative.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_min_moves(machines: Vec<i32>) -> i32 {
+        let n = machines.len() as i32;
+        let sum = machines.iter().sum::<i32>();
+
+        if sum % n == 0 {
+            let expected = sum / n;
+            let mut extra = 0;
+
+            machines
+                .into_iter()
+                .map(|num| {
+                    let diff = num - expected;
+
+                    extra += diff;
+
+                    extra.abs().max(diff)
+                })
+                .max()
+                .unwrap()
+        } else {
+            -1
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_min_moves(machines: Vec<i32>) -> i32 {
+        Self::find_min_moves(machines)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
