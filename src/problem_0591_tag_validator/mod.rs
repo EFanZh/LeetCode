18a60c8bc59d9cf7a3d8e8f4819e38344a467d75
EFanZh <efanzh@gmail.com>
2021-05-18 09:14:26 +0800

    Add problem 591: Tag Validator

---
+++ src/problem_0591_tag_validator/mod.rs
@@ -0,0 +1,66 @@
+pub mod recursive_descent;
+pub mod recursive_descent_2;
+
+pub trait Solution {
+    fn is_valid(code: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("<DIV>This is the first line <![CDATA[<div>]]></DIV>", true),
+            ("<DIV>>>  ![cdata[]] <![CDATA[<div>]>]]>]]>>]</DIV>", true),
+            ("<A>  <B> </A>   </B>", false),
+            ("<DIV>  div tag is not closed  <DIV>", false),
+            (
+                "<DIV>\
+                     <YFSYYS>\
+                         <UVBNIQ>\
+                             <XPMXUNT>\
+                                 <WNGMV>\
+                                     <OJJGQREMT>\
+                                         <Z>\
+                                             <GEJDP>\
+                                                 <LIQS>\
+                                                     <NCVYU>\
+                                                         <RAS>\
+                                                             <UYFKCJCDN>\
+                                                                 <NA>\
+                                                                     <POJVYT>\
+                                                                         <Z>\
+                                                                             <TDC>\
+                                                                                 <VUIZQC>\
+                                                                                     <BNANGX>\
+                                                                                         <TOF>\
+                                                                                             <MR>MK</MR>\
+                                                                                         </TOF>\
+                                                                                     </BNANGX>\
+                                                                                 </VUIZQC>\
+                                                                             </TDC>\
+                                                                         </Z>\
+                                                                     </POJVYT>\
+                                                                 </NA>\
+                                                             </UYFKCJCDN>\
+                                                         </RAS>\
+                                                     </NCVYU>\
+                                                 </LIQS>\
+                                             </GEJDP>\
+                                         </Z>\
+                                     </OJJGQREMT>\
+                                 </WNGMV>\
+                             </XPMXUNT>\
+                         </UVBNIQ>\
+                     </YFSYYS>\
+                 </DIV>",
+                true,
+            ),
+        ];
+
+        for (code, expected) in test_cases.iter().copied() {
+            assert_eq!(S::is_valid(code.to_string()), expected);
+        }
+    }
+}
---
+++ src/problem_0591_tag_validator/recursive_descent_2.rs
@@ -0,0 +1,75 @@
+pub struct Solution;
+
+impl Solution {
+    fn parse_tag_name(input: &str) -> Option<(&str, &str)> {
+        let search_area = if input.len() <= 9 { input } else { &input[..9] };
+
+        let length = search_area
+            .bytes()
+            .position(|c| !c.is_ascii_uppercase())
+            .unwrap_or_else(|| search_area.len());
+
+        if length == 0 {
+            None
+        } else {
+            Some(input.split_at(length))
+        }
+    }
+
+    fn parse_cdata(input: &str) -> Option<&str> {
+        let input = input.strip_prefix("<![CDATA[")?;
+        let length = input.find("]]>")?;
+
+        Some(&input[length..])
+    }
+
+    fn parse_text(input: &str) -> Option<&str> {
+        let length = input.bytes().position(|c| c == b'<').unwrap_or_else(|| input.len());
+
+        if length == 0 {
+            None
+        } else {
+            Some(&input[length..])
+        }
+    }
+
+    fn parse_content(mut input: &str) -> &str {
+        while let Some(next_input) = Self::parse_tag(input)
+            .or_else(|| Self::parse_cdata(input))
+            .or_else(|| Self::parse_text(input))
+        {
+            input = next_input;
+        }
+
+        input
+    }
+
+    fn parse_tag(input: &str) -> Option<&str> {
+        let input = input.strip_prefix("<")?;
+        let (tag_name, input) = Self::parse_tag_name(input)?;
+        let input = input.strip_prefix(">")?;
+        let input = Self::parse_content(input);
+        let input = input.strip_prefix("</")?;
+        let input = input.strip_prefix(tag_name)?;
+
+        input.strip_prefix(">")
+    }
+
+    pub fn is_valid(code: String) -> bool {
+        Self::parse_tag(&code) == Some("")
+    }
+}
+
+impl super::Solution for Solution {
+    fn is_valid(code: String) -> bool {
+        Self::is_valid(code)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
