164c54da9ab71b6ba2466208609ab59bcc7dba32
EFanZh <efanzh@gmail.com>
2021-05-19 09:21:05 +0800

    Add problem 576: Out of Boundary Paths

---
+++ src/problem_0576_out_of_boundary_paths/mod.rs
@@ -0,0 +1,23 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((2, 2, 2, 0, 0), 6),
+            ((1, 3, 3, 0, 1), 12),
+            ((3, 2, 5, 1, 1), 109),
+            ((8, 50, 23, 5, 26), 914_783_380),
+        ];
+
+        for ((m, n, max_move, start_row, start_column), expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_paths(m, n, max_move, start_row, start_column), expected);
+        }
+    }
+}
