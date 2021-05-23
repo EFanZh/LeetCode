980fb1c2dcf26d2b5ce07a442b08e279e244a022
EFanZh <efanzh@gmail.com>
2021-05-23 09:01:38 +0800

    Add problem 522: Longest Uncommon Subsequence II

---
+++ src/problem_0522_longest_uncommon_subsequence_ii/mod.rs
@@ -0,0 +1,26 @@
+pub mod trie;
+
+pub trait Solution {
+    fn find_lu_slength(strs: Vec<String>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&["aba", "cdc", "eae"] as &[_], 3),
+            (&["aaa", "aaa", "aa"], -1),
+            (&["aabbcc", "aabbcc", "bc", "bcc", "aabbccc"], 7),
+            (&["aabbcc", "aabbcc", "cb"], 2),
+        ];
+
+        for (strs, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::find_lu_slength(strs.iter().copied().map(str::to_string).collect()),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0522_longest_uncommon_subsequence_ii/trie.rs
@@ -0,0 +1,82 @@
+pub struct Solution;
+
+use std::cmp::Reverse;
+use std::rc::Rc;
+
+#[derive(Clone, Default)]
+struct Node {
+    children: [Option<Rc<Node>>; 26],
+}
+
+impl Solution {
+    fn build_trie(s: &[u8]) -> Node {
+        let mut root = Node::default();
+
+        for c in s.iter().rev() {
+            let mut new_root = root.clone();
+
+            new_root.children[usize::from(c - b'a')] = Some(Rc::new(root));
+
+            root = new_root;
+        }
+
+        root
+    }
+
+    fn is_subsequence(mut root: &Node, s: &[u8]) -> bool {
+        for c in s {
+            if let Some(child) = root.children[usize::from(c - b'a')].as_deref() {
+                root = child;
+            } else {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    pub fn find_lu_slength(mut strs: Vec<String>) -> i32 {
+        strs.sort_unstable_by(|lhs, rhs| ((Reverse(lhs.len()), lhs.as_str()).cmp(&(Reverse(rhs.len()), rhs.as_str()))));
+
+        let mut tries = Vec::with_capacity(strs.len() - 1);
+        let mut iter = strs.iter().map(String::as_bytes);
+        let mut prev = iter.next().unwrap();
+        let mut start = 0;
+        let mut i = 1;
+
+        for s in iter {
+            if s != prev {
+                if i - start == 1 && tries.iter().all(|trie| !Self::is_subsequence(trie, prev)) {
+                    return prev.len() as _;
+                }
+
+                tries.push(Self::build_trie(prev));
+
+                start = i;
+                prev = s;
+            }
+
+            i += 1;
+        }
+
+        if i - start == 1 && tries.iter().all(|trie| !Self::is_subsequence(trie, prev)) {
+            prev.len() as _
+        } else {
+            -1
+        }
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_lu_slength(strs: Vec<String>) -> i32 {
+        Self::find_lu_slength(strs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
