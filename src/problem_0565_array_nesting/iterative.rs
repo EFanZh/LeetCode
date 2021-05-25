333be6c02afb7626595045f9cbb1b28099c8d37b
EFanZh <efanzh@gmail.com>
2021-05-25 09:26:12 +0800

    Add problem 565: Array Nesting

---
+++ src/problem_0565_array_nesting/iterative.rs
@@ -0,0 +1,45 @@
+pub struct Solution;
+
+use std::mem;
+
+impl Solution {
+    pub fn array_nesting(mut nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+
+        for i in 0..nums.len() {
+            if nums[i] != -1 {
+                let mut current = i;
+                let mut count = 1;
+
+                loop {
+                    let next = mem::replace(&mut nums[current], -1) as _;
+
+                    if next == i {
+                        break;
+                    }
+
+                    current = next;
+                    count += 1;
+                }
+
+                result = result.max(count);
+            }
+        }
+
+        result
+    }
+}
+
+impl super::Solution for Solution {
+    fn array_nesting(nums: Vec<i32>) -> i32 {
+        Self::array_nesting(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
