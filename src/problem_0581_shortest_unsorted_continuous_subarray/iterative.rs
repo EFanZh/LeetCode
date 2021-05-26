3a33e2b9ff94fdad6bce8dc0910d86595a49482c
EFanZh <efanzh@gmail.com>
2021-05-26 08:50:31 +0800

    Add problem 581: Shortest Unsorted Continuous Subarray

---
+++ src/problem_0581_shortest_unsorted_continuous_subarray/iterative.rs
@@ -0,0 +1,43 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
+        let mut max = i32::min_value();
+        let mut right = 0;
+
+        for (i, &num) in nums.iter().enumerate() {
+            if num < max {
+                right = i + 1;
+            }
+
+            max = max.max(num);
+        }
+
+        let mut min = i32::max_value();
+        let mut left = right;
+
+        for (i, &num) in nums[..right].iter().enumerate().rev() {
+            if num > min {
+                left = i;
+            }
+
+            min = min.min(num);
+        }
+
+        (right - left) as _
+    }
+}
+
+impl super::Solution for Solution {
+    fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
+        Self::find_unsorted_subarray(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
