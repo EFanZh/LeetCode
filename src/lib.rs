24a45407eda6c15d5e7baf0e0d68eddafc231f93
EFanZh <efanzh@gmail.com>
2021-05-26 08:50:31 +0800

    Add problem 581: Shortest Unsorted Continuous Subarray

--- src/lib.rs
+++ src/lib.rs
@@ -481,6 +481,7 @@ pub mod problem_0567_permutation_in_string;
 pub mod problem_0572_subtree_of_another_tree;
 pub mod problem_0575_distribute_candies;
 pub mod problem_0576_out_of_boundary_paths;
+pub mod problem_0581_shortest_unsorted_continuous_subarray;
 pub mod problem_0583_delete_operation_for_two_strings;
 pub mod problem_0591_tag_validator;
 pub mod problem_0593_valid_square;
---
+++ src/problem_0581_shortest_unsorted_continuous_subarray/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn find_unsorted_subarray(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[2, 6, 4, 8, 10, 9, 15] as &[_], 5), (&[1, 2, 3, 4], 0), (&[1], 0)];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_unsorted_subarray(nums.to_vec()), expected);
+        }
+    }
+}
