b1ee4109a600b7ac175e83d2a0bff0c178e2f063
EFanZh <efanzh@gmail.com>
2021-05-27 09:09:35 +0800

    Add problem 633: Sum of Square Numbers

---
+++ src/problem_0633_sum_of_square_numbers/mod.rs
@@ -0,0 +1,26 @@
+pub mod brute_force;
+pub mod two_pointers;
+
+pub trait Solution {
+    fn judge_square_sum(c: i32) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (1, true),
+            (2, true),
+            (3, false),
+            (4, true),
+            (5, true),
+            (12_132_321, false),
+        ];
+
+        for (c, expected) in test_cases.iter().copied() {
+            assert_eq!(S::judge_square_sum(c), expected);
+        }
+    }
+}
---
+++ src/problem_0633_sum_of_square_numbers/two_pointers.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+use std::cmp::Ordering;
+
+impl Solution {
+    pub fn judge_square_sum(c: i32) -> bool {
+        let c = c as u32;
+        let mut left = 0;
+        let mut right = f64::from(c).sqrt() as u32;
+        let mut current = right * right;
+
+        while left <= right {
+            match current.cmp(&c) {
+                Ordering::Less => {
+                    current += left * 2 + 1;
+                    left += 1;
+                }
+                Ordering::Equal => return true,
+                Ordering::Greater => {
+                    right -= 1;
+                    current -= right * 2 + 1;
+                }
+            }
+        }
+
+        false
+    }
+}
+
+impl super::Solution for Solution {
+    fn judge_square_sum(c: i32) -> bool {
+        Self::judge_square_sum(c)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
