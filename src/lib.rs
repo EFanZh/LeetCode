103dcebc90c2a4f088e68d3e7dfe61c6eee02946
EFanZh <efanzh@gmail.com>
2021-05-29 09:30:48 +0800

    Add problem 637: Average of Levels in Binary Tree

--- src/lib.rs
+++ src/lib.rs
@@ -495,6 +495,7 @@ pub mod problem_0623_add_one_row_to_tree;
 pub mod problem_0628_maximum_product_of_three_numbers;
 pub mod problem_0633_sum_of_square_numbers;
 pub mod problem_0636_exclusive_time_of_functions;
+pub mod problem_0637_average_of_levels_in_binary_tree;
 pub mod problem_0647_palindromic_substrings;
 pub mod problem_0657_robot_return_to_origin;
 pub mod problem_0661_image_smoother;
---
+++ src/problem_0637_average_of_levels_in_binary_tree/mod.rs
@@ -0,0 +1,37 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod bfs;
+
+pub trait Solution {
+    fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[Some(3), Some(9), Some(20), None, Some(15), Some(7)] as &[_],
+                &[3.0, 14.5, 11.0] as &[_],
+            ),
+            (&[Some(3), Some(9), Some(20), Some(15), Some(7)], &[3.0, 14.5, 11.0]),
+            (
+                &[Some(0x7FFF_FFFF), Some(0x7FFF_FFFF), Some(0x7FFF_FFFF)],
+                &[2_147_483_647.0, 2_147_483_647.0],
+            ),
+            (&[], &[]),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::average_of_levels(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
