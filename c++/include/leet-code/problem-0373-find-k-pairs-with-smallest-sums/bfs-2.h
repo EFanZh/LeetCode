0e53dea55ccf991d5c409a52fe4b7b2e4719d97d
EFanZh <efanzh@gmail.com>
2021-05-29 18:30:13 +0800

    Refactor

--- c++/include/leet-code/problem-0373-find-k-pairs-with-smallest-sums/bfs-2.h
+++ c++/include/leet-code/problem-0373-find-k-pairs-with-smallest-sums/bfs-2.h
@@ -19,35 +19,32 @@ public:
         auto result = vector<vector<int>>{};
 
         result.reserve(k_2);
+        result.emplace_back(initializer_list<int>{nums1.front(), nums2.front()});
 
-        if (k_2 != 0 && !nums1.empty() && !nums2.empty()) {
-            result.emplace_back(initializer_list<int>{nums1.front(), nums2.front()});
+        auto node = tuple{size_t{0}, size_t{0}};
 
-            auto node = tuple{size_t{0}, size_t{0}};
+        auto queue =
+            priority_queue{[&](const auto &lhs, const auto &rhs) { return std::get<0>(rhs) < std::get<0>(lhs); },
+                           vector<tuple<int, tuple<size_t, size_t>>>{}};
 
-            auto queue =
-                priority_queue{[&](const auto &lhs, const auto &rhs) { return std::get<0>(rhs) < std::get<0>(lhs); },
-                               vector<tuple<int, tuple<size_t, size_t>>>{}};
+        while (result.size() != k_2) {
+            const auto [i, j] = node;
 
-            while (result.size() != k_2) {
-                const auto [i, j] = node;
-
-                if (j != nums2.size() - 1) {
-                    queue.emplace(nums1[i] + nums2[j + 1], tuple{i, j + 1});
-                }
-
-                if (j == 0 && i != nums1.size() - 1) {
-                    queue.emplace(nums1[i + 1] + nums2[j], tuple{i + 1, j});
-                }
-
-                if (queue.empty()) {
-                    break;
-                }
-
-                node = std::get<1>(queue.top());
-                queue.pop();
-                result.emplace_back(initializer_list<int>{nums1[std::get<0>(node)], nums2[std::get<1>(node)]});
+            if (j != nums2.size() - 1) {
+                queue.emplace(nums1[i] + nums2[j + 1], tuple{i, j + 1});
             }
+
+            if (j == 0 && i != nums1.size() - 1) {
+                queue.emplace(nums1[i + 1] + nums2[j], tuple{i + 1, j});
+            }
+
+            if (queue.empty()) {
+                break;
+            }
+
+            node = std::get<1>(queue.top());
+            queue.pop();
+            result.emplace_back(initializer_list<int>{nums1[std::get<0>(node)], nums2[std::get<1>(node)]});
         }
 
         return result;
