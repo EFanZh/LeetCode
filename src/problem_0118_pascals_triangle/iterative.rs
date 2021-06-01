d1be5e85b7931b3b10064165d4559bf9a66974a3
EFanZh <efanzh@gmail.com>
2021-06-01 09:28:01 +0800

    Refactor

--- src/problem_0118_pascals_triangle/iterative.rs
+++ src/problem_0118_pascals_triangle/iterative.rs
@@ -7,19 +7,17 @@ impl Solution {
         let num_rows = num_rows as usize;
         let mut result = Vec::with_capacity(num_rows);
 
-        if num_rows > 0 {
-            result.push(vec![1]);
+        result.push(vec![1]);
 
-            for length in 2..=num_rows {
-                let mut row = Vec::with_capacity(length);
-                let previous_row = result.last().unwrap().as_slice();
+        for length in 2..=num_rows {
+            let mut row = Vec::with_capacity(length);
+            let previous_row = result.last().unwrap().as_slice();
 
-                row.push(1);
-                row.extend(previous_row.iter().zip(&previous_row[1..]).map(|(lhs, rhs)| lhs + rhs));
-                row.push(1);
+            row.push(1);
+            row.extend(previous_row.iter().zip(&previous_row[1..]).map(|(lhs, rhs)| lhs + rhs));
+            row.push(1);
 
-                result.push(row);
-            }
+            result.push(row);
         }
 
         result
