b6b068981484b51081db63d820b657a32788a39d
EFanZh <efanzh@gmail.com>
2021-06-01 23:43:15 +0800

    Add some test cases

--- src/problem_0300_longest_increasing_subsequence/mod.rs
+++ src/problem_0300_longest_increasing_subsequence/mod.rs
@@ -10,7 +10,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[10, 9, 2, 5, 3, 7, 101, 18] as &[_], 4)];
+        let test_cases = [
+            (&[10, 9, 2, 5, 3, 7, 101, 18] as &[_], 4),
+            (&[0, 1, 0, 3, 2, 3], 4),
+            (&[7, 7, 7, 7, 7, 7, 7], 1),
+        ];
 
         for (nums, expected) in test_cases.iter().copied() {
             assert_eq!(S::length_of_lis(nums.to_vec()), expected);
--- src/problem_0313_super_ugly_number/mod.rs
+++ src/problem_0313_super_ugly_number/mod.rs
@@ -10,7 +10,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((12, &[2, 7, 13, 19] as &[_]), 32)];
+        let test_cases = [
+            ((12, &[2, 7, 13, 19] as &[_]), 32),
+            ((1, &[2, 3, 5]), 1),
+            ((10, &[2, 5, 7, 11, 13, 17, 23, 29, 43, 53]), 14),
+        ];
 
         for ((n, primes), expected) in test_cases.iter().copied() {
             assert_eq!(S::nth_super_ugly_number(n, primes.to_vec()), expected);
