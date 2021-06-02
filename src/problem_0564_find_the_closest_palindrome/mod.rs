bf30ca63a6f33553353f2658d942a8d5f06f02db
EFanZh <efanzh@gmail.com>
2021-06-02 09:17:24 +0800

    Add problem 564: Find the Closest Palindrome

---
+++ src/problem_0564_find_the_closest_palindrome/mod.rs
@@ -0,0 +1,42 @@
+pub mod brute_force;
+
+pub trait Solution {
+    fn nearest_palindromic(n: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (1, 0),
+            (2, 1),
+            (3, 2),
+            (4, 3),
+            (5, 4),
+            (6, 5),
+            (7, 6),
+            (8, 7),
+            (9, 8),
+            (10, 9),
+            (11, 9),
+            (12, 11),
+            (13, 11),
+            (99, 101),
+            (101, 99),
+            (111, 101),
+            (123, 121),
+            (999, 1001),
+            (1001, 999),
+            (1111, 1001),
+            (1283, 1331),
+            (1213, 1221),
+            (9999, 10001),
+        ];
+
+        for (n, expected) in test_cases.iter().copied() {
+            assert_eq!(S::nearest_palindromic(n.to_string()).parse::<u64>().unwrap(), expected);
+        }
+    }
+}
