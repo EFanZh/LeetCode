c6629459681e9b8155140a25d4d2643cf70cfe91
EFanZh <efanzh@gmail.com>
2021-06-05 12:48:34 +0800

    Add problem 645: Set Mismatch

---
+++ src/problem_0645_set_mismatch/highest_bit_as_marker.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn find_error_nums(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut extra_xor_missing = 0;
+        let mut i = 0;
+
+        let extra = loop {
+            let num = nums[i] & i32::max_value();
+            let slot = &mut nums[(num - 1) as usize];
+
+            extra_xor_missing ^= num ^ ((i + 1) as i32);
+            i += 1;
+
+            if *slot & i32::min_value() == 0 {
+                *slot |= i32::min_value();
+            } else {
+                break num;
+            }
+        };
+
+        while let Some(num) = nums.get(i) {
+            extra_xor_missing ^= (num & i32::max_value()) ^ ((i + 1) as i32);
+            i += 1;
+        }
+
+        vec![extra, extra_xor_missing ^ extra]
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+        Self::find_error_nums(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0645_set_mismatch/mod.rs
@@ -0,0 +1,24 @@
+pub mod highest_bit_as_marker;
+pub mod partition_by_bit;
+
+pub trait Solution {
+    fn find_error_nums(nums: Vec<i32>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[1, 2, 2, 4] as &[_], [2, 3]),
+            (&[1, 1], [1, 2]),
+            (&[1, 2, 2, 4], [2, 3]),
+            (&[3, 3, 1], [3, 2]),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(S::find_error_nums(nums.to_vec()), expected);
+        }
+    }
+}
