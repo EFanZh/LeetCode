4c555e82d7559eb02a821c90ac1cc5185477c42f
EFanZh <efanzh@gmail.com>
2021-06-09 21:58:55 +0800

    Refactor

--- src/problem_0600_non_negative_integers_without_consecutive_ones/mathematical.rs
+++ src/problem_0600_non_negative_integers_without_consecutive_ones/mathematical.rs
@@ -4,9 +4,9 @@ pub struct Solution;
 
 impl Solution {
     pub fn find_integers(n: i32) -> i32 {
-        let fibonacci_number_sums = [
+        let fibonacci = [
             1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1_597, 2_584, 4_181, 6_765, 10_946, 17_711,
-            28_657, 46_368, 75_025, 121_393, 196_418, 317_811, 514_229, 832_040, 1_346_269, 2_178_309, 3_524_578,
+            28_657, 46_368, 75_025, 121_393, 196_418, 317_811, 514_229, 832_040, 1_346_269, 2_178_309,
         ];
 
         let keep_bits = (n & (n >> 1)).leading_zeros();
@@ -16,7 +16,7 @@ impl Solution {
 
         for i in 0..32 - n.leading_zeros() {
             if n & (1 << i) != 0 {
-                result += fibonacci_number_sums[i as usize];
+                result += fibonacci[i as usize];
             }
         }
 
