d4008bbc0ada095985ce6c3c645a15f452e73337
EFanZh <efanzh@gmail.com>
2021-06-10 09:30:55 +0800

    Add problem 629: K Inverse Pairs Array

---
+++ src/problem_0629_k_inverse_pairs_array/mod.rs
@@ -0,0 +1,27 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn k_inverse_pairs(n: i32, k: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        // https://oeis.org/A008302.
+
+        let test_cases = [
+            ((3, 0), 1),
+            ((3, 1), 2),
+            ((3, 2), 2),
+            ((3, 3), 1),
+            ((10, 7), 4489),
+            ((1000, 1000), 663_677_020),
+        ];
+
+        for ((n, k), expected) in test_cases.iter().copied() {
+            assert_eq!(S::k_inverse_pairs(n, k), expected);
+        }
+    }
+}
