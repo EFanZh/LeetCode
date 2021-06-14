1f8cc354ee9995cf2b5a74b61e4ce1735eff4367
EFanZh <efanzh@gmail.com>
2021-06-14 10:07:43 +0800

    Add problem 632: Smallest Range Covering Elements from K Lists

---
+++ src/problem_0632_smallest_range_covering_elements_from_k_lists/mod.rs
@@ -0,0 +1,31 @@
+pub mod binary_heap;
+pub mod sliding_window;
+
+pub trait Solution {
+    fn smallest_range(nums: Vec<Vec<i32>>) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[&[4, 10, 15, 24, 26] as &[_], &[0, 9, 12, 20], &[5, 18, 22, 30]] as &[&[_]],
+                [20, 24],
+            ),
+            (&[&[1, 2, 3], &[1, 2, 3], &[1, 2, 3]], [1, 1]),
+            (&[&[10, 10], &[11, 11]], [10, 11]),
+            (&[&[10], &[11]], [10, 11]),
+            (&[&[1], &[2], &[3], &[4], &[5], &[6], &[7]], [1, 7]),
+        ];
+
+        for (nums, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::smallest_range(nums.iter().copied().map(<[_]>::to_vec).collect()),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_0632_smallest_range_covering_elements_from_k_lists/sliding_window.rs
@@ -0,0 +1,86 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn smallest_range(nums: Vec<Vec<i32>>) -> Vec<i32> {
+        let k = nums.len();
+        let mut events = HashMap::new();
+
+        for (i, num) in nums.iter().enumerate() {
+            for &x in num {
+                events
+                    .entry(x)
+                    .and_modify(|v| Vec::push(v, i))
+                    .or_insert_with(|| vec![i]);
+            }
+        }
+
+        let mut events = events.into_iter().collect::<Vec<_>>();
+
+        events.sort_unstable_by_key(|&(x, _)| x);
+
+        let mut best_low = 0;
+        let mut best_high = i32::max_value();
+        let mut left_iter = events.iter();
+        let mut counts = vec![0; nums.len()];
+        let mut covered = 0;
+
+        for (high, lists) in &events {
+            for &list in lists {
+                let count = &mut counts[list];
+
+                if *count == 0 {
+                    covered += 1;
+                }
+
+                *count += 1;
+            }
+
+            if covered == k {
+                loop {
+                    let (low, lists) = left_iter.next().unwrap();
+
+                    for &list in lists {
+                        let count = &mut counts[list];
+
+                        *count -= 1;
+
+                        if *count == 0 {
+                            covered -= 1;
+                        }
+                    }
+
+                    if covered != k {
+                        if high - low < best_high - best_low {
+                            best_low = *low;
+                            best_high = *high;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        vec![best_low, best_high]
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn smallest_range(nums: Vec<Vec<i32>>) -> Vec<i32> {
+        Self::smallest_range(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
