dba12cd30d09923fc61094feb5b4195e8f76b5bd
EFanZh <efanzh@gmail.com>
2021-06-15 08:39:46 +0800

    Add problem 638: Shopping Offers

---
+++ src/problem_0638_shopping_offers/memoized_dynamic_programming_2.rs
@@ -0,0 +1,82 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::HashMap;
+
+impl Solution {
+    fn check_offer(offer: u32, needs: u32) -> bool {
+        offer <= needs
+            && (offer & 0x_000f_0000) <= (needs & 0x_000f_0000)
+            && (offer & 0x_0000_f000) <= (needs & 0x_0000_f000)
+            && (offer & 0x_0000_0f00) <= (needs & 0x_0000_0f00)
+            && (offer & 0x_0000_00f0) <= (needs & 0x_0000_00f0)
+            && (offer & 0x_0000_000f) <= (needs & 0x_0000_000f)
+    }
+
+    fn helper(offers: &[(u32, i32)], needs: u32, cache: &mut HashMap<u32, i32>) -> i32 {
+        if needs == 0 {
+            0
+        } else if let Some(&result) = cache.get(&needs) {
+            result
+        } else {
+            let mut result = i32::max_value();
+
+            for &(offer, price) in offers {
+                if price < result && Self::check_offer(offer, needs) {
+                    result = result.min(price + Self::helper(offers, needs - offer, cache));
+                }
+            }
+
+            cache.insert(needs, result);
+
+            result
+        }
+    }
+
+    pub fn shopping_offers(price: Vec<i32>, special: Vec<Vec<i32>>, needs: Vec<i32>) -> i32 {
+        let mut offers = Vec::with_capacity(price.len() + special.len());
+
+        for (i, &price) in price.iter().enumerate() {
+            offers.push((1 << (4 * i), price));
+        }
+
+        for offer_description in special {
+            let (&price, offer) = offer_description.split_last().unwrap();
+
+            let offer = offer
+                .iter()
+                .enumerate()
+                .map(|(i, &count)| (count as u32) << (4 * i))
+                .sum();
+
+            offers.push((offer, price));
+        }
+
+        let max_states = needs.iter().product::<i32>() as _;
+
+        let needs = needs
+            .iter()
+            .enumerate()
+            .map(|(i, &count)| (count as u32) << (4 * i))
+            .sum();
+
+        Self::helper(&offers, needs, &mut HashMap::with_capacity(max_states))
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn shopping_offers(price: Vec<i32>, special: Vec<Vec<i32>>, needs: Vec<i32>) -> i32 {
+        Self::shopping_offers(price, special, needs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
