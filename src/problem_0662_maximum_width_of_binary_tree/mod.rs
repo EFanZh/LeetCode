bdda1df58e06c40b3e2128967679e902055dd164
EFanZh <efanzh@gmail.com>
2021-06-16 09:04:21 +0800

    Add problem 662: Maximum Width of Binary Tree

---
+++ src/problem_0662_maximum_width_of_binary_tree/mod.rs
@@ -0,0 +1,46 @@
+use super::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod bfs;
+
+pub trait Solution {
+    fn width_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::test_utilities;
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[Some(1), Some(3), Some(2), Some(5), Some(3), None, Some(9)] as &[_], 4),
+            (&[Some(1), Some(3), None, Some(5), Some(3)], 2),
+            (&[Some(1), Some(3), Some(2), Some(5)], 2),
+            (
+                &[
+                    Some(1),
+                    Some(3),
+                    Some(2),
+                    Some(5),
+                    None,
+                    None,
+                    Some(9),
+                    Some(6),
+                    None,
+                    None,
+                    Some(7),
+                ],
+                8,
+            ),
+        ];
+
+        for (root, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                S::width_of_binary_tree(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
