62f5eceb47a20cef33cb2655ea19351fc1b06d41
EFanZh <efanzh@gmail.com>
2021-06-16 09:04:21 +0800

    Add problem 662: Maximum Width of Binary Tree

---
+++ src/problem_0662_maximum_width_of_binary_tree/bfs.rs
@@ -0,0 +1,55 @@
+use super::super::data_structures::TreeNode;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn width_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut queue = VecDeque::from(vec![(0, root.unwrap())]);
+        let mut result = 0;
+
+        loop {
+            result = result.max(queue.back().unwrap().0 - queue.front().unwrap().0);
+
+            for _ in 0..queue.len() {
+                let (position, node) = queue.pop_front().unwrap();
+                let node = node.borrow();
+
+                if let Some(left) = node.left.clone() {
+                    queue.push_back((position << 1, left));
+                }
+
+                if let Some(right) = node.right.clone() {
+                    queue.push_back(((position << 1) + 1, right));
+                }
+            }
+
+            if queue.is_empty() {
+                break;
+            }
+        }
+
+        result + 1
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn width_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::width_of_binary_tree(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
