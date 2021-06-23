36c9f346cc0fd7ed5937a124580107c08fe9608f
EFanZh <efanzh@gmail.com>
2021-06-23 09:17:20 +0800

    Add problem 665: Non-decreasing Array

--- src/lib.rs
+++ src/lib.rs
@@ -524,6 +524,7 @@ pub mod problem_0657_robot_return_to_origin;
 pub mod problem_0658_find_k_closest_elements;
 pub mod problem_0661_image_smoother;
 pub mod problem_0662_maximum_width_of_binary_tree;
+pub mod problem_0665_non_decreasing_array;
 pub mod problem_0704_binary_search;
 pub mod problem_0709_to_lower_case;
 pub mod problem_0721_accounts_merge;
