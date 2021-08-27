4090eb9b9e443f2de6bf02e0eacc9076ef8e4c3d
EFanZh <efanzh@gmail.com>
2021-08-28 02:31:28 +0800

    Use separate coverage toolchains for C++ and Rust

--- xtask/src/coverage/mod.rs
+++ xtask/src/coverage/mod.rs
@@ -1,9 +1,11 @@
 use crate::tools;
+use serde_json::{Deserializer, Value};
 use std::ffi::{OsStr, OsString};
+use std::fmt::Write;
 use std::fmt::{self, Display, Formatter};
-use std::fs;
+use std::fs::{self, OpenOptions};
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::str::FromStr;
 use structopt::StructOpt;
 
@@ -53,6 +55,9 @@ pub struct Subcommand {
     #[structopt(long)]
     cmake_toolchain_file: Option<PathBuf>,
 
+    #[structopt(long)]
+    llvm_version: Option<String>,
+
     #[structopt(long, default_value = "nightly")]
     rust_toolchain: String,
 
@@ -61,6 +66,44 @@ pub struct Subcommand {
 
     #[structopt(short = "t", long, default_value = "lcov")]
     output_type: OutputType,
+}
+
+fn run_coverage_tests(test_executable: &Path, llvm_profdata: &Path, llvm_cov: &Path, output: &Path) {
+    let profile_dir = tempfile::tempdir().unwrap();
+    let profraw_file = profile_dir.path().join("coverage.profraw");
+
+    assert!(Command::new(test_executable)
+        .env("LLVM_PROFILE_FILE", profraw_file.as_os_str())
+        .status()
+        .unwrap()
+        .success());
+
+    let profdata_file = profile_dir.path().join("coverage.profdata");
+
+    assert!(Command::new(llvm_profdata)
+        .args([
+            "merge".as_ref(),
+            "-o".as_ref(),
+            profdata_file.as_os_str(),
+            profraw_file.as_ref(),
+        ])
+        .status()
+        .unwrap()
+        .success());
+
+    assert!(Command::new(llvm_cov)
+        .args([
+            "export".as_ref(),
+            "--format".as_ref(),
+            "lcov".as_ref(),
+            "--instr-profile".as_ref(),
+            profdata_file.as_os_str(),
+            test_executable.as_os_str(),
+        ])
+        .stdout(OpenOptions::new().append(true).create(true).open(output).unwrap())
+        .status()
+        .unwrap()
+        .success());
 }
 
 fn add_cmake_variable(command: &mut Command, variable: &str, value: &OsStr) {
@@ -74,94 +117,158 @@ fn add_cmake_variable(command: &mut Command, variable: &str, value: &OsStr) {
     command.arg(arg);
 }
 
-impl Subcommand {
-    pub fn run(self) {
-        const CPP_COVERAGE_TARGET_DIR: &str = "target/c++-coverage";
+fn run_cpp_tests(cmake_toolchain_file: Option<&Path>, llvm_version: Option<&str>, output: &Path) {
+    const CPP_COVERAGE_TARGET_DIR: &str = "target/c++-coverage";
 
-        let cmake_executable = tools::get_cmake().unwrap();
+    let cmake_executable = tools::get_cmake().unwrap();
+    let mut clang = String::from("clang");
+    let mut clang_plus_plus = String::from("clang++");
+    let mut llvm_profdata = String::from("llvm-profdata");
+    let mut llvm_cov = String::from("llvm-cov");
 
-        // Configure C++.
+    if let Some(llvm_version) = llvm_version {
+        for tool in [&mut clang, &mut clang_plus_plus, &mut llvm_profdata, &mut llvm_cov] {
+            write!(tool, "-{}", llvm_version).unwrap();
+        }
+    }
 
-        let mut cmake_command = Command::new(cmake_executable);
+    // Configure.
 
-        cmake_command.args(&[
-            "-S",
-            "c++",
-            "-B",
-            CPP_COVERAGE_TARGET_DIR,
-            "-G",
-            "Ninja",
-            "-D",
-            "CMAKE_BUILD_TYPE=Debug",
-            "-D",
-            "CMAKE_C_COMPILER=clang",
-            "-D",
-            "CMAKE_CXX_COMPILER=clang++",
-            "-D",
-            "ENABLE_SOURCE_BASED_CODE_COVERAGE=ON",
-        ]);
+    let mut cmake_command = Command::new(cmake_executable);
 
+    cmake_command.args([
+        "-S",
+        "c++",
+        "-B",
+        CPP_COVERAGE_TARGET_DIR,
+        "-G",
+        "Ninja",
+        "-D",
+        "CMAKE_BUILD_TYPE=Debug",
+        "-D",
+        format!("CMAKE_C_COMPILER={}", clang).as_str(),
+        "-D",
+        format!("CMAKE_CXX_COMPILER={}", clang_plus_plus).as_str(),
+        "-D",
+        "ENABLE_SOURCE_BASED_CODE_COVERAGE=ON",
+    ]);
+
+    add_cmake_variable(
+        &mut cmake_command,
+        "CMAKE_MAKE_PROGRAM",
+        tools::get_ninja().unwrap().as_os_str(),
+    );
+
+    if let Some(cmake_toolchain_file) = cmake_toolchain_file {
         add_cmake_variable(
             &mut cmake_command,
-            "CMAKE_MAKE_PROGRAM",
-            tools::get_ninja().unwrap().as_os_str(),
+            "CMAKE_TOOLCHAIN_FILE",
+            cmake_toolchain_file.as_os_str(),
         );
+    }
 
-        if let Some(cmake_toolchain_file) = self.cmake_toolchain_file {
-            add_cmake_variable(
-                &mut cmake_command,
-                "CMAKE_TOOLCHAIN_FILE",
-                cmake_toolchain_file.as_os_str(),
-            );
-        }
+    assert!(cmake_command.status().unwrap().success());
 
-        assert!(cmake_command.status().unwrap().success());
+    // Build.
 
-        // Build C++.
+    let mut cmake_build_command = Command::new(cmake_executable);
 
-        let mut cmake_build_command = Command::new(cmake_executable);
+    cmake_build_command.args(["--build", CPP_COVERAGE_TARGET_DIR, "-j"]);
 
-        cmake_build_command.args(&["--build", CPP_COVERAGE_TARGET_DIR, "-j"]);
+    assert!(cmake_build_command.status().unwrap().success());
 
-        assert!(cmake_build_command.status().unwrap().success());
+    // Run.
 
-        // Run C++.
+    #[cfg(target_os = "windows")]
+    let executable = "leet-code-tests.exe";
 
-        let profile_dir = tempfile::tempdir().unwrap();
+    #[cfg(not(target_os = "windows"))]
+    let executable = "leet-code-tests";
 
-        assert!(Command::new(Path::new(CPP_COVERAGE_TARGET_DIR).join("leet-code-tests"))
-            .env("LLVM_PROFILE_FILE", profile_dir.path().join("c++.profraw"))
-            .status()
-            .unwrap()
-            .success());
+    run_coverage_tests(
+        &Path::new(CPP_COVERAGE_TARGET_DIR).join(executable),
+        llvm_profdata.as_ref(),
+        llvm_cov.as_ref(),
+        output,
+    );
+}
 
-        // Run Rust.
+fn get_cargo_test_command(toolchain: &str) -> Command {
+    let mut command = Command::new("cargo");
 
-        assert!(Command::new("cargo")
-            .args(&[format!("+{}", self.rust_toolchain).as_str(), "test"])
-            .envs(
-                [
-                    ("LLVM_PROFILE_FILE", profile_dir.path().join("rust.profraw").as_os_str()),
-                    ("RUSTFLAGS", OsStr::new("-Zinstrument-coverage"))
-                ]
-                .iter()
-                .copied()
-            )
-            .status()
-            .unwrap()
-            .success());
+    command
+        .args([format!("+{}", toolchain).as_str(), "test"])
+        .env("RUSTFLAGS", "-Zinstrument-coverage");
 
-        // Generate report with grcov.
+    command
+}
+
+fn get_llvm_tools(toolchain: &str) -> (PathBuf, PathBuf) {
+    let llvm_tool_dir = tools::find_rust_lib(toolchain);
+    let mut llvm_profdata = llvm_tool_dir.clone();
+
+    llvm_profdata.push("llvm-profdata");
+
+    let mut llvm_cov = llvm_tool_dir;
+
+    llvm_cov.push("llvm-cov");
+
+    (llvm_profdata, llvm_cov)
+}
+
+fn run_rust_tests(toolchain: &str, output: &Path) {
+    let mut build_process = get_cargo_test_command(toolchain)
+        .args(["--no-run", "--message-format", "json"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let test_executable = Deserializer::from_reader(build_process.stdout.as_mut().unwrap())
+        .into_iter::<Value>()
+        .find_map(|item| {
+            if let Ok(Value::Object(mut item)) = item {
+                if let Some(Value::String(executable)) = item.remove("executable") {
+                    Some(executable)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .unwrap();
+
+    assert!(build_process.wait_with_output().unwrap().status.success());
+
+    let (llvm_profdata, llvm_cov) = get_llvm_tools(toolchain);
+
+    run_coverage_tests(test_executable.as_ref(), &llvm_profdata, &llvm_cov, output);
+}
+
+fn run_all_tests(cmake_toolchain_file: Option<&Path>, llvm_version: Option<&str>, rust_toolchain: &str, output: &Path) {
+    run_cpp_tests(cmake_toolchain_file.as_deref(), llvm_version, output);
+    run_rust_tests(rust_toolchain, output);
+}
+
+impl Subcommand {
+    pub fn run(self) {
+        let coverage_dir = tempfile::tempdir().unwrap();
+        let coverage_file = coverage_dir.path().join("coverage.info");
+
+        run_all_tests(
+            self.cmake_toolchain_file.as_deref(),
+            self.llvm_version.as_deref(),
+            &self.rust_toolchain,
+            &coverage_file,
+        );
 
         if let Some(parent) = self.output_path.parent() {
             fs::create_dir_all(parent).unwrap();
         }
 
         assert!(Command::new("grcov")
-            .args(&[
+            .args([
                 OsStr::new("--branch"),
-                OsStr::new("-b"),
-                OsStr::new("target"),
                 OsStr::new("--keep-only"),
                 OsStr::new("c++/*"),
                 OsStr::new("--keep-only"),
@@ -169,12 +276,11 @@ impl Subcommand {
                 OsStr::new("-o"),
                 self.output_path.as_os_str(),
                 OsStr::new("-t"),
-                OsStr::new(self.output_type.value()),
+                self.output_type.value().as_ref(),
                 OsStr::new("-s"),
                 OsStr::new("."),
-                profile_dir.path().as_os_str()
+                coverage_file.as_os_str()
             ])
-            .env("RUSTUP_TOOLCHAIN", self.rust_toolchain)
             .status()
             .unwrap()
             .success());
--- xtask/src/tools/mod.rs
+++ xtask/src/tools/mod.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::path::PathBuf;
+use std::process::Command;
 
 #[cfg(target_os = "windows")]
 mod windows;
@@ -9,26 +10,65 @@ lazy_static::lazy_static! {
     static ref NINJA_PATH: Option<PathBuf> = find_ninja();
 }
 
+fn run_rustc(toolchain: &str, args: &[&str]) -> String {
+    let mut command = Command::new("rustc");
+
+    command.arg(format!("+{}", toolchain));
+    command.args(args);
+
+    String::from_utf8(command.output().unwrap().stdout).unwrap()
+}
+
+fn find_rustc_host(toolchain: &str) -> String {
+    run_rustc(toolchain, &["-vV"])
+        .lines()
+        .find_map(|line| line.strip_prefix("host: "))
+        .map(str::to_string)
+        .unwrap()
+}
+
+pub fn find_rust_lib(toolchain: &str) -> PathBuf {
+    let mut path_buffer = run_rustc(toolchain, &["--print", "sysroot"]);
+
+    path_buffer.pop();
+
+    let mut path = PathBuf::from(path_buffer);
+
+    path.extend(["lib", "rustlib", find_rustc_host(toolchain).as_str(), "bin"]);
+
+    path
+}
+
+fn find_cmake_common() -> Option<PathBuf> {
+    which::which("cmake").ok()
+}
+
+#[cfg(target_os = "windows")]
 fn find_cmake() -> Option<PathBuf> {
-    let result = which::which("cmake").ok();
+    find_cmake_common().or_else(windows::find_cmake)
+}
 
-    #[cfg(target_os = "windows")]
-    let result = result.or_else(windows::find_cmake);
-
-    result
+#[cfg(not(target_os = "windows"))]
+fn find_cmake() -> Option<PathBuf> {
+    find_cmake_common()
 }
 
 pub fn get_cmake() -> Option<&'static Path> {
     CMAKE_PATH.as_deref()
 }
 
+fn find_ninja_common() -> Option<PathBuf> {
+    which::which("ninja").ok()
+}
+
+#[cfg(target_os = "windows")]
 fn find_ninja() -> Option<PathBuf> {
-    let result = which::which("ninja").ok();
+    find_ninja_common().or_else(windows::find_ninja)
+}
 
-    #[cfg(target_os = "windows")]
-    let result = result.or_else(windows::find_ninja);
-
-    result
+#[cfg(not(target_os = "windows"))]
+fn find_ninja() -> Option<PathBuf> {
+    find_ninja_common()
 }
 
 pub fn get_ninja() -> Option<&'static Path> {
