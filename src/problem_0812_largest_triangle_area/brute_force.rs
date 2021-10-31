f7c0c6d0f03032ab62f2b1d4ce5ea810d6121290
EFanZh <efanzh@gmail.com>
2021-10-31 16:52:47 +0800

    Add imports for online submission

--- src/problem_0812_largest_triangle_area/brute_force.rs
+++ src/problem_0812_largest_triangle_area/brute_force.rs
@@ -2,6 +2,8 @@ pub struct Solution;
 
 // ------------------------------------------------------ snip ------------------------------------------------------ //
 
+use std::convert::TryInto;
+
 impl Solution {
     fn calculate_triangle_area_times_2(p0: (i32, i32), p1: (i32, i32), p2: (i32, i32)) -> i32 {
         (p0.0 * (p1.1 - p2.1) + p1.0 * (p2.1 - p0.1) + p2.0 * (p0.1 - p1.1)).abs()
