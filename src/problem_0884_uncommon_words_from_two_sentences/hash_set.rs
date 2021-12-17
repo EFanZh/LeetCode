47811f08c7c0944cdacd59bd6005d84a5e20ef16
EFanZh <efanzh@gmail.com>
2021-12-17 09:21:36 +0800

    Add problem 884: Uncommon Words from Two Sentences

---
+++ src/problem_0884_uncommon_words_from_two_sentences/hash_set.rs
@@ -0,0 +1,38 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn uncommon_from_sentences(s1: String, s2: String) -> Vec<String> {
+        let mut states = HashMap::new();
+
+        for s in &[s1.as_str(), s2.as_str()] {
+            for word in s.split(' ') {
+                states.entry(word).and_modify(|state| *state = true).or_insert(false);
+            }
+        }
+
+        states
+            .into_iter()
+            .filter_map(|(key, value)| if value { None } else { Some(key.to_string()) })
+            .collect()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn uncommon_from_sentences(s1: String, s2: String) -> Vec<String> {
+        Self::uncommon_from_sentences(s1, s2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0884_uncommon_words_from_two_sentences/mod.rs
@@ -0,0 +1,28 @@
+pub mod hash_set;
+
+pub trait Solution {
+    fn uncommon_from_sentences(s1: String, s2: String) -> Vec<String>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                ("this apple is sweet", "this apple is sour"),
+                &["sour", "sweet"] as &[_],
+            ),
+            (("apple apple", "banana"), &["banana"]),
+        ];
+
+        for ((s1, s2), expected) in test_cases {
+            assert_eq!(
+                test_utilities::unstable_sorted(S::uncommon_from_sentences(s1.to_string(), s2.to_string())),
+                expected
+            );
+        }
+    }
+}
