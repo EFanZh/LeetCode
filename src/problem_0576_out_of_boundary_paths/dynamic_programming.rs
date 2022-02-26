cd5d6c92c731da36b0e834fcd62168c6d54a31fe
EFanZh <efanzh@gmail.com>
2022-02-26 14:45:18 +0800

    Fix tests

--- src/problem_0576_out_of_boundary_paths/dynamic_programming.rs
+++ src/problem_0576_out_of_boundary_paths/dynamic_programming.rs
@@ -5,6 +5,14 @@ pub struct Solution;
 use std::mem;
 
 impl Solution {
+    fn abs_diff(lhs: usize, rhs: usize) -> usize {
+        if lhs < rhs {
+            rhs - lhs
+        } else {
+            lhs - rhs
+        }
+    }
+
     pub fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32 {
         let modulus = 1_000_000_007;
         let m = m as usize;
@@ -20,7 +28,7 @@ impl Solution {
             let border = if current_max_move == max_move - 1 { 1 } else { 0 };
 
             for y in start_row.saturating_sub(current_max_move)..(start_row + current_max_move + 1).min(m) {
-                let x_max_move = current_max_move - start_row.checked_sub(y).unwrap_or(y - start_row);
+                let x_max_move = current_max_move - Self::abs_diff(start_row, y);
 
                 for x in start_column.saturating_sub(x_max_move)..(start_column + x_max_move + 1).min(n) {
                     let top = if y == 0 { border } else { cache[n * (y - 1) + x] };
