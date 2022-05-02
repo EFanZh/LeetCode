d56dfa3f6beb288d84a18a7e91bf110bba55da4f
EFanZh <efanzh@gmail.com>
2022-05-02 23:30:00 +0800

    Add problem 1026: Maximum Difference Between Node and Ancestor

---
+++ src/problem_1026_maximum_difference_between_node_and_ancestor/mod.rs
@@ -0,0 +1,45 @@
+use crate::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn max_ancestor_diff(root: Option<Rc<RefCell<TreeNode>>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[
+                    Some(8),
+                    Some(3),
+                    Some(10),
+                    Some(1),
+                    Some(6),
+                    None,
+                    Some(14),
+                    None,
+                    None,
+                    Some(4),
+                    Some(7),
+                    Some(13),
+                ] as &[_],
+                7,
+            ),
+            (&[Some(1), None, Some(2), None, Some(0), Some(3)], 3),
+        ];
+
+        for (root, expected) in test_cases {
+            assert_eq!(
+                S::max_ancestor_diff(test_utilities::make_tree(root.iter().copied())),
+                expected
+            );
+        }
+    }
+}
---
+++ src/problem_1026_maximum_difference_between_node_and_ancestor/recursive.rs
@@ -0,0 +1,61 @@
+use crate::data_structures::TreeNode;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    fn helper(node: &RefCell<TreeNode>, result: &mut i32) -> (i32, i32) {
+        let node = node.borrow();
+        let mut min = node.val;
+        let mut max = node.val;
+
+        if let Some(left) = node.left.as_deref() {
+            let (left_min, left_max) = Self::helper(left, result);
+
+            min = min.min(left_min);
+            max = max.max(left_max);
+        }
+
+        if let Some(right) = node.right.as_deref() {
+            let (right_min, right_max) = Self::helper(right, result);
+
+            min = min.min(right_min);
+            max = max.max(right_max);
+        }
+
+        *result = (*result).max(node.val - min);
+        *result = (*result).max(max - node.val);
+
+        (min, max)
+    }
+
+    pub fn max_ancestor_diff(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut result = 0;
+
+        if let Some(root) = root {
+            Self::helper(&root, &mut result);
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn max_ancestor_diff(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::max_ancestor_diff(root)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
