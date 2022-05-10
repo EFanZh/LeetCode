6b2429edd24ba24c7066d1b32798f4c79b07ca82
EFanZh <efanzh@gmail.com>
2022-05-10 23:40:00 +0800

    Remove an unnecessary cast

--- src/problem_0976_largest_perimeter_triangle/mod.rs
+++ src/problem_0976_largest_perimeter_triangle/mod.rs
@@ -9,7 +9,7 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[2, 1, 2] as &[_], 5), (&[1, 2, 1], 0), (&[3, 6, 2, 3] as &[_], 8)];
+        let test_cases = [(&[2, 1, 2] as &[_], 5), (&[1, 2, 1], 0), (&[3, 6, 2, 3], 8)];
 
         for (nums, expected) in test_cases {
             assert_eq!(S::largest_perimeter(nums.to_vec()), expected);
