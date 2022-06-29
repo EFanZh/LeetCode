1d48b88f7684b3334bee83a788912f14183c80a1
EFanZh <efanzh@gmail.com>
2022-06-29 15:31:27 +0800

    Add problem 947: Most Stones Removed with Same Row or Column

---
+++ src/problem_0947_most_stones_removed_with_same_row_or_column/connected_components_2.rs
@@ -0,0 +1,77 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::mem;
+
+impl Solution {
+    pub fn remove_stones(stones: Vec<Vec<i32>>) -> i32 {
+        let n = stones.len();
+        let mut graph = vec![Vec::new(); n];
+        let mut connections = HashMap::with_capacity(n * 2);
+
+        for (i, stone) in stones.iter().enumerate() {
+            let [x, y]: [_; 2] = stone.as_slice().try_into().unwrap();
+
+            for key in [x, !y] {
+                connections
+                    .entry(key)
+                    .and_modify(|prev| {
+                        let prev = mem::replace(prev, i);
+
+                        graph[prev].push(i);
+                        graph[i].push(prev);
+                    })
+                    .or_insert(i);
+            }
+        }
+
+        let mut components = 0;
+        let mut queue = Vec::new();
+        let mut visited = vec![false; n];
+
+        for i in 0..n {
+            if let value @ false = &mut visited[i] {
+                *value = true;
+                components += 1;
+
+                let mut node = i;
+
+                loop {
+                    for &next in &graph[node] {
+                        if let next_value @ false = &mut visited[next] {
+                            *next_value = true;
+                            queue.push(next);
+                        }
+                    }
+
+                    if let Some(next) = queue.pop() {
+                        node = next;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        (n - components) as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn remove_stones(stones: Vec<Vec<i32>>) -> i32 {
+        Self::remove_stones(stones)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0947_most_stones_removed_with_same_row_or_column/mod.rs
@@ -0,0 +1,26 @@
+pub mod connected_components;
+pub mod connected_components_2;
+
+pub trait Solution {
+    fn remove_stones(stones: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]] as &[_], 5),
+            (&[[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]], 3),
+            (&[[0, 0]], 0),
+        ];
+
+        for (stones, expected) in test_cases {
+            assert_eq!(
+                S::remove_stones(stones.iter().copied().map(Vec::from).collect()),
+                expected
+            );
+        }
+    }
+}
