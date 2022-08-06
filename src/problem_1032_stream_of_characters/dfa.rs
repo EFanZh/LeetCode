a49a8bfa8d8628d4b405992bc69c5fee4f46f78d
EFanZh <efanzh@gmail.com>
2022-08-06 15:56:48 +0800

    Reduce memory usage

--- src/problem_1032_stream_of_characters/dfa.rs
+++ src/problem_1032_stream_of_characters/dfa.rs
@@ -1,12 +1,12 @@
 // ------------------------------------------------------ snip ------------------------------------------------------ //
 
 use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
 
 struct Node {
-    children: [usize; 26],
+    children: [u16; 26],
     has_value: bool,
 }
 
@@ -19,7 +19,7 @@ impl Node {
 impl Default for Node {
     fn default() -> Self {
         Self {
-            children: [usize::MAX; 26],
+            children: [u16::MAX; 26],
             has_value: false,
         }
     }
@@ -40,9 +40,8 @@ impl Hash for Node {
 }
 
 pub struct StreamChecker {
-    transitions: Vec<[usize; 26]>,
-    terminal_states: HashSet<usize>,
-    current_state: usize,
+    states: Vec<([u32; 26], bool)>,
+    current_state: u32,
 }
 
 impl StreamChecker {
@@ -50,13 +49,13 @@ impl StreamChecker {
         // Build trie.
 
         let mut pool = vec![Node::default()];
-        let mut pool_length = pool.len();
+        let mut pool_length = 1;
 
         for word in &words {
             let mut node = 0;
 
             for c in word.bytes() {
-                let child = &mut pool[node].children[usize::from(c) - usize::from(b'a')];
+                let child = &mut pool[usize::from(node)].children[usize::from(c) - usize::from(b'a')];
 
                 node = if *child < pool_length {
                     *child
@@ -72,7 +71,7 @@ impl StreamChecker {
                 };
             }
 
-            pool[node].has_value = true;
+            pool[usize::from(node)].has_value = true;
         }
 
         // Build DFA.
@@ -80,17 +79,16 @@ impl StreamChecker {
         let root = pool.first().unwrap();
         let mut current_state: Rc<[&Node]> = Rc::new([root]);
         let mut next_state_buffer = Vec::new();
-        let mut transitions = Vec::new();
         let mut state_ids = HashMap::<Rc<[_]>, _>::new();
-        let mut terminal_states = HashSet::new();
         let mut queue = VecDeque::new();
+        let mut states = Vec::new();
 
         loop {
-            let mut transition = [0; 26];
+            let mut transitions = [0; 26];
 
-            for (c, next) in transition.iter_mut().enumerate() {
+            for (c, next) in transitions.iter_mut().enumerate() {
                 for &sub_state in current_state.iter() {
-                    if let Some(child) = pool.get(sub_state.children[c]) {
+                    if let Some(child) = pool.get(usize::from(sub_state.children[c])) {
                         next_state_buffer.push(child);
                     }
                 }
@@ -104,15 +102,11 @@ impl StreamChecker {
 
                     next_state_buffer.clear();
 
-                    let candidate_id = state_ids.len() + 1;
+                    let candidate_id = state_ids.len() as u32 + 1;
 
                     *next = match state_ids.entry(next_state) {
                         Entry::Occupied(entry) => *entry.get(),
                         Entry::Vacant(entry) => {
-                            if entry.key().iter().any(|node| node.has_value) {
-                                terminal_states.insert(candidate_id);
-                            }
-
                             queue.push_back(Rc::clone(entry.key()));
 
                             entry.insert(candidate_id);
@@ -123,7 +117,7 @@ impl StreamChecker {
                 }
             }
 
-            transitions.push(transition);
+            states.push((transitions, current_state.iter().any(|node| node.has_value)));
 
             if let Some(next_state) = queue.pop_front() {
                 current_state = next_state;
@@ -133,16 +127,15 @@ impl StreamChecker {
         }
 
         Self {
-            transitions,
-            terminal_states,
+            states,
             current_state: 0,
         }
     }
 
     fn query(&mut self, letter: char) -> bool {
-        self.current_state = self.transitions[self.current_state][u32::from(letter) as usize - usize::from(b'a')];
+        self.current_state = self.states[self.current_state as usize].0[u32::from(letter) as usize - usize::from(b'a')];
 
-        self.terminal_states.contains(&self.current_state)
+        self.states[self.current_state as usize].1
     }
 }
 
