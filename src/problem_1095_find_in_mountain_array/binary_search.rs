8c64b483d32be7b8c5bc6a73b720fb9f9c0faa70
EFanZh <efanzh@gmail.com>
2022-08-26 09:23:36 +0800

    Add problem 1095: Find in Mountain Array

---
+++ src/problem_1095_find_in_mountain_array/binary_search.rs
@@ -0,0 +1,62 @@
+use super::MountainArray;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cmp::Ordering;
+
+impl Solution {
+    fn partition_point(mut left: u32, mut right: u32, mut f: impl FnMut(u32) -> bool) -> u32 {
+        while left < right {
+            let middle = (left + right) / 2;
+
+            if f(middle) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+
+        left
+    }
+
+    fn binary_search(mut left: u32, mut right: u32, mut f: impl FnMut(u32) -> Ordering) -> Option<u32> {
+        while left < right {
+            let middle = (left + right) / 2;
+
+            match f(middle) {
+                Ordering::Less => left = middle + 1,
+                Ordering::Equal => return Some(middle),
+                Ordering::Greater => right = middle,
+            }
+        }
+
+        None
+    }
+
+    pub fn find_in_mountain_array(target: i32, mountain_arr: &MountainArray) -> i32 {
+        let n = mountain_arr.length() as u32;
+        let peak = Self::partition_point(1, n - 2, |i| mountain_arr.get(i as _) < mountain_arr.get((i + 1) as _));
+
+        Self::binary_search(0, peak + 1, |i| mountain_arr.get(i as _).cmp(&target))
+            .or_else(|| Self::binary_search(peak + 1, n, |i| target.cmp(&mountain_arr.get(i as _))))
+            .unwrap_or(u32::MAX) as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn find_in_mountain_array(target: i32, mountain_arr: &MountainArray) -> i32 {
+        Self::find_in_mountain_array(target, mountain_arr)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1095_find_in_mountain_array/mod.rs
@@ -0,0 +1,53 @@
+pub mod binary_search;
+
+use std::cell::Cell;
+
+pub struct MountainArray<'a> {
+    data: &'a [i32],
+    remaining_get_calls: Cell<u8>,
+}
+
+impl MountainArray<'_> {
+    fn get(&self, index: i32) -> i32 {
+        assert_ne!(self.remaining_get_calls.get(), 0);
+
+        self.remaining_get_calls.set(self.remaining_get_calls.get());
+
+        self.data[index as usize]
+    }
+
+    fn length(&self) -> i32 {
+        self.data.len() as _
+    }
+}
+
+pub trait Solution {
+    fn find_in_mountain_array(target: i32, mountain_arr: &MountainArray) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{MountainArray, Solution};
+    use std::cell::Cell;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5, 3, 1] as &[_], 3), 2),
+            ((&[0, 1, 2, 4, 2, 1], 3), -1),
+            ((&[1, 5, 2], 2), 2),
+        ];
+
+        for ((mountain_arr, target), expected) in test_cases {
+            assert_eq!(
+                S::find_in_mountain_array(
+                    target,
+                    &MountainArray {
+                        data: mountain_arr,
+                        remaining_get_calls: Cell::new(100)
+                    }
+                ),
+                expected,
+            );
+        }
+    }
+}
