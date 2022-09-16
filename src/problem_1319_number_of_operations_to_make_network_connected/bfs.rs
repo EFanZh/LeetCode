d2788d2824dbf598fb1fd8e3058222054d0620b4
EFanZh <efanzh@gmail.com>
2022-09-16 20:21:37 +0800

    Add problem 1139: Largest 1-Bordered Square

---
+++ src/problem_1319_number_of_operations_to_make_network_connected/bfs.rs
@@ -0,0 +1,74 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::VecDeque;
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn make_connected(n: i32, connections: Vec<Vec<i32>>) -> i32 {
+        let n = n as usize;
+
+        if connections.len() < n - 1 {
+            -1
+        } else {
+            let mut graph = vec![Vec::new(); n];
+
+            for connection in connections {
+                let [from, to]: [_; 2] = connection.try_into().ok().unwrap();
+                let from = from as usize;
+                let to = to as usize;
+
+                graph[from].push(to);
+                graph[to].push(from);
+            }
+
+            let mut queue = VecDeque::new();
+            let mut visited = vec![false; n];
+            let mut components = 0;
+
+            for node in 0..n {
+                if let visited_value @ false = &mut visited[node] {
+                    *visited_value = true;
+
+                    components += 1;
+
+                    let mut node = node;
+
+                    loop {
+                        for &next in &graph[node] {
+                            if let visited_value @ false = &mut visited[next] {
+                                *visited_value = true;
+                                queue.push_back(next);
+                            }
+                        }
+
+                        if let Some(next) = queue.pop_front() {
+                            node = next;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            components - 1
+        }
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn make_connected(n: i32, connections: Vec<Vec<i32>>) -> i32 {
+        Self::make_connected(n, connections)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1319_number_of_operations_to_make_network_connected/mod.rs
@@ -0,0 +1,25 @@
+pub mod bfs;
+
+pub trait Solution {
+    fn make_connected(n: i32, connections: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((4, &[[0, 1], [0, 2], [1, 2]] as &[_]), 1),
+            ((6, &[[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]), 2),
+            ((6, &[[0, 1], [0, 2], [0, 3], [1, 2]]), -1),
+        ];
+
+        for ((n, connections), expected) in test_cases {
+            assert_eq!(
+                S::make_connected(n, connections.iter().copied().map(Vec::from).collect()),
+                expected,
+            );
+        }
+    }
+}
