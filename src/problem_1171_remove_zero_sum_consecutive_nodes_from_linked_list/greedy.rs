c908d5dacf56eb7ebcae8636c247269e98fbd58a
EFanZh <efanzh@gmail.com>
2022-10-07 13:56:00 +0800

    Add problem 1171: Remove Zero Sum Consecutive Nodes from Linked List

---
+++ src/problem_1171_remove_zero_sum_consecutive_nodes_from_linked_list/greedy.rs
@@ -0,0 +1,62 @@
+use crate::data_structures::ListNode;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn remove_zero_sum_sublists(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head = head;
+        let mut nodes = Vec::<Box<ListNode>>::new();
+        let mut sums = HashMap::from([(0, 0)]);
+        let mut sum = 0;
+
+        while let Some(mut node) = head {
+            head = node.next.take();
+            sum += node.val;
+
+            match sums.entry(sum) {
+                Entry::Occupied(entry) => {
+                    let mut current_sum = sum - node.val;
+
+                    for node in nodes.drain(*entry.get()..).rev() {
+                        sums.remove(&current_sum);
+                        current_sum -= node.val;
+                    }
+                }
+                Entry::Vacant(entry) => {
+                    nodes.push(node);
+                    entry.insert(nodes.len());
+                }
+            }
+        }
+
+        let mut head = None;
+        let mut slot = &mut head;
+
+        for node in nodes {
+            slot = &mut slot.insert(node).next;
+        }
+
+        head
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn remove_zero_sum_sublists(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::remove_zero_sum_sublists(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
