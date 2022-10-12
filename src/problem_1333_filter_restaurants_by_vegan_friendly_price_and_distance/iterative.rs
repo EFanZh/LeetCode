5e2ac84639f5cce2b1138f17a02a6daceceaddb9
EFanZh <efanzh@gmail.com>
2022-10-12 19:19:07 +0800

    Add problem 1333: Filter Restaurants by Vegan-Friendly, Price and Distance

---
+++ src/problem_1333_filter_restaurants_by_vegan_friendly_price_and_distance/iterative.rs
@@ -0,0 +1,64 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cmp::Reverse;
+use std::convert::TryInto;
+
+impl Solution {
+    fn unwrap_restaurant(restaurant: &[i32]) -> [i32; 5] {
+        restaurant.try_into().ok().unwrap()
+    }
+
+    pub fn filter_restaurants(
+        restaurants: Vec<Vec<i32>>,
+        vegan_friendly: i32,
+        max_price: i32,
+        max_distance: i32,
+    ) -> Vec<i32> {
+        let mut restaurants = restaurants;
+
+        if vegan_friendly == 0 {
+            restaurants.retain(|restaurant| {
+                let [_, _, _, price, distance]: [_; 5] = Self::unwrap_restaurant(restaurant);
+
+                price <= max_price && distance <= max_distance
+            });
+        } else {
+            restaurants.retain(|restaurant| {
+                let [_, _, vegan_friendly, price, distance]: [_; 5] = Self::unwrap_restaurant(restaurant);
+
+                vegan_friendly != 0 && price <= max_price && distance <= max_distance
+            });
+        }
+
+        restaurants.sort_unstable_by_key(|restaurant| {
+            let [id, rating, _, _, _]: [_; 5] = Self::unwrap_restaurant(restaurant);
+
+            Reverse((rating, id))
+        });
+
+        restaurants.into_iter().map(|restaurant| restaurant[0]).collect()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn filter_restaurants(
+        restaurants: Vec<Vec<i32>>,
+        vegan_friendly: i32,
+        max_price: i32,
+        max_distance: i32,
+    ) -> Vec<i32> {
+        Self::filter_restaurants(restaurants, vegan_friendly, max_price, max_distance)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1333_filter_restaurants_by_vegan_friendly_price_and_distance/mod.rs
@@ -0,0 +1,77 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn filter_restaurants(
+        restaurants: Vec<Vec<i32>>,
+        vegan_friendly: i32,
+        max_price: i32,
+        max_distance: i32,
+    ) -> Vec<i32>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[
+                        [1, 4, 1, 40, 10],
+                        [2, 8, 0, 50, 5],
+                        [3, 8, 1, 30, 4],
+                        [4, 10, 0, 10, 3],
+                        [5, 1, 1, 15, 1],
+                    ] as &[_],
+                    1,
+                    50,
+                    10,
+                ),
+                &[3, 1, 5] as &[_],
+            ),
+            (
+                (
+                    &[
+                        [1, 4, 1, 40, 10],
+                        [2, 8, 0, 50, 5],
+                        [3, 8, 1, 30, 4],
+                        [4, 10, 0, 10, 3],
+                        [5, 1, 1, 15, 1],
+                    ],
+                    0,
+                    50,
+                    10,
+                ),
+                &[4, 3, 2, 1, 5],
+            ),
+            (
+                (
+                    &[
+                        [1, 4, 1, 40, 10],
+                        [2, 8, 0, 50, 5],
+                        [3, 8, 1, 30, 4],
+                        [4, 10, 0, 10, 3],
+                        [5, 1, 1, 15, 1],
+                    ],
+                    0,
+                    30,
+                    3,
+                ),
+                &[4, 5],
+            ),
+        ];
+
+        for ((restaurants, vegan_friendly, max_price, max_distance), expected) in test_cases {
+            assert_eq!(
+                S::filter_restaurants(
+                    restaurants.iter().copied().map(Vec::from).collect(),
+                    vegan_friendly,
+                    max_price,
+                    max_distance
+                ),
+                expected
+            );
+        }
+    }
+}
