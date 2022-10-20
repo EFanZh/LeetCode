026bb9e627d21186717ecffede434957fe7d4f2d
EFanZh <efanzh@gmail.com>
2022-10-20 21:36:25 +0800

    Add problem 1346: Check If N and Its Double Exist

---
+++ src/problem_1346_check_if_n_and_its_double_exist/mod.rs
@@ -0,0 +1,18 @@
+pub mod hash_map;
+
+pub trait Solution {
+    fn check_if_exist(arr: Vec<i32>) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[10, 2, 5, 3] as &[_], true), (&[3, 1, 7, 11], false)];
+
+        for (arr, expected) in test_cases {
+            assert_eq!(S::check_if_exist(arr.to_vec()), expected);
+        }
+    }
+}
