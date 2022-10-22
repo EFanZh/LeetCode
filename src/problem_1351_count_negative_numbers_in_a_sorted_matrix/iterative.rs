cdf5820241776f14defa775cb04d625088c86d6a
EFanZh <efanzh@gmail.com>
2022-10-22 23:30:00 +0800

    Add problem 1351: Count Negative Numbers in a Sorted Matrix

---
+++ src/problem_1351_count_negative_numbers_in_a_sorted_matrix/iterative.rs
@@ -0,0 +1,40 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+        let rows = grid.len();
+        let columns = grid.first().map_or(0, Vec::len);
+        let mut y = 0;
+        let mut x = columns.wrapping_sub(1);
+        let mut result = 0;
+
+        while let Some(&value) = grid.get(y).and_then(|row| row.get(x)) {
+            if value < 0 {
+                result += rows - y;
+                x = x.wrapping_sub(1);
+            } else {
+                y += 1;
+            }
+        }
+
+        result as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
+        Self::count_negatives(grid)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
