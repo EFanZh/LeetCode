ec38224502088da631f23cc6772956cd2507d7dd
EFanZh <efanzh@gmail.com>
2022-11-13 23:30:00 +0800

    Add problem 1224: Maximum Equal Frequency

---
+++ src/problem_1224_maximum_equal_frequency/iterative.rs
@@ -0,0 +1,58 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::HashMap;
+
+impl Solution {
+    fn add_and_get(lhs: &mut u32, rhs: u32) -> u32 {
+        *lhs = u32::wrapping_add(*lhs, rhs);
+
+        *lhs
+    }
+
+    pub fn max_equal_freq(nums: Vec<i32>) -> i32 {
+        let mut counts = HashMap::<_, u32>::new();
+        let mut count_counts = vec![0_u32; nums.len() + 1];
+        let mut count_count_count = 0;
+        let mut result = 0;
+
+        for (candidate, num) in (1..).zip(nums) {
+            if count_count_count == 1 {
+                result = candidate;
+            }
+
+            let count = *counts.entry(num).and_modify(|count| *count += 1).or_insert(1) as usize;
+
+            if Self::add_and_get(&mut count_counts[count - 1], u32::MAX) == 0 {
+                count_count_count -= 1;
+            }
+
+            if Self::add_and_get(&mut count_counts[count], 1) == 1 {
+                count_count_count += 1;
+            }
+
+            if count_count_count == 2 && (count_counts[1] == 1 || count_counts.get(count + 1).copied() == Some(1)) {
+                result = candidate;
+            }
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn max_equal_freq(nums: Vec<i32>) -> i32 {
+        Self::max_equal_freq(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1224_maximum_equal_frequency/mod.rs
@@ -0,0 +1,25 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn max_equal_freq(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[2, 2, 1, 1, 5, 3, 3, 5] as &[_], 7),
+            (&[1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5], 13),
+            (&[10, 2, 8, 9, 3, 8, 1, 5, 2, 3, 7, 6], 8),
+            (&[1, 1], 2),
+            (&[1, 2, 3, 4, 5, 6, 7, 8, 9, 9], 10),
+            (&[1, 1, 1, 2, 2, 2, 3, 3, 3], 7),
+        ];
+
+        for (nums, expected) in test_cases {
+            assert_eq!(S::max_equal_freq(nums.to_vec()), expected);
+        }
+    }
+}
