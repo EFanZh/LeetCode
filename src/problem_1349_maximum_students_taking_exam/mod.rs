f2f43671b59729ceb4be154f5495a9688832ae26
EFanZh <efanzh@gmail.com>
2022-12-10 21:24:05 +0800

    Add problem 1349: Maximum Students Taking Exam

---
+++ src/problem_1349_maximum_students_taking_exam/mod.rs
@@ -0,0 +1,31 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn max_students(seats: Vec<Vec<char>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&["#.##.#", ".####.", "#.##.#"] as &[_], 4),
+            (&[".#", "##", "#.", "##", ".#"], 3),
+            (&["#...#", ".#.#.", "..#..", ".#.#.", "#...#"], 10),
+            (
+                &[
+                    "....#...", "........", "........", "......#.", "........", "..#.....", "........", "...#..#.",
+                ],
+                31,
+            ),
+        ];
+
+        for (seats, expected) in test_cases {
+            assert_eq!(
+                S::max_students(seats.iter().map(|row| row.chars().collect()).collect()),
+                expected
+            );
+        }
+    }
+}
