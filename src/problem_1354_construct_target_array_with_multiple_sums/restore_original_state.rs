83ad65e018b038e3e339200b7800509313f65c05
EFanZh <efanzh@gmail.com>
2022-12-20 19:40:08 +0800

    Add problem 1354: Construct Target Array With Multiple Sums

---
+++ src/problem_1354_construct_target_array_with_multiple_sums/restore_original_state.rs
@@ -0,0 +1,50 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::BinaryHeap;
+use std::num::NonZeroU32;
+
+impl Solution {
+    pub fn is_possible(target: Vec<i32>) -> bool {
+        let mut sum = target.iter().copied().sum::<i32>() as u32;
+        let mut queue = target.into_iter().map(|value| value as u32).collect::<BinaryHeap<_>>();
+
+        loop {
+            let mut top = queue.peek_mut().unwrap();
+            let old_top = *top;
+            let rest = sum - old_top;
+
+            let rest = if let Some(rest) = NonZeroU32::new(rest).filter(|_| old_top >= rest) {
+                rest
+            } else {
+                return old_top == 1;
+            };
+
+            let new_top = old_top % rest;
+
+            if new_top == 0 {
+                return rest.get() == 1;
+            }
+
+            sum -= old_top - new_top;
+            *top = new_top;
+        }
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn is_possible(target: Vec<i32>) -> bool {
+        Self::is_possible(target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
