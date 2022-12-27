6d8fe38ad5899018f5b2a93930c52fb4b2b99bcf
EFanZh <efanzh@gmail.com>
2022-12-27 22:58:59 +0800

    Add problem 1385: Find the Distance Value Between Two Arrays

---
+++ src/problem_1385_find_the_distance_value_between_two_arrays/binary_search.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn find_the_distance_value(arr1: Vec<i32>, arr2: Vec<i32>, d: i32) -> i32 {
+        let mut arr2 = arr2;
+
+        arr2.sort_unstable();
+
+        let mut result = 0;
+
+        for value in arr1 {
+            let low = value - d;
+            let high = value + d;
+            let i = arr2.partition_point(|&x| x < low);
+
+            if arr2.get(i).map_or(true, |&x| x > high) {
+                result += 1;
+            }
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn find_the_distance_value(arr1: Vec<i32>, arr2: Vec<i32>, d: i32) -> i32 {
+        Self::find_the_distance_value(arr1, arr2, d)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
