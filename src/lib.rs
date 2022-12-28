201f9563044dbf851aa873b106e1ba1241c55eb8
EFanZh <efanzh@gmail.com>
2022-12-28 19:39:30 +0800

    Add problem 1386: Cinema Seat Allocation

--- src/lib.rs
+++ src/lib.rs
@@ -1102,6 +1102,7 @@ pub mod problem_1380_lucky_numbers_in_a_matrix;
 pub mod problem_1381_design_a_stack_with_increment_operation;
 pub mod problem_1382_balance_a_binary_search_tree;
 pub mod problem_1385_find_the_distance_value_between_two_arrays;
+pub mod problem_1386_cinema_seat_allocation;
 pub mod problem_1387_sort_integers_by_the_power_value;
 pub mod problem_1720_decode_xored_array;
 pub mod problem_1736_latest_time_by_replacing_hidden_digits;
---
+++ src/problem_1386_cinema_seat_allocation/mod.rs
@@ -0,0 +1,25 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn max_number_of_families(n: i32, reserved_seats: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((3, &[[1, 2], [1, 3], [1, 8], [2, 6], [3, 1], [3, 10]] as &[_]), 4),
+            ((2, &[[2, 1], [1, 8], [2, 6]]), 2),
+            ((4, &[[4, 3], [1, 4], [4, 6], [1, 7]]), 4),
+        ];
+
+        for ((n, reserved_seats), expected) in test_cases {
+            assert_eq!(
+                S::max_number_of_families(n, reserved_seats.iter().copied().map(Vec::from).collect()),
+                expected,
+            );
+        }
+    }
+}
