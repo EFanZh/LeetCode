e8de203373b8e507761fc85bf71d470666afd9c6
EFanZh <efanzh@gmail.com>
2022-12-31 18:00:27 +0800

    Optimize solution to problem 239: Sliding Window Maximum

--- src/problem_0239_sliding_window_maximum/mod.rs
+++ src/problem_0239_sliding_window_maximum/mod.rs
@@ -1,4 +1,5 @@
 pub mod deque;
+pub mod deque_2;
 
 pub trait Solution {
     fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32>;
@@ -13,6 +14,7 @@ mod tests {
             ((&[1, 3, -1, -3, 5, 3, 6, 7] as &[_], 3), &[3, 3, 5, 5, 6, 7] as &[_]),
             ((&[1], 1), &[1]),
             ((&[-7, -8, 7, 5, 7, 1, 6, 0], 4), &[7, 7, 7, 7, 7]),
+            ((&[1, -1], 1), &[1, -1]),
         ];
 
         for ((nums, k), expected) in test_cases {
