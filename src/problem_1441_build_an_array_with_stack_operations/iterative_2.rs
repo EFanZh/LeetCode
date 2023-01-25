d62f440735d7454441307d33517aa2815eb82fd3
EFanZh <efanzh@gmail.com>
2023-01-25 11:42:16 +0800

    Add a new solution to problem 1441: Build an Array With Stack Operations

---
+++ src/problem_1441_build_an_array_with_stack_operations/iterative_2.rs
@@ -0,0 +1,42 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
+        let _ = n;
+        let mut result = Vec::new();
+        let mut i = 0;
+
+        for value in target {
+            loop {
+                result.push(String::from("Push"));
+                i += 1;
+
+                if i == value {
+                    break;
+                }
+
+                result.push(String::from("Pop"));
+            }
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
+        Self::build_array(target, n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
