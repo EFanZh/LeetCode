369994c3bc0e856453a238df9fa3978ed638c4e7
EFanZh <efanzh@gmail.com>
2023-02-13 10:51:32 +0800

    Add problem 1443: Minimum Time to Collect All Apples in a Tree

---
+++ src/problem_1443_minimum_time_to_collect_all_apples_in_a_tree/dfs.rs
@@ -0,0 +1,68 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::convert::TryInto;
+
+impl Solution {
+    fn dfs(graph: &[(Vec<usize>, bool)], parent: usize, node: usize) -> i32 {
+        let (neighbors, has_apple) = &graph[node];
+        let has_apple = *has_apple;
+        let mut total_cost = 0;
+
+        for &neighbor in neighbors {
+            if neighbor != parent {
+                total_cost += Self::dfs(graph, node, neighbor);
+            }
+        }
+
+        if !has_apple && total_cost == 0 {
+            0
+        } else {
+            total_cost + 2
+        }
+    }
+
+    pub fn min_time(n: i32, edges: Vec<Vec<i32>>, has_apple: Vec<bool>) -> i32 {
+        let _ = n;
+
+        let mut graph = has_apple
+            .into_iter()
+            .map(|has_apple| (Vec::new(), has_apple))
+            .collect::<Vec<_>>();
+
+        for edge in edges {
+            let [from, to]: [_; 2] = edge.try_into().ok().unwrap();
+            let from = from as usize;
+            let to = to as usize;
+
+            graph[from].0.push(to);
+            graph[to].0.push(from);
+        }
+
+        let graph = graph.as_slice();
+        let mut result = 0;
+
+        for &neighbor in &graph[0].0 {
+            result += Self::dfs(graph, 0, neighbor);
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn min_time(n: i32, edges: Vec<Vec<i32>>, has_apple: Vec<bool>) -> i32 {
+        Self::min_time(n, edges, has_apple)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1443_minimum_time_to_collect_all_apples_in_a_tree/mod.rs
@@ -0,0 +1,54 @@
+pub mod dfs;
+
+pub trait Solution {
+    fn min_time(n: i32, edges: Vec<Vec<i32>>, has_apple: Vec<bool>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    7,
+                    &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]] as &[_],
+                    &[false, false, true, false, true, true, false] as &[_],
+                ),
+                8,
+            ),
+            (
+                (
+                    7,
+                    &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                    &[false, false, true, false, false, true, false],
+                ),
+                6,
+            ),
+            (
+                (
+                    7,
+                    &[[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]],
+                    &[false, false, false, false, false, false, false],
+                ),
+                0,
+            ),
+            (
+                (
+                    5,
+                    &[[0, 1], [0, 2], [1, 3], [0, 4]],
+                    &[false, false, false, true, false],
+                ),
+                4,
+            ),
+        ];
+
+        for ((n, edges, has_apple), expected) in test_cases {
+            assert_eq!(
+                S::min_time(n, edges.iter().copied().map(Vec::from).collect(), has_apple.to_vec()),
+                expected
+            );
+        }
+    }
+}
