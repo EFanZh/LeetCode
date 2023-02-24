93618026fe509c21e10b315eceab30966bd7caa2
EFanZh <efanzh@gmail.com>
2023-02-24 19:26:42 +0800

    Add problem 1466: Reorder Routes to Make All Paths Lead to the City Zero

---
+++ src/problem_1466_reorder_routes_to_make_all_paths_lead_to_the_city_zero/mod.rs
@@ -0,0 +1,25 @@
+pub mod dfs;
+
+pub trait Solution {
+    fn min_reorder(n: i32, connections: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((6, &[[0, 1], [1, 3], [2, 3], [4, 0], [4, 5]] as &[_]), 3),
+            ((5, &[[1, 0], [1, 2], [3, 2], [3, 4]]), 2),
+            ((3, &[[1, 0], [2, 0]]), 0),
+        ];
+
+        for ((n, connections), expected) in test_cases {
+            assert_eq!(
+                S::min_reorder(n, connections.iter().copied().map(Vec::from).collect()),
+                expected,
+            );
+        }
+    }
+}
