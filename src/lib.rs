23486fe155583d40c0ac44f86fa8106e73e50ac0
EFanZh <efanzh@gmail.com>
2023-04-07 18:32:19 +0800

    Add problem 1561: Maximum Number of Coins You Can Get

--- src/lib.rs
+++ src/lib.rs
@@ -1250,6 +1250,7 @@ pub mod problem_1551_minimum_operations_to_make_array_equal;
 pub mod problem_1552_magnetic_force_between_two_balls;
 pub mod problem_1556_thousand_separator;
 pub mod problem_1557_minimum_number_of_vertices_to_reach_all_nodes;
+pub mod problem_1561_maximum_number_of_coins_you_can_get;
 pub mod problem_1720_decode_xored_array;
 pub mod problem_1736_latest_time_by_replacing_hidden_digits;
 pub mod problem_1743_restore_the_array_from_adjacent_pairs;
