9906b794a4117124863ccc0c5dea1e2b2407ab56
EFanZh <efanzh@gmail.com>
2023-04-14 23:30:00 +0800

    Add problem 1576: Replace All ?'s to Avoid Consecutive Repeating Characters

---
+++ src/problem_1576_replace_all_s_to_avoid_consecutive_repeating_characters/mod.rs
@@ -0,0 +1,32 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn modify_string(s: String) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = ["?zs", "ubv?w", "j?qg??b", "??yw?ipkj?", "b?a"];
+
+        for s in test_cases {
+            let result = S::modify_string(s.to_string());
+
+            assert_eq!(result.len(), s.len());
+
+            assert!(s
+                .bytes()
+                .zip(result.bytes())
+                .all(|(lhs, rhs)| rhs.is_ascii_lowercase() && (lhs == b'?' || lhs == rhs)));
+
+            let mut prev = 0;
+
+            for c in result.bytes() {
+                assert_ne!(c, prev);
+                prev = c;
+            }
+        }
+    }
+}
