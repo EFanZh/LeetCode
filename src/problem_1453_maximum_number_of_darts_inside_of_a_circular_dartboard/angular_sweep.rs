bc3dfb68d06592964a58c56dc4d900f9fd2341cf
EFanZh <efanzh@gmail.com>
2023-05-02 11:37:01 +0800

    Add problem 1453: Maximum Number of Darts Inside of a Circular Dartboard

---
+++ src/problem_1453_maximum_number_of_darts_inside_of_a_circular_dartboard/angular_sweep.rs
@@ -0,0 +1,79 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn num_points(darts: Vec<Vec<i32>>, r: i32) -> i32 {
+        let darts = darts
+            .into_iter()
+            .map(|dart| {
+                let [x, y]: [_; 2] = dart.try_into().ok().unwrap();
+
+                (x, y)
+            })
+            .collect::<Box<_>>();
+
+        let darts = darts.as_ref();
+        let max_distance_squared = r * r * 4;
+        let mut angles = Vec::new();
+        let mut result = 0;
+
+        for center in darts {
+            let mut count = 0;
+
+            // Collect points that is reachable from `center`.
+
+            for point in darts {
+                let x_diff = point.0 - center.0;
+                let y_diff = point.1 - center.1;
+                let distance_squared = x_diff * x_diff + y_diff * y_diff;
+
+                if distance_squared == 0 {
+                    count += 1;
+                } else if distance_squared <= max_distance_squared {
+                    let base = f64::from(y_diff).atan2(f64::from(x_diff));
+
+                    let delta = (f64::from(distance_squared) / f64::from(max_distance_squared))
+                        .sqrt()
+                        .acos();
+
+                    angles.push((base - delta, false));
+                    angles.push((base + delta, true));
+                }
+            }
+
+            // Angular sweep.
+
+            angles.sort_unstable_by(|lhs, rhs| lhs.0.partial_cmp(&rhs.0).unwrap().then_with(|| lhs.1.cmp(&rhs.1)));
+
+            result = result.max(count);
+
+            for &(_, is_exit) in &angles {
+                count += 1 - i32::from(is_exit) * 2;
+                result = result.max(count);
+            }
+
+            angles.clear();
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn num_points(darts: Vec<Vec<i32>>, r: i32) -> i32 {
+        Self::num_points(darts, r)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
