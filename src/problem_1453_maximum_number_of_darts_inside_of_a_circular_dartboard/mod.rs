796c221f501bc297e9df507d264b3c3fa6ac6419
EFanZh <efanzh@gmail.com>
2023-05-02 11:37:01 +0800

    Add problem 1453: Maximum Number of Darts Inside of a Circular Dartboard

---
+++ src/problem_1453_maximum_number_of_darts_inside_of_a_circular_dartboard/mod.rs
@@ -0,0 +1,25 @@
+pub mod angular_sweep;
+
+pub trait Solution {
+    fn num_points(darts: Vec<Vec<i32>>, r: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[[-2, 0], [2, 0], [0, 2], [0, -2]] as &[_], 2), 4),
+            ((&[[-3, 0], [3, 0], [2, 6], [5, 4], [0, 9], [7, 8]], 5), 5),
+            ((&[[-2, 0], [2, 0], [0, 2], [0, -2]], 1), 1),
+        ];
+
+        for ((darts, r), expected) in test_cases {
+            assert_eq!(
+                S::num_points(darts.iter().copied().map(Vec::from).collect(), r),
+                expected,
+            );
+        }
+    }
+}
