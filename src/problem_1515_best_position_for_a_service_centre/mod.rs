2fe7ae7857385ae592b6bbab4bfb36d83b8046bd
EFanZh <efanzh@gmail.com>
2023-05-27 18:31:58 +0800

    Add problem 1515: Best Position for a Service Centre

---
+++ src/problem_1515_best_position_for_a_service_centre/mod.rs
@@ -0,0 +1,26 @@
+pub mod geometric_median;
+
+pub trait Solution {
+    fn get_min_dist_sum(positions: Vec<Vec<i32>>) -> f64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[[0, 1], [1, 0], [1, 2], [2, 1]] as &[_], 4.0),
+            (&[[1, 1], [3, 3]], f64::sqrt(8.0)),
+            (&[[1, 1]], 0.0),
+            (&[[27, 90], [99, 75], [99, 38]], 109.303_171_735_149_06),
+        ];
+
+        for (positions, expected) in test_cases {
+            approx::assert_ulps_eq!(
+                S::get_min_dist_sum(positions.iter().copied().map(Vec::from).collect()),
+                expected,
+            );
+        }
+    }
+}
