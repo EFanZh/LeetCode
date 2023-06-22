9eccefb5ee5e37182aa628cb62741d51fc72c445
EFanZh <efanzh@gmail.com>
2023-06-22 10:41:59 +0800

    Refactor

--- src/problem_0908_smallest_range_i/iterative.rs
+++ src/problem_0908_smallest_range_i/iterative.rs
@@ -4,38 +4,28 @@ pub struct Solution;
 
 impl Solution {
     fn min_max(nums: &[i32]) -> (i32, i32) {
-        if let [first, second, rest @ ..] = nums {
-            let (mut min, mut max) = if second < first {
-                (*second, *first)
+        let mut iter = nums.iter().copied();
+        let mut min = iter.next().unwrap();
+        let mut max = min;
+
+        while let Some(left) = iter.next() {
+            if let Some(right) = iter.next() {
+                let (new_min, new_max) = if right < left { (right, left) } else { (left, right) };
+
+                min = min.min(new_min);
+                max = max.max(new_max);
             } else {
-                (*first, *second)
-            };
-
-            let mut iter = rest.iter().copied();
-
-            while let Some(left) = iter.next() {
-                if let Some(right) = iter.next() {
-                    let (new_min, new_max) = if right < left { (right, left) } else { (left, right) };
-
-                    min = min.min(new_min);
-                    max = max.max(new_max);
-                } else {
-                    if left < min {
-                        min = left;
-                    } else if left > max {
-                        max = left;
-                    }
-
-                    break;
+                if left < min {
+                    min = left;
+                } else if left > max {
+                    max = left;
                 }
+
+                break;
             }
-
-            (min, max)
-        } else {
-            let first = nums[0];
-
-            (first, first)
         }
+
+        (min, max)
     }
 
     pub fn smallest_range_i(nums: Vec<i32>, k: i32) -> i32 {
