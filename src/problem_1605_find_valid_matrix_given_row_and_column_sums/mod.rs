553e22bd05f7ccc0e463b7880df72cdaf0b8c69e
EFanZh <efanzh@gmail.com>
2023-07-06 23:43:19 +0800

    Add problem 1605: Find Valid Matrix Given Row and Column Sums

---
+++ src/problem_1605_find_valid_matrix_given_row_and_column_sums/mod.rs
@@ -0,0 +1,32 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn restore_matrix(row_sum: Vec<i32>, col_sum: Vec<i32>) -> Vec<Vec<i32>>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[3, 8] as &[_], &[4, 7] as &[_]), (&[5, 7, 10], &[8, 6, 8])];
+
+        for (row_sum, col_sum) in test_cases {
+            let result = S::restore_matrix(row_sum.to_vec(), col_sum.to_vec());
+
+            assert!(result
+                .iter()
+                .map(|row| {
+                    assert_eq!(row.len(), col_sum.len());
+
+                    row.iter().sum::<i32>()
+                })
+                .eq(row_sum.iter().copied()));
+
+            assert!(col_sum
+                .iter()
+                .enumerate()
+                .all(|(i, &sum)| { result.iter().map(|row| row[i]).sum::<i32>() == sum }));
+        }
+    }
+}
