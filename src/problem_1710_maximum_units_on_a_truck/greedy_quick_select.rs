843734d2024158e0152d041b4ee3cb933cbf24ef
EFanZh <efanzh@gmail.com>
2023-07-11 21:33:05 +0800

    Add problem 1710: Maximum Units on a Truck

---
+++ src/problem_1710_maximum_units_on_a_truck/greedy_quick_select.rs
@@ -0,0 +1,109 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cmp::{Ordering, Reverse};
+use std::convert::TryInto;
+
+impl Solution {
+    fn partition<T, K>(values: &mut [T], mut key_fn: impl FnMut(&T) -> K) -> (usize, usize)
+    where
+        K: Ord,
+    {
+        // | less | equal | unknown | greater |
+
+        let mut equal_start = 0;
+        let mut unknown_start = 1;
+        let mut greater_start = values.len();
+        let target_key = key_fn(&values[0]);
+
+        while unknown_start < greater_start {
+            match key_fn(&values[unknown_start]).cmp(&target_key) {
+                Ordering::Less => {
+                    values.swap(equal_start, unknown_start);
+
+                    equal_start += 1;
+                    unknown_start += 1;
+                }
+                Ordering::Equal => unknown_start += 1,
+                Ordering::Greater => {
+                    greater_start -= 1;
+
+                    values.swap(unknown_start, greater_start);
+                }
+            }
+        }
+
+        (equal_start, greater_start)
+    }
+
+    pub fn maximum_units(box_types: Vec<Vec<i32>>, truck_size: i32) -> i32 {
+        let mut box_types = box_types
+            .into_iter()
+            .map(|box_type| {
+                let [count, units]: [_; 2] = box_type.try_into().ok().unwrap();
+
+                (units as u16, count as u16)
+            })
+            .collect::<Box<_>>();
+
+        let mut box_types = box_types.as_mut();
+        let mut truck_size = truck_size as u32;
+        let mut result = 0;
+
+        // Quick select.
+
+        'outer: while !box_types.is_empty() {
+            let (equal_start, equal_end) = Self::partition(box_types, |&(units, _)| Reverse(units));
+            let (left, rest) = box_types.split_at_mut(equal_start);
+            let left_total_count = left.iter().map(|&(_, count)| u32::from(count)).sum::<u32>();
+
+            box_types = if left_total_count < truck_size {
+                // Take all boxes in `left`.
+
+                for &(units, count) in &*left {
+                    result += u32::from(units) * u32::from(count);
+                }
+
+                truck_size -= left_total_count;
+
+                // Take as much boxes as possible in `middle`.
+
+                let (middle, right) = rest.split_at_mut(equal_end - equal_start);
+
+                for &(units, count) in &*middle {
+                    if u32::from(count) < truck_size {
+                        result += u32::from(units) * u32::from(count);
+                        truck_size -= u32::from(count);
+                    } else {
+                        result += u32::from(units) * truck_size;
+
+                        break 'outer;
+                    }
+                }
+
+                right
+            } else {
+                left
+            }
+        }
+
+        result as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn maximum_units(box_types: Vec<Vec<i32>>, truck_size: i32) -> i32 {
+        Self::maximum_units(box_types, truck_size)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1710_maximum_units_on_a_truck/mod.rs
@@ -0,0 +1,43 @@
+pub mod greedy_binary_heap;
+pub mod greedy_quick_select;
+
+pub trait Solution {
+    fn maximum_units(box_types: Vec<Vec<i32>>, truck_size: i32) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[[1, 3], [2, 2], [3, 1]] as &[_], 4), 8),
+            ((&[[5, 10], [2, 5], [4, 7], [3, 9]], 10), 91),
+            (
+                (
+                    &[
+                        [2, 1],
+                        [4, 4],
+                        [3, 1],
+                        [4, 1],
+                        [2, 4],
+                        [3, 4],
+                        [1, 3],
+                        [4, 3],
+                        [5, 3],
+                        [5, 3],
+                    ],
+                    13,
+                ),
+                48,
+            ),
+        ];
+
+        for ((box_types, truck_size), expected) in test_cases {
+            assert_eq!(
+                S::maximum_units(box_types.iter().copied().map(Vec::from).collect(), truck_size),
+                expected,
+            );
+        }
+    }
+}
