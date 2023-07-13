7e49c6a1d7c53c5d74c29fc4af620579f74d692a
EFanZh <efanzh@gmail.com>
2023-07-13 21:42:33 +0800

    Add problem 1716: Calculate Money in Leetcode Bank

--- src/lib.rs
+++ src/lib.rs
@@ -1347,6 +1347,7 @@ pub mod problem_1704_determine_if_string_halves_are_alike;
 pub mod problem_1706_where_will_the_ball_fall;
 pub mod problem_1710_maximum_units_on_a_truck;
 pub mod problem_1711_count_good_meals;
+pub mod problem_1716_calculate_money_in_leetcode_bank;
 pub mod problem_1720_decode_xored_array;
 pub mod problem_1736_latest_time_by_replacing_hidden_digits;
 pub mod problem_1743_restore_the_array_from_adjacent_pairs;
