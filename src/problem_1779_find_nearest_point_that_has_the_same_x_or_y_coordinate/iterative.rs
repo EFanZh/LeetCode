c8024f5a8a9e323fd55589515916674fa54058a3
EFanZh <efanzh@gmail.com>
2023-08-05 16:56:26 +0800

    Add problem 1779: Find Nearest Point That Has the Same X or Y Coordinate

---
+++ src/problem_1779_find_nearest_point_that_has_the_same_x_or_y_coordinate/iterative.rs
@@ -0,0 +1,47 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32 {
+        let mut result = -1;
+        let mut min_distance = i32::MAX;
+
+        for (i, point) in (0..).zip(&points) {
+            let [p_x, p_y]: [_; 2] = point.as_slice().try_into().ok().unwrap();
+
+            let distance = if p_x == x {
+                (p_y - y).abs()
+            } else if p_y == y {
+                (p_x - x).abs()
+            } else {
+                continue;
+            };
+
+            if distance < min_distance {
+                min_distance = distance;
+                result = i;
+            }
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32 {
+        Self::nearest_valid_point(x, y, points)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1779_find_nearest_point_that_has_the_same_x_or_y_coordinate/mod.rs
@@ -0,0 +1,25 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn nearest_valid_point(x: i32, y: i32, points: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((3, 4, &[[1, 2], [3, 1], [2, 4], [2, 3], [4, 4]] as &[_]), 2),
+            ((3, 4, &[[3, 4]]), 0),
+            ((3, 4, &[[2, 3]]), -1),
+        ];
+
+        for ((x, y, points), expected) in test_cases {
+            assert_eq!(
+                S::nearest_valid_point(x, y, points.iter().copied().map(Vec::from).collect()),
+                expected
+            );
+        }
+    }
+}
