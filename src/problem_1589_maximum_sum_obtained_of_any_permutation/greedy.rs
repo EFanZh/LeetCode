94b36745957b904cc4503c1fbd2e5e861dbbeaa2
EFanZh <efanzh@gmail.com>
2023-08-14 23:48:10 +0800

    Add problem 1589: Maximum Sum Obtained of Any Permutation

---
+++ src/problem_1589_maximum_sum_obtained_of_any_permutation/greedy.rs
@@ -0,0 +1,58 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn max_sum_range_query(nums: Vec<i32>, requests: Vec<Vec<i32>>) -> i32 {
+        let mut nums = nums;
+        let nums = nums.as_mut_slice();
+        let n = nums.len();
+        let mut usages = vec![0_i32; n].into_boxed_slice();
+
+        for request in requests {
+            let [start, end]: [_; 2] = request.try_into().ok().unwrap();
+
+            usages[start as u32 as usize] += 1;
+
+            if let Some(diff) = usages.get_mut(end as u32 as usize + 1) {
+                *diff -= 1;
+            }
+        }
+
+        let mut usage = 0;
+
+        for diff_or_usage in &mut *usages {
+            usage += *diff_or_usage;
+            *diff_or_usage = usage;
+        }
+
+        nums.sort_unstable();
+        usages.sort_unstable();
+
+        let result = nums
+            .iter()
+            .zip(&*usages)
+            .map(|(&num, &usage)| i64::from(num) * i64::from(usage))
+            .sum::<i64>();
+
+        (result as u64 % 1_000_000_007) as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn max_sum_range_query(nums: Vec<i32>, requests: Vec<Vec<i32>>) -> i32 {
+        Self::max_sum_range_query(nums, requests)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_1589_maximum_sum_obtained_of_any_permutation/mod.rs
@@ -0,0 +1,26 @@
+pub mod greedy;
+
+pub trait Solution {
+    fn max_sum_range_query(nums: Vec<i32>, requests: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5] as &[_], &[[1, 3], [0, 1]] as &[_]), 19),
+            ((&[1, 2, 3, 4, 5, 6], &[[0, 1]]), 11),
+            ((&[1, 2, 3, 4, 5, 10], &[[0, 2], [1, 3], [1, 1]]), 47),
+            ((&[1, 8, 5, 5, 2], &[[4, 4], [3, 4], [4, 4], [2, 4], [0, 0]]), 49),
+        ];
+
+        for ((nums, requests), expected) in test_cases {
+            assert_eq!(
+                S::max_sum_range_query(nums.to_vec(), requests.iter().copied().map(Vec::from).collect()),
+                expected
+            );
+        }
+    }
+}
