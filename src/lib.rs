f3ea4e59863487b3204609025796dc2ff82905bb
EFanZh <efanzh@gmail.com>
2023-09-13 20:54:07 +0800

    Add problem 1837: Sum of Digits in Base K

--- src/lib.rs
+++ src/lib.rs
@@ -1414,6 +1414,7 @@ pub mod problem_1824_minimum_sideway_jumps;
 pub mod problem_1827_minimum_operations_to_make_the_array_increasing;
 pub mod problem_1832_check_if_the_sentence_is_pangram;
 pub mod problem_1833_maximum_ice_cream_bars;
+pub mod problem_1837_sum_of_digits_in_base_k;
 
 #[cfg(test)]
 mod test_utilities;
