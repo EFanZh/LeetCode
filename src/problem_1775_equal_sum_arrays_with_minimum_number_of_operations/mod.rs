652874547b49e5c4b8538b2a89f5890fc84ffd95
EFanZh <efanzh@gmail.com>
2023-09-29 19:18:02 +0800

    Add problem 1775: Equal Sum Arrays With Minimum Number of Operations

---
+++ src/problem_1775_equal_sum_arrays_with_minimum_number_of_operations/mod.rs
@@ -0,0 +1,22 @@
+pub mod greedy;
+
+pub trait Solution {
+    fn min_operations(nums1: Vec<i32>, nums2: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5, 6] as &[_], &[1, 1, 2, 2, 2, 2] as &[_]), 3),
+            ((&[1, 1, 1, 1, 1, 1, 1], &[6]), -1),
+            ((&[6, 6], &[1]), 3),
+        ];
+
+        for ((nums1, nums2), expected) in test_cases {
+            assert_eq!(S::min_operations(nums1.to_vec(), nums2.to_vec()), expected);
+        }
+    }
+}
