a58d62293dfe1363238c4ce601d73fffa0b1ecc8
EFanZh <efanzh@gmail.com>
2023-10-17 22:10:28 +0800

    Cover more codes

--- src/problem_1802_maximum_value_at_a_given_index_in_a_bounded_array/mod.rs
+++ src/problem_1802_maximum_value_at_a_given_index_in_a_bounded_array/mod.rs
@@ -9,7 +9,13 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [((4, 2, 6), 2), ((6, 1, 10), 3), ((4, 0, 4), 1), ((5, 0, 28), 7)];
+        let test_cases = [
+            ((4, 2, 6), 2),
+            ((6, 1, 10), 3),
+            ((4, 0, 4), 1),
+            ((5, 0, 28), 7),
+            ((3, 2, 18), 7),
+        ];
 
         for ((n, index, max_sum), expected) in test_cases {
             assert_eq!(S::max_value(n, index, max_sum), expected);
