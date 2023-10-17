a1740533638b4735c6a2c77ba757f38ac83d5055
EFanZh <efanzh@gmail.com>
2023-10-17 22:11:27 +0800

    Add problem 1884: Egg Drop With 2 Eggs and N Floors

--- src/lib.rs
+++ src/lib.rs
@@ -1449,6 +1449,7 @@ pub mod problem_1876_substrings_of_size_three_with_distinct_characters;
 pub mod problem_1877_minimize_maximum_pair_sum_in_array;
 pub mod problem_1880_check_if_word_equals_summation_of_two_words;
 pub mod problem_1881_maximum_value_after_insertion;
+pub mod problem_1884_egg_drop_with_2_eggs_and_n_floors;
 
 #[cfg(test)]
 mod test_utilities;
