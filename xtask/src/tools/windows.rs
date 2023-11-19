20e8959b2099edc679aa78016e92e58e8f2f2244
EFanZh <efanzh@gmail.com>
2023-11-19 17:22:24 +0800

    Use `OnceLock` instead of `lazy_static`

--- xtask/src/tools/windows.rs
+++ xtask/src/tools/windows.rs
@@ -1,19 +1,21 @@
 use std::path::{Path, PathBuf};
+use std::sync::OnceLock;
 
-lazy_static::lazy_static! {
-    static ref VISUAL_STUDIO_PATH: Option<PathBuf> = find_visual_studio();
-}
+static VISUAL_STUDIO_PATH: OnceLock<Option<Box<Path>>> = OnceLock::new();
 
-fn find_visual_studio() -> Option<PathBuf> {
-    let mut buffer = cc::windows_registry::find_tool("x86_64-pc-windows-msvc", "cl.exe")?
-        .path()
-        .to_path_buf();
+fn find_visual_studio() -> Option<&'static Path> {
+    VISUAL_STUDIO_PATH
+        .get_or_init(|| {
+            let tool = cc::windows_registry::find_tool("x86_64-pc-windows-msvc", "cl.exe")?;
+            let mut path = tool.path();
 
-    for _ in 0..8 {
-        buffer.pop();
-    }
+            for _ in 0..8 {
+                path = path.parent().unwrap();
+            }
 
-    Some(buffer)
+            Some(path.into())
+        })
+        .as_deref()
 }
 
 pub fn find_cmake() -> Option<PathBuf> {
@@ -21,22 +23,18 @@ pub fn find_cmake() -> Option<PathBuf> {
         r"C:\Program Files\CMake\bin\cmake.exe",
         r"C:\Program Files (x86)\CMake\bin\cmake.exe",
     ] {
-        if Path::new(path).is_file() {
+        if Path::is_file(path.as_ref()) {
             return Some(PathBuf::from(path));
         }
     }
 
-    let cmake_path = VISUAL_STUDIO_PATH
-        .as_deref()?
-        .join(r"Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe");
+    let cmake_path = find_visual_studio()?.join(r"Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe");
 
     cmake_path.is_file().then_some(cmake_path)
 }
 
 pub fn find_ninja() -> Option<PathBuf> {
-    let ninja_path = VISUAL_STUDIO_PATH
-        .as_deref()?
-        .join(r"Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe");
+    let ninja_path = find_visual_studio()?.join(r"Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe");
 
     ninja_path.is_file().then_some(ninja_path)
 }
