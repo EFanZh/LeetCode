a7f29613983bbda1f1a596683f83e122a2c32fcb
EFanZh <efanzh@gmail.com>
2023-11-20 23:53:34 +0800

    Add problem 1718: Construct the Lexicographically Largest Valid Sequence

--- src/lib.rs
+++ src/lib.rs
@@ -1276,6 +1276,7 @@ pub mod problem_1711_count_good_meals;
 pub mod problem_1712_ways_to_split_array_into_three_subarrays;
 pub mod problem_1716_calculate_money_in_leetcode_bank;
 pub mod problem_1717_maximum_score_from_removing_substrings;
+pub mod problem_1718_construct_the_lexicographically_largest_valid_sequence;
 pub mod problem_1720_decode_xored_array;
 pub mod problem_1721_swapping_nodes_in_a_linked_list;
 pub mod problem_1725_number_of_rectangles_that_can_form_the_largest_square;
