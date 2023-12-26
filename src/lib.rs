c02e843544493b749c1c1e5f35590da41c487192
EFanZh <efanzh@gmail.com>
2023-12-26 20:37:10 +0800

    Fix the name of problem 2022

--- src/lib.rs
+++ src/lib.rs
@@ -1396,7 +1396,7 @@ pub mod problem_2007_find_original_array_from_doubled_array;
 pub mod problem_2011_final_value_of_variable_after_performing_operations;
 pub mod problem_2012_sum_of_beauty_in_the_array;
 pub mod problem_2016_maximum_difference_between_increasing_elements;
-pub mod problem_2022_find_original_array_from_doubled_array;
+pub mod problem_2022_convert_1d_array_into_2d_array;
 pub mod problem_2027_minimum_moves_to_convert_string;
 pub mod problem_2028_find_missing_observations;
 
