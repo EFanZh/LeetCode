bd7ac8f8146628786bbd45f6cb44c45489ca22d2
EFanZh <efanzh@gmail.com>
2024-01-07 22:52:43 +0800

    Add problem 1744: Can You Eat Your Favorite Candy on Your Favorite Day?

---
+++ src/problem_1744_can_you_eat_your_favorite_candy_on_your_favorite_day/iterative.rs
@@ -0,0 +1,56 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::convert::TryInto;
+
+impl Solution {
+    fn interval_overlaps(left: (u32, u32), right: (u32, u32)) -> bool {
+        left.0 <= right.1 && left.1 >= right.0
+    }
+
+    pub fn can_eat(candies_count: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<bool> {
+        let mut candy_sums = candies_count;
+        let mut sum = 0;
+
+        for target in &mut candy_sums {
+            sum += *target;
+            *target = sum;
+        }
+
+        queries
+            .into_iter()
+            .map(|query| {
+                let [favorite_type, favorite_day, daily_cap]: [_; 3] = query.try_into().ok().unwrap();
+                let favorite_type = favorite_type as u32 as usize;
+                let favorite_day = favorite_day as u32 as usize;
+                let daily_cap = daily_cap as u32;
+                let left_sum = candy_sums.get(favorite_type.wrapping_sub(1)).copied().unwrap_or(0) as u32;
+
+                Self::interval_overlaps(
+                    (
+                        left_sum.saturating_sub(daily_cap - 1),
+                        candy_sums[favorite_type] as u32 - 1,
+                    ),
+                    (favorite_day as _, daily_cap.saturating_mul(favorite_day as _)),
+                )
+            })
+            .collect()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn can_eat(candies_count: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<bool> {
+        Self::can_eat(candies_count, queries)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
