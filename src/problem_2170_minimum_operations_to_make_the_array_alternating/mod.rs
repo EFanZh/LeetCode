19db3271f82615add8e9d251f92212f396aa2ed9
EFanZh <efanzh@gmail.com>
2024-03-25 20:45:50 +0800

    Cover more codes

--- src/problem_2170_minimum_operations_to_make_the_array_alternating/mod.rs
+++ src/problem_2170_minimum_operations_to_make_the_array_alternating/mod.rs
@@ -9,7 +9,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [(&[3, 1, 3, 2, 4, 3] as &[_], 3), (&[1, 2, 2, 2, 2], 2)];
+        let test_cases = [
+            (&[3, 1, 3, 2, 4, 3] as &[_], 3),
+            (&[1, 2, 2, 2, 2], 2),
+            (&[3, 1, 3, 2, 4, 3], 3),
+        ];
 
         for (nums, expected) in test_cases {
             assert_eq!(S::minimum_operations(nums.to_vec()), expected);
