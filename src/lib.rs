d1d409a4aedb9860c60d8ce8a64809d511b9915b
EFanZh <efanzh@gmail.com>
2024-04-02 22:49:49 +0800

    Add problem 2248: Intersection of Multiple Arrays

--- src/lib.rs
+++ src/lib.rs
@@ -1496,6 +1496,7 @@ pub mod problem_2236_root_equals_sum_of_children;
 pub mod problem_2239_find_closest_number_to_zero;
 pub mod problem_2243_calculate_digit_sum_of_a_string;
 pub mod problem_2244_minimum_rounds_to_complete_all_tasks;
+pub mod problem_2248_intersection_of_multiple_arrays;
 
 #[cfg(test)]
 mod test_utilities;
