d249dae65a1bc67136498dbc83eded676c492554
EFanZh <efanzh@gmail.com>
2024-06-06 19:13:06 +0800

    Cover more codes

--- src/problem_2266_count_number_of_texts/mod.rs
+++ src/problem_2266_count_number_of_texts/mod.rs
@@ -9,7 +9,11 @@ mod tests {
     use super::Solution;
 
     pub fn run<S: Solution>() {
-        let test_cases = [("22233", 8), ("222222222222222222222222222222222222", 82_876_089)];
+        let test_cases = [
+            ("22233", 8),
+            ("222222222222222222222222222222222222", 82_876_089),
+            ("444479999555588866", 3136),
+        ];
 
         for (pressed_keys, expected) in test_cases {
             assert_eq!(S::count_texts(pressed_keys.to_string()), expected);
