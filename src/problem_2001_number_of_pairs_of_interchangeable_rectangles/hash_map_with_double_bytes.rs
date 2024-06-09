1741daa54d7237af123adac2f7dced1d6b029163
EFanZh <efanzh@gmail.com>
2024-06-09 20:11:57 +0800

    Add problem 2001: Number of Pairs of Interchangeable Rectangles

---
+++ src/problem_2001_number_of_pairs_of_interchangeable_rectangles/hash_map_with_double_bytes.rs
@@ -0,0 +1,49 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use std::convert::TryInto;
+
+impl Solution {
+    pub fn interchangeable_rectangles(rectangles: Vec<Vec<i32>>) -> i64 {
+        let mut counts = HashMap::<_, u32>::new();
+        let mut result = 0_u64;
+
+        for rectangle in rectangles {
+            let [width, height]: [_; 2] = rectangle.try_into().ok().unwrap();
+
+            match counts.entry((f64::from(width) / f64::from(height)).to_ne_bytes()) {
+                Entry::Occupied(entry) => {
+                    let count = entry.into_mut();
+
+                    result += u64::from(*count);
+
+                    *count += 1;
+                }
+                Entry::Vacant(entry) => {
+                    entry.insert(1);
+                }
+            }
+        }
+
+        result as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn interchangeable_rectangles(rectangles: Vec<Vec<i32>>) -> i64 {
+        Self::interchangeable_rectangles(rectangles)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
