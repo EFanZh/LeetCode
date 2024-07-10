52c348e9bd14a88d2689e6118e0260dba2720ebf
EFanZh <efanzh@gmail.com>
2024-07-10 22:26:27 +0800

    Add problem 2096: Step-By-Step Directions From a Binary Tree Node to Another

---
+++ src/problem_2096_step_by_step_directions_from_a_binary_tree_node_to_another/mod.rs
@@ -0,0 +1,129 @@
+use crate::data_structures::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub mod recursive;
+
+pub trait Solution {
+    fn get_directions(root: Option<Rc<RefCell<TreeNode>>>, start_value: i32, dest_value: i32) -> String;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                (
+                    &[Some(5), Some(1), Some(2), Some(3), None, Some(6), Some(4)] as &[_],
+                    3,
+                    6,
+                ),
+                "UURL",
+            ),
+            ((&[Some(2), Some(1)], 2, 1), "L"),
+            (
+                (
+                    &[
+                        Some(5),
+                        Some(8),
+                        Some(3),
+                        Some(1),
+                        None,
+                        Some(4),
+                        Some(7),
+                        Some(6),
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        Some(2),
+                    ],
+                    5,
+                    6,
+                ),
+                "LLL",
+            ),
+            (
+                (
+                    &[
+                        Some(7),
+                        Some(8),
+                        Some(3),
+                        Some(1),
+                        None,
+                        Some(4),
+                        Some(5),
+                        Some(6),
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        Some(2),
+                    ],
+                    7,
+                    5,
+                ),
+                "RR",
+            ),
+            (
+                (
+                    &[
+                        Some(3),
+                        Some(6),
+                        Some(5),
+                        None,
+                        None,
+                        Some(13),
+                        Some(8),
+                        Some(9),
+                        Some(19),
+                        Some(11),
+                        Some(2),
+                        Some(10),
+                        Some(17),
+                        None,
+                        None,
+                        None,
+                        Some(18),
+                        None,
+                        Some(14),
+                        None,
+                        None,
+                        None,
+                        None,
+                        Some(15),
+                        None,
+                        Some(4),
+                        Some(16),
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        Some(7),
+                        None,
+                        Some(1),
+                        Some(12),
+                    ],
+                    10,
+                    13,
+                ),
+                "UU",
+            ),
+        ];
+
+        for ((root, start_value, dest_value), expected) in test_cases {
+            assert_eq!(
+                S::get_directions(test_utilities::make_tree(root.iter().copied()), start_value, dest_value),
+                expected,
+            );
+        }
+    }
+}
---
+++ src/problem_2096_step_by_step_directions_from_a_binary_tree_node_to_another/recursive.rs
@@ -0,0 +1,129 @@
+use crate::data_structures::TreeNode;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::cell::RefCell;
+use std::ops::ControlFlow;
+use std::rc::Rc;
+use std::{iter, mem};
+
+impl Solution {
+    fn find_1(
+        node: &Option<Rc<RefCell<TreeNode>>>,
+        path: &mut Vec<u8>,
+        value: i32,
+        target: &mut Vec<u8>,
+    ) -> ControlFlow<()> {
+        if let Some(node) = node {
+            let node = node.borrow();
+            let node = &*node;
+
+            if node.val == value {
+                mem::swap(path, target);
+
+                return ControlFlow::Break(());
+            }
+
+            Self::find_1_children(node, path, value, target)?;
+        }
+
+        ControlFlow::Continue(())
+    }
+
+    fn find_1_children(node: &TreeNode, path: &mut Vec<u8>, value: i32, target: &mut Vec<u8>) -> ControlFlow<()> {
+        path.push(b'L');
+        Self::find_1(&node.left, path, value, target)?;
+        path.pop();
+
+        path.push(b'R');
+        Self::find_1(&node.right, path, value, target)?;
+        path.pop();
+
+        ControlFlow::Continue(())
+    }
+
+    fn find_2(
+        node: &Option<Rc<RefCell<TreeNode>>>,
+        path: &mut Vec<u8>,
+        value_1: i32,
+        target_1: &mut Vec<u8>,
+        value_2: i32,
+        target_2: &mut Vec<u8>,
+    ) -> ControlFlow<()> {
+        if let Some(node) = node {
+            let node = node.borrow();
+            let node = &*node;
+
+            let (found, value, target) = if node.val == value_1 {
+                (target_1, value_2, target_2)
+            } else if node.val == value_2 {
+                (target_2, value_1, target_1)
+            } else {
+                path.push(b'L');
+                Self::find_2(&node.left, path, value_1, target_1, value_2, target_2)?;
+                path.pop();
+
+                path.push(b'R');
+                Self::find_2(&node.right, path, value_1, target_1, value_2, target_2)?;
+                path.pop();
+
+                return ControlFlow::Continue(());
+            };
+
+            drop(mem::replace(found, path.clone()));
+
+            Self::find_1_children(node, path, value, target)?;
+        }
+
+        ControlFlow::Continue(())
+    }
+
+    pub fn get_directions(root: Option<Rc<RefCell<TreeNode>>>, start_value: i32, dest_value: i32) -> String {
+        let mut start_path = Vec::new();
+        let mut dest_path = Vec::new();
+
+        Self::find_2(
+            &root,
+            &mut Vec::new(),
+            start_value,
+            &mut start_path,
+            dest_value,
+            &mut dest_path,
+        );
+
+        let mut common_length = 0_usize;
+
+        for (lhs, rhs) in start_path.iter().zip(&dest_path) {
+            if lhs == rhs {
+                common_length += 1;
+            } else {
+                break;
+            }
+        }
+
+        dest_path.splice(
+            ..common_length,
+            iter::repeat(b'U').take(start_path.len() - common_length),
+        );
+
+        String::from_utf8(dest_path).unwrap()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn get_directions(root: Option<Rc<RefCell<TreeNode>>>, start_value: i32, dest_value: i32) -> String {
+        Self::get_directions(root, start_value, dest_value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
