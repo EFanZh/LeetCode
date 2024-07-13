193456289997fd5dcaec81a63ffc64a9de5e7245
EFanZh <efanzh@gmail.com>
2024-07-13 23:09:25 +0800

    Add problem 2183: Count Array Pairs Divisible by K

---
+++ src/problem_2183_count_array_pairs_divisible_by_k/mod.rs
@@ -0,0 +1,25 @@
+pub mod gcd_and_hash_map;
+
+pub trait Solution {
+    fn count_pairs(nums: Vec<i32>, k: i32) -> i64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ((&[1, 2, 3, 4, 5] as &[_], 2), 7),
+            ((&[1, 2, 3, 4], 5), 0),
+            ((&[8, 10, 2, 5, 9, 6, 3, 8, 2], 6), 18),
+            ((&[67, 64, 65, 66, 16, 51], 33), 5),
+            ((&[5, 8], 1), 1),
+            ((&[9, 2, 6, 9, 1, 2], 9), 9),
+        ];
+
+        for ((nums, k), expected) in test_cases {
+            assert_eq!(S::count_pairs(nums.to_vec(), k), expected);
+        }
+    }
+}
