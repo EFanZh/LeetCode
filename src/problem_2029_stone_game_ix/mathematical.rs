6e61a89ac0d271b282aad3f04e09f0f47ad837ee
EFanZh <efanzh@gmail.com>
2024-07-29 21:58:12 +0800

    Add problem 2029: Stone Game IX

---
+++ src/problem_2029_stone_game_ix/mathematical.rs
@@ -0,0 +1,38 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn stone_game_ix(stones: Vec<i32>) -> bool {
+        let mut counts = [0_u32; 3];
+
+        for stone in stones {
+            counts[usize::from(stone as u16 % 3)] += 1;
+        }
+
+        let [zero, one, two] = counts;
+        let (x, y) = if two < one { (two, one) } else { (one, two) };
+
+        if zero % 2 == 0 {
+            x != 0
+        } else {
+            y - x > 2
+        }
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn stone_game_ix(stones: Vec<i32>) -> bool {
+        Self::stone_game_ix(stones)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
