129b436f30e227f0ce7deab34252787d8ee9950e
EFanZh <efanzh@gmail.com>
2024-09-28 13:53:07 +0800

    Add problem 2245: Maximum Trailing Zeros in a Cornered Path

---
+++ src/problem_2245_maximum_trailing_zeros_in_a_cornered_path/mod.rs
@@ -0,0 +1,31 @@
+pub mod dynamic_programming;
+
+pub trait Solution {
+    fn max_trailing_zeros(grid: Vec<Vec<i32>>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::test_utilities::Matrix;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (
+                &[
+                    [23, 17, 15, 3, 20],
+                    [8, 1, 20, 27, 11],
+                    [9, 4, 6, 2, 21],
+                    [40, 9, 1, 10, 6],
+                    [22, 7, 4, 5, 3],
+                ] as &dyn Matrix<_>,
+                3,
+            ),
+            (&[[4, 3, 2], [7, 6, 1], [8, 8, 8]], 0),
+        ];
+
+        for (grid, expected) in test_cases {
+            assert_eq!(S::max_trailing_zeros(grid.to_vec()), expected);
+        }
+    }
+}
