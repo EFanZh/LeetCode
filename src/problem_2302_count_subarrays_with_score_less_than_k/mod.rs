31539c05ffd3a7e9c908bdce1faa982cd6f06663
EFanZh <efanzh@gmail.com>
2024-11-01 23:50:49 +0800

    Add problem 2302: Count Subarrays With Score Less Than K

---
+++ src/problem_2302_count_subarrays_with_score_less_than_k/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn count_subarrays(nums: Vec<i32>, k: i64) -> i64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [((&[2, 1, 4, 3, 5] as &[_], 10), 6), ((&[1, 1, 1], 5), 5)];
+
+        for ((nums, k), expected) in test_cases {
+            assert_eq!(S::count_subarrays(nums.to_vec(), k), expected);
+        }
+    }
+}
