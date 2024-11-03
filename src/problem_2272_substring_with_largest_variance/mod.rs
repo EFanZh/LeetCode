2fb61865385e0a33a1ea8d80fbe94a953f67a680
EFanZh <efanzh@gmail.com>
2024-11-03 22:50:20 +0800

    Add problem 2272: Substring With Largest Variance

---
+++ src/problem_2272_substring_with_largest_variance/mod.rs
@@ -0,0 +1,24 @@
+pub mod prefix_sums;
+
+pub trait Solution {
+    fn largest_variance(s: String) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("aababbb", 3),
+            ("abcde", 0),
+            ("bbc", 1),
+            ("aaaaabbba", 4),
+            ("ykudzhiixwttnvtesiwnbcjmsydidttiyabbwzlfbmmycwjgzwhbtvtxyvkkjgfehaypiygpstkhakfasiloaveqzcywsiujvixcdnxpvvtobxgroznswwwipypwmdhldsoswrzyqthaqlbwragjrqwjxgmftjxqugoonxadazeoxalmccfeyqtmoxwbnphxih", 12),
+        ];
+
+        for (s, expected) in test_cases {
+            assert_eq!(S::largest_variance(s.to_string()), expected);
+        }
+    }
+}
