08a9f1bfa04abe25b6c0701058bd51a487855626
EFanZh <efanzh@gmail.com>
2025-02-25 21:54:12 +0800

    Add problem 2455: Average Value of Even Numbers That Are Divisible by Three

---
+++ src/problem_2455_average_value_of_even_numbers_that_are_divisible_by_three/iterative.rs
@@ -0,0 +1,37 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn average_value(nums: Vec<i32>) -> i32 {
+        let mut sum = 0_u32;
+        let mut count = 0_u32;
+
+        for num in nums {
+            let num = num as u32;
+
+            if num % 6 == 0 {
+                sum += num;
+                count += 1;
+            }
+        }
+
+        sum.checked_div(count).unwrap_or(0) as _
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn average_value(nums: Vec<i32>) -> i32 {
+        Self::average_value(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
