5a806946810153ae5e5e94d88c28af8909732009
EFanZh <efanzh@gmail.com>
2025-02-27 22:08:12 +0800

    Add problem 2460: Apply Operations to an Array

--- src/lib.rs
+++ src/lib.rs
@@ -1827,6 +1827,7 @@ pub mod problem_2452_words_within_two_edits_of_dictionary;
 pub mod problem_2453_destroy_sequential_targets;
 pub mod problem_2455_average_value_of_even_numbers_that_are_divisible_by_three;
 pub mod problem_2456_most_popular_video_creator;
+pub mod problem_2460_apply_operations_to_an_array;
 
 #[cfg(test)]
 mod test_utilities;
