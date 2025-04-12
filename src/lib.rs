8b88eb8fd8075ffba1f0644fd34ae94ba9629f1d
EFanZh <efanzh@gmail.com>
2025-04-12 23:30:00 +0800

    Add problem 2549: Count Distinct Numbers on Board

--- src/lib.rs
+++ src/lib.rs
@@ -1890,6 +1890,7 @@ pub mod problem_2542_maximum_subsequence_score;
 pub mod problem_2544_alternating_digit_sum;
 pub mod problem_2545_sort_the_students_by_their_kth_score;
 pub mod problem_2546_apply_bitwise_operations_to_make_strings_equal;
+pub mod problem_2549_count_distinct_numbers_on_board;
 
 #[cfg(test)]
 mod test_utilities;
