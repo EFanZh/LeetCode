c5316275bcc79c634374a7a00262e6fbd2a00017
EFanZh <efanzh@gmail.com>
2025-06-05 21:44:28 +0800

    Add problem 2706: Buy Two Chocolates

--- src/lib.rs
+++ src/lib.rs
@@ -1944,6 +1944,7 @@ pub mod problem_2682_find_the_losers_of_the_circular_game;
 pub mod problem_2683_neighboring_bitwise_xor;
 pub mod problem_2696_minimum_string_length_after_removing_substrings;
 pub mod problem_2697_lexicographically_smallest_palindrome;
+pub mod problem_2706_buy_two_chocolates;
 
 #[cfg(test)]
 mod test_utilities;
