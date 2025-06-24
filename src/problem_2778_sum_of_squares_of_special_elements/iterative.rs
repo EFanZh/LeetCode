d312bf462d259e3bf49c7e278a9d602b6769fdb9
EFanZh <efanzh@gmail.com>
2025-06-24 22:53:37 +0800

    Add problem 2778: Sum of Squares of Special Elements

---
+++ src/problem_2778_sum_of_squares_of_special_elements/iterative.rs
@@ -0,0 +1,31 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn sum_of_squares(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as u32;
+
+        (1..)
+            .zip(nums)
+            .filter(|&(i, _)| n % i == 0)
+            .map(|(_, num)| num * num)
+            .sum()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn sum_of_squares(nums: Vec<i32>) -> i32 {
+        Self::sum_of_squares(nums)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
