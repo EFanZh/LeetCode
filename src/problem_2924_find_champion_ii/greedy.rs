00331f3b3d93c36f55307f5576e576368217401a
EFanZh <efanzh@gmail.com>
2025-08-13 21:42:03 +0800

    Add problem 2924: Find Champion II

---
+++ src/problem_2924_find_champion_ii/greedy.rs
@@ -0,0 +1,33 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn find_champion(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        let mut lost = vec![false; n as u32 as usize].into_boxed_slice();
+
+        edges.into_iter().for_each(|edge| lost[edge[1] as u32 as usize] = true);
+
+        if lost.iter().filter(|&&lost| !lost).count() == 1 {
+            lost.iter().position(|&lost| !lost).unwrap() as _
+        } else {
+            -1
+        }
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn find_champion(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        Self::find_champion(n, edges)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
