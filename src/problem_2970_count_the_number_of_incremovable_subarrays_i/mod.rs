2b614ab67ed4b25a3adc7fff4bb41c047ddc7727
EFanZh <efanzh@gmail.com>
2025-08-31 23:36:41 +0800

    Add problem 2970: Count the Number of Incremovable Subarrays I

---
+++ src/problem_2970_count_the_number_of_incremovable_subarrays_i/mod.rs
@@ -0,0 +1,18 @@
+pub mod iterative;
+
+pub trait Solution {
+    fn incremovable_subarray_count(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[1, 2, 3, 4] as &[_], 10), (&[6, 5, 7, 8], 7), (&[8, 7, 6, 6], 3)];
+
+        for (nums, expected) in test_cases {
+            assert_eq!(S::incremovable_subarray_count(nums.to_vec()), expected);
+        }
+    }
+}
