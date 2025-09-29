2cbf9f7f5d4c737b989763dbe026a7636be8fa6c
EFanZh <efanzh@gmail.com>
2025-09-29 22:31:19 +0800

    Add problem 2762: Continuous Subarrays

---
+++ src/problem_2762_continuous_subarrays/mod.rs
@@ -0,0 +1,18 @@
+pub mod sliding_window;
+
+pub trait Solution {
+    fn continuous_subarrays(nums: Vec<i32>) -> i64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[5, 4, 2, 4] as &[_], 8), (&[1, 2, 3], 6)];
+
+        for (nums, expected) in test_cases {
+            assert_eq!(S::continuous_subarrays(nums.to_vec()), expected);
+        }
+    }
+}
