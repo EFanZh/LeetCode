ca917083d9f356dfb1c5d519b5be0d056d30406d
EFanZh <efanzh@gmail.com>
2025-10-23 21:40:18 +0800

    Add problem 3096: Minimum Levels to Gain More Points

---
+++ src/problem_3096_minimum_levels_to_gain_more_points/mod.rs
@@ -0,0 +1,23 @@
+pub mod greedy;
+
+pub trait Solution {
+    fn minimum_levels(possible: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[1, 0, 1, 0] as &[_], 1),
+            (&[1, 1, 1, 1, 1], 3),
+            (&[0, 0], -1),
+            (&[1, 1], -1),
+        ];
+
+        for (possible, expected) in test_cases {
+            assert_eq!(S::minimum_levels(possible.to_vec()), expected);
+        }
+    }
+}
