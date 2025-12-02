2f5e2b053be70edf4946ff73869e91830417e824
EFanZh <efanzh@gmail.com>
2025-12-02 21:36:14 +0800

    Add problem 3185: Count Pairs That Form a Complete Day II

---
+++ src/problem_3185_count_pairs_that_form_a_complete_day_ii/mod.rs
@@ -0,0 +1,18 @@
+pub mod modular_arithmetic;
+
+pub trait Solution {
+    fn count_complete_day_pairs(hours: Vec<i32>) -> i64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(&[12, 12, 30, 24, 24] as &[_], 2), (&[72, 48, 24, 3], 3)];
+
+        for (hours, expected) in test_cases {
+            assert_eq!(S::count_complete_day_pairs(hours.to_vec()), expected);
+        }
+    }
+}
---
+++ src/problem_3185_count_pairs_that_form_a_complete_day_ii/modular_arithmetic.rs
@@ -0,0 +1,35 @@
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl Solution {
+    pub fn count_complete_day_pairs(hours: Vec<i32>) -> i64 {
+        let mut counts = [0_u64; 24];
+        let mut result = 0;
+
+        for hour in hours {
+            let hour = (hour.cast_unsigned() % 24) as usize;
+
+            result += counts[if hour == 0 { 0 } else { 24 - hour }];
+            counts[hour] += 1;
+        }
+
+        result.cast_signed()
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn count_complete_day_pairs(hours: Vec<i32>) -> i64 {
+        Self::count_complete_day_pairs(hours)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
