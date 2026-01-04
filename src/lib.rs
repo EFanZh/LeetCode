6430b8ec2bd3600737ba772bdb359a9acd114b1a
EFanZh <efanzh@gmail.com>
2026-01-04 21:30:30 +0800

    Add problem 3255: Find the Power of K-Size Subarrays II

--- src/lib.rs
+++ src/lib.rs
@@ -2158,6 +2158,7 @@ pub mod problem_3249_count_the_number_of_good_nodes;
 pub mod problem_3250_find_the_count_of_monotonic_pairs_i;
 pub mod problem_3251_find_the_count_of_monotonic_pairs_ii;
 pub mod problem_3254_find_the_power_of_k_size_subarrays_i;
+pub mod problem_3255_find_the_power_of_k_size_subarrays_ii;
 pub mod problem_3350_adjacent_increasing_subarrays_detection_ii;
 
 #[cfg(test)]
