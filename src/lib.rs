4a54ce2f7aba6bcd15516b6ce9091dfcd6962416
EFanZh <efanzh@gmail.com>
2026-01-31 21:36:23 +0800

    Add problem 3314: Construct the Minimum Bitwise Array I

--- src/lib.rs
+++ src/lib.rs
@@ -2185,6 +2185,7 @@ pub mod problem_3306_count_of_substrings_containing_every_vowel_and_k_consonants
 pub mod problem_3307_find_the_k_th_character_in_string_game_ii;
 pub mod problem_3309_maximum_possible_number_by_binary_concatenation;
 pub mod problem_3310_remove_methods_from_project;
+pub mod problem_3314_construct_the_minimum_bitwise_array_i;
 pub mod problem_3350_adjacent_increasing_subarrays_detection_ii;
 
 #[cfg(test)]
