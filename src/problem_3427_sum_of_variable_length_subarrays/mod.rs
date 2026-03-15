d52b7b8307a081ae035e0be80ed83f883af47838
EFanZh <efanzh@gmail.com>
2026-03-15 21:42:00 +0800

    Add problem 3427: Sum of Variable Length Subarrays

---
+++ src/problem_3427_sum_of_variable_length_subarrays/mod.rs
@@ -0,0 +1,18 @@
+pub mod prefix_sums;
+
+pub trait Solution {
+    fn subarray_sum(nums: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [(& [2,3,1] as &[_], 11), (& [3,1,1,2], 13)];
+
+        for (nums, expected) in test_cases {
+            assert_eq!(S::subarray_sum(nums.to_vec()), expected);
+        }
+    }
+}
